b44d6c01d9c1dedf89f5a62778b5aea5c129c8cf
Dustin Sallings <dustin@spy.net>
1997-01-07 15:42:36 +0000

    Ran indent on it's ass
    Author: dustin
    Date: 1997/01/07 07:42:36
    
    imported from: dustin@spy.net--projects/snippets--c--1.0--patch-41

--- c/c/net/tcp/cracker/getstatus.c
+++ c/c/net/tcp/cracker/getstatus.c
@@ -15,90 +15,94 @@
 
 #include "cracker.h"
 
-int openhost(char *host)
+int
+openhost(char *host)
 {
-struct hostent *hp;
-register int s;
-struct sockaddr_in sin;
+  struct hostent *hp;
+  register int s;
+  struct sockaddr_in sin;
 
-        if((hp=gethostbyname(host)) == NULL)
-        {
-                printf("ERR: gethostbyname\n");
-                exit(1);
-        }
-
-        if((s=socket(AF_INET, SOCK_STREAM, 0))<0)
-        {
-                perror("socket");
-                exit(1);
-        }
-
-        sin.sin_family = AF_INET;
-        sin.sin_port=htons(PORT);
-        bcopy(hp->h_addr, &sin.sin_addr, hp->h_length);
-
-        if(connect(s, (struct sockaddr *)&sin, sizeof(sin))<0)
-        {
-                perror("connect");
-                exit(1);
-        }
-
-        return(s);
-}
-
-void showconn(struct dllist thing, int curtime)
-{
-    time_t t;
-    int runtime;
-    t=ntohl(thing.contime);
-
-    runtime=(curtime-ntohl(thing.contime));
-
-    printf(" Socket:     %d\n", thing.socket);
-    printf(" Connected:  %s",   ctime(&t));
-    printf(" Runtime:    %d\n", runtime);
-    printf(" Tries:      %d\n", ntohl(thing.tries));
-    printf(" Tries/s:    %d\n", ntohl(thing.tries) / runtime);
-    puts(" ----------");
-}
-
-void main(int argc, char **argv)
-{
-int s, i;
-struct command c;
-struct info stat;
-time_t t;
-char *p;
-
-    s=openhost(argv[1]);
-
-    c.command=htonl(COM_STAT);
-
-    send(s, (char *)&c, sizeof(c), 0);
-
-    i=recv(s, (char *)&stat, sizeof(stat), 0);
-
-    while(i<sizeof(stat))
+  if ((hp = gethostbyname(host)) == NULL)
     {
-	printf("Got %d bytes, needed %d  Will wait for %d more\n", i,
-		sizeof(stat), sizeof(stat)-i);
-	p=(char *)&stat+i;
-        i+=recv(s, p, sizeof(stat)-i, 0);
+      printf("ERR: gethostbyname\n");
+      exit(1);
     }
 
-    t=ntohl(stat.starttime);
-    printf("Num cons:    %d\n", (ntohl(stat.cons)-1));
-    printf("Attemps:     %d\n", ntohl(stat.tries));
-    printf("Encrypted:   %s\n", stat.encrypted);
-    printf("Last try:    %s\n", stat.curpass);
-    printf("Start time:  %s", ctime(&t));
-    printf("Runtime:     %d\n\n", ntohl(stat.curtime)-ntohl(stat.starttime));
-
-    for(i=0; i<MAXCONS; i++)
+  if ((s = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-	if(ntohl(stat.acons[i].socket)>0 && ntohl(stat.acons[i].tries)>0)
+      perror("socket");
+      exit(1);
+    }
+
+  sin.sin_family = AF_INET;
+  sin.sin_port = htons(PORT);
+  bcopy(hp->h_addr, &sin.sin_addr, hp->h_length);
+
+  if (connect(s, (struct sockaddr *) &sin, sizeof(sin)) < 0)
+    {
+      perror("connect");
+      exit(1);
+    }
+
+  return (s);
+}
+
+void
+showconn(struct dllist thing, int curtime)
+{
+  time_t t;
+  int runtime;
+
+  t = ntohl(thing.contime);
+
+  runtime = (curtime - ntohl(thing.contime));
+
+  printf(" Socket:     %d\n", thing.socket);
+  printf(" Connected:  %s", ctime(&t));
+  printf(" Runtime:    %d\n", runtime);
+  printf(" Tries:      %d\n", ntohl(thing.tries));
+  printf(" Tries/s:    %d\n", ntohl(thing.tries) / runtime);
+  puts(" ----------");
+}
+
+void
+main(int argc, char **argv)
+{
+  int s, i;
+  struct command c;
+  struct info stat;
+  time_t t;
+  char *p;
+
+  s = openhost(argv[1]);
+
+  c.command = htonl(COM_STAT);
+
+  send(s, (char *) &c, sizeof(c), 0);
+
+  i = recv(s, (char *) &stat, sizeof(stat), 0);
+
+  while (i < sizeof(stat))
+    {
+      printf("Got %d bytes, needed %d  Will wait for %d more\n", i,
+	     sizeof(stat), sizeof(stat) - i);
+      p = (char *) &stat + i;
+      i += recv(s, p, sizeof(stat) - i, 0);
+    }
+
+  t = ntohl(stat.starttime);
+  printf("Num cons:    %d\n", (ntohl(stat.cons) - 1));
+  printf("Attemps:     %d\n", ntohl(stat.tries));
+  printf("Encrypted:   %s\n", stat.encrypted);
+  printf("Last try:    %s\n", stat.curpass);
+  printf("Start time:  %s", ctime(&t));
+  printf("Runtime:     %d\n\n", ntohl(stat.curtime) - ntohl(stat.starttime));
+
+  for (i = 0; i < MAXCONS; i++)
+    {
+      if (ntohl(stat.acons[i].socket) > 0 && ntohl(stat.acons[i].tries) > 0)
 	{
-	    showconn(stat.acons[i], ntohl(stat.curtime));
+	  showconn(stat.acons[i], ntohl(stat.curtime));
 	}
     }
 }
