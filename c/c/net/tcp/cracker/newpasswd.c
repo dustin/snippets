69e9c50a9e19a93d2e43cabb0933f41d6a520b76
Dustin Sallings <dustin@spy.net>
1997-01-10 13:58:44 +0000

    Added setter function
    Author: dustin
    Date: 1997/01/10 05:58:44
    
    imported from: dustin@spy.net--projects/snippets--c--1.0--patch-47

--- c/c/net/tcp/cracker/newpasswd.c
+++ c/c/net/tcp/cracker/newpasswd.c
@@ -1,21 +1,27 @@
 /*
  * This stuff was all written by James Lemley then stolen by me.
  *
- * $Id: newpasswd.c,v 1.2 1997/01/05 08:41:00 dustin Exp $
+ * $Id: newpasswd.c,v 1.3 1997/01/10 05:58:44 dustin Exp $
  */
 
 #include "cracker.h"
 
 extern struct global *glob;
 
+/*
+char keychars[] = "  abcdefghijklmnopqrstuvwxyz"
+                  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                  "0123456789"
+                  "{}[]~!@#$%^&*()_+`-=:;\"'|\\<,>.?/";
+*/
+
+#define keychars "  abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\
+0123456789{}[]~!@#$%^&*()_+`-=:;\"'|\\<,>.?/"
+
 char *newpasswd()
 {
 
     /* all characters that might be used in a password */
-    char keychars[] = "  abcdefghijklmnopqrstuvwxyz"
-                      "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                      "0123456789"
-                      "{}[]~!@#$%^&*()_+`-=:;\"'|\\<,>.?/";
     register int i;
 
         /* update the passmap */
@@ -36,4 +42,28 @@ char *newpasswd()
                 }
         }
         return (glob->password);
+}
+
+void set_pass(char *p)
+{
+        int i, j;
+        int len;
+
+        len = strlen(p);
+        if (len > 8)
+                len = 8;
+
+        for (i = 0; i < len; i++)
+        {
+                for (j = 0; j < sizeof(keychars); j++)
+                        if (p[i] == keychars[j])
+                                break;
+                glob->passmap[i] = j;
+                if (j == sizeof(keychars))
+                {
+                        printf("Can't restart with this password\n");
+                        exit(1);
+                }
+        }
+        strcpy(glob->password, p);
 }
