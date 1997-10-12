12bce6b395db5f90b77a866eb59c6ed2ab13d111
Dustin Sallings <dustin@spy.net>
1997-10-12 07:47:11 +0000

    SPAM database stuff.
    Author: dustin
    Date: 1997/10/11 23:47:11
       SPAM database stuff.
    
    imported from: dustin@spy.net--projects/snippets--sql--1.0--patch-3

---
+++ sql/sql/spam.sql
@@ -0,0 +1,41 @@
+-- SPAM Database stuff
+-- $Id: spam.sql,v 1.1 1997/10/11 23:47:11 dustin Exp $
+
+create sequence sid_seq increment 1;
+
+create table bill_info (
+    spam_id int default nextval('sid_seq'),
+    name varchar,
+    addr1 varchar,
+    addr2 varchar,
+    city varchar,
+    state char2,
+    zip char16,
+    country char2,
+    phone varchar,
+    fax varchar,
+    email varchar
+);
+
+create unique index bill_id on bill_info (spam_id);
+
+create table trans (
+    spam_id int,
+    amount money,
+    descr text,
+    ts timestamp default now()
+);
+
+create index trans_byspam on trans (spam_id);
+
+create table messages (
+    spam_id int,
+    subject text,
+    md5sum char(32),
+    size int,
+    thedate datetime,
+    ts timestamp default now()
+);
+
+create index mess_byspam on messages (spam_id);
+create index mess_bymd5 on messages (md5sum);
