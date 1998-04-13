9b3af00944293a4f87233edeea3d882994c95a51
Dustin Sallings <dustin@spy.net>
1998-04-13 14:08:46 +0000

    Oops, left out defaults and a function.
    Author: dustin
    Date: 1998/04/13 06:08:46
        Oops, left out defaults and a function.
    
    imported from: dustin@spy.net--projects/snippets--java--1.0--patch-7

--- java/java/servlet/photo/etc/photo.sql
+++ java/java/servlet/photo/etc/photo.sql
@@ -1,6 +1,6 @@
 -- Copyright (c) 1998  Dustin Sallings
 --
--- $Id: photo.sql,v 1.1 1998/04/12 03:14:18 dustin Exp $
+-- $Id: photo.sql,v 1.2 1998/04/13 06:08:46 dustin Exp $
 --
 -- Use this to bootstrap your SQL database to do cool shite with the
 -- photo album.
@@ -11,7 +11,7 @@ create table album(
     fn       varchar(32),
     keywords varchar(50),
     descr    text,
-    ts       timestamp,
+    ts       timestamp default now(),
     cat      int,
     taken    date,
     size     int
@@ -20,19 +20,19 @@ create table album(
 create unique index album_byoid on album(oid);
 grant all on album to nobody;
 
--- The categories
-
-create table cat(
-    id       int,
-    name     varchar
-);
-
-grant all on cat to nobody;
-
 -- A sequence for generating categories
 
 create sequence cat_seq;
 grant all on cat_seq to nobody;
+
+-- The categories
+
+create table cat(
+    id       int default nextval('cat_seq'),
+    name     varchar
+);
+
+grant all on cat to nobody;
 
 -- The ACLs for the categories
 
@@ -64,3 +64,10 @@ create table wwwusers(
 
 create index user_byname on wwwusers(username);
 grant all on wwwusers to nobody;
+
+-- A SQL function to return the count of elements in a category.
+
+create function catsum (int4 )
+    returns int4 AS
+	'select count(*) from album where cat = $1'
+    language 'SQL';
