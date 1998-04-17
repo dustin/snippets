f8a17a02c46ad11acf150dc3ac123a511eebf49f
Dustin Sallings <dustin@spy.net>
1998-04-17 13:58:24 +0000

    Fixed the test.
    Author: dustin
    Date: 1998/04/17 05:58:24
       Fixed the test.
    
    imported from: dustin@spy.net--projects/snippets--objc--1.0--patch-11

--- objc/objc/snppclient/snpp/snpp.m
+++ objc/objc/snppclient/snpp/snpp.m
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 1997  Dustin Sallings
  *
- * $Id: snpp.m,v 1.1 1998/03/17 04:04:56 dustin Exp $
+ * $Id: snpp.m,v 1.2 1998/04/17 05:58:24 dustin Exp $
  */
 
 #include <dString.h>
@@ -14,43 +14,9 @@
 
 void main(void)
 {
-    id socket=[[[dSocket alloc] init] listento: 8088];
-    id ns, req;
-    id string;
-
-    req=[[dRequest alloc] init];
-
-    for(;;)
-    {
-    ns=[socket accept];
-    if([ns getsocket]>=0)
-    {
-        puts("I got a connection!!!!");
-        string=[ns read];
-        [string print];
-        while([req parse :string]==0)
-        {
-	    [string free];
-            string=[ns read];
-	    [string print];
-        }
-    }
-
-    if([req verify]==0)
-    {
-	puts("Request was valid.");
-	puts("Doing showdoc");
-	[req showdoc :ns];
-	puts("Done with showdoc");
-    }
-    else
-    {
-	puts("Request was invalid.");
-    }
-
-    [req print];
-
-    puts("done");
-    [ns clear];
-    }
+    id snpp;
+    snpp=[[dSNPP alloc] init];
+    [snpp connectTo :"pager" :1031];
+    [snpp sendAPage :"dustin" thatsays:"Hey, what's up? (from objc snpp)"];
+    [snpp quit];
 }
--- objc/objc/snppclient/snpp/test.m
+++ objc/objc/snppclient/snpp/test.m
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 1998  Dustin Sallings
+ *
+ * $Id: test.m,v 1.3 1998/04/17 05:58:25 dustin Exp $
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <dSNPP.h>
@@ -5,11 +11,26 @@
 void main(int argc, char **argv)
 {
     id snpp;
+
+    if(argc<2)
+    {
+	printf("Not enough arguments (whom do you want to page today?)\n");
+	exit(0);
+    }
+
     snpp=[[dSNPP alloc] init];
-    [snpp connectTo :"pager" :1041];
-    [snpp sendAPage :"dustin" thatsays:"Hey, what's up? (from objc snpp)"];
+    [snpp connectTo :"pager" :1031];
+    if([snpp sendAPage :argv[1]
+               thatsays:"Hey, what's up? (from objc snpp)"]==0)
+	printf("Page was successful\n");
+    else
+	printf("Page failed\n");
     [snpp reset];
-    [snpp sendAPage :"dustin" thatsays:"Hey, what's up? (high, from objc snpp)"
-          priority:"high"];
+    if([snpp sendAPage :argv[1]
+	       thatsays:"Hey, what's up? (high, from objc snpp)"
+               priority:"high"]==0)
+	printf("Page was successful\n");
+    else
+	printf("Page failed\n");
     [snpp quit];
 }
