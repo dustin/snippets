48d73c146ae5a5219308c7504e59900543fce0c4
Dustin Sallings <dustin@spy.net>
1998-05-07 00:30:37 +0000

    Better documentation, variable sized blocks.
    Author: dustin
    Date: 1998/05/06 16:30:37
       Better documentation, variable sized blocks.
    
    imported from: dustin@spy.net--projects/snippets--perl--1.0--patch-64

--- perl/perl/misc/HMAC.pm
+++ perl/perl/misc/HMAC.pm
@@ -1,7 +1,7 @@
 #!/usr/local/bin/perl
 # Copyright (c) 1998 Dustin Sallings
 #
-# $Id: HMAC.pm,v 1.2 1998/05/06 08:23:13 dustin Exp $
+# $Id: HMAC.pm,v 1.3 1998/05/06 16:30:37 dustin Exp $
 
 package HMAC;
 
@@ -12,9 +12,11 @@ sub new
 {
     my $proto=shift;
     my $type=(shift||"MD5");
+    my $blocksize=(shift||64);
     my $self={};
 
-    $self->{type}=$type;
+    $self->{'type'}=$type;
+    $self->{'blocksize'}=$blocksize;
     bless($self);
     return($self);
 }
@@ -30,16 +32,18 @@ sub hexprint
 
 sub do_md5
 {
-    my($self, $text, $tlen, $key, $klen)=@_;
+    my($self, $text, $key)=@_;
     my($md5, @i, @o);
 
     $md5=MD5->new;
 
-    @i=map{$_^0x36}unpack("C$klen", $key); while(@i<64) { push(@i, 0x36) }
-    @o=map{$_^0x5c}unpack("C$klen", $key); while(@o<64) { push(@o, 0x5c) }
+    @i=map{$_^0x36}unpack("C*", $key);
+    while(@i<$self->{'blocksize'}) { push(@i, 0x36) }
+    @o=map{$_^0x5c}unpack("C*", $key);
+    while(@o<$self->{'blocksize'}) { push(@o, 0x5c) }
 
     return($md5->hash( pack("c*", @o, unpack("C16",
-		 $md5->hash(pack("c*",@i,unpack("C$tlen",$text)))))));
+		 $md5->hash(pack("c*",@i,unpack("C*",$text)))))));
 
 }
 
@@ -62,7 +66,7 @@ sub test
     my($self)=@_;
     my($h);
     print "Should print:\n0x750c783e6ab0b503eaa86e310a5db738\n";
-    $h=$self->do("what do ya want for nothing?", 28, "Jefe", 4);
+    $h=$self->do("what do ya want for nothing?", "Jefe");
     $h=$self->hexprint($h);
     print "0x$h\n";
 }
@@ -73,14 +77,33 @@ __END__
 
 =head1 NAME
 
-HMAC.pm - HMAC library following RFC 2104.
+HMAC.pm - Keyed-Hashing for Message Authentication
+
+=head1 SYNOPSIS
+
+   use HMAC;
+
+   $hmac=HMAC->new($hashtype, $blocksize);
+
+   $res=$hmac->do($text, $key);
+
+=head1 DESCRIPTION
+
+The B<HMAC> module provides a perl interface to the HMAC Keyed-Hashing for
+Message Authentication as described in RFC 2104.
 
 =head1 USAGE
 
-use HMAC;
+C<$hmac=HMAC-E<gt>new($hashtype, $blocksize);> will create a new hmac object
+using C<$hashtype> for the hashing algorithm and C<$blocksize> for the key
+block size.  Currently, only the MD5 hashing algorithm is supported.  Both the
+hashtype and the block size are optional, and the defaults of MD5 for the
+hash and 64 bytes for the block size will be used if no arguments are
+given.
 
-$hmac=HMAC->new('MD5');  # Returns a new HMAC_MD5 object;
+Once you create your object, you can get your results via the C<do> method,
+i.e.:  C<$res=$hmac-E<gt>do($text, $key);>.
 
-$res=$hmac->do($text, $tlen, $key, $klen); # do calculation
+=head1 AUTHOR
 
-=end
+This module was written by Dustin Sallings E<lt>dustin@spy.netE<gt>
