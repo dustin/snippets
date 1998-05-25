feb71cf366ff98d0c6ad2e2419c2b900277e0d9c
Dustin Sallings <dustin@spy.net>
1998-05-25 05:20:00 +0000

    Some thread stuff.
    Author: dustin
    Date: 1998/05/24 21:20:00
       Some thread stuff.
    
    imported from: dustin@spy.net--projects/snippets--c--1.0--patch-119

---
+++ c/c/threads/test.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define DBMUTEX 0
+#define NMUTEXEN 1
+
+pthread_mutex_t mutexen[NMUTEXEN];
+
+void dodbwork(char *query)
+{
+    int r;
+    printf("Attempting %s\n", query);
+    pthread_mutex_lock(&mutexen[DBMUTEX]);
+    printf("Doing query:  %s\n", query);
+    r=rand()%5;
+    printf("\tsleeping %d\n", r);
+    sleep(r);
+    pthread_mutex_unlock(&mutexen[DBMUTEX]);
+}
+
+int thread_main(int *n)
+{
+    int me, i;
+    char buffer[1024];
+
+    me=*n;
+    printf("Thread #%d initializing\n", me);
+
+    for(i=0; i<20; i++)
+    {
+        sleep(1);
+        sprintf(buffer, "I am thread %d", me);
+        dodbwork(buffer);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    pthread_t threads[8];
+    int       thread_n[8];
+    int i;
+
+    srand(time(NULL));
+
+    pthread_mutex_init(&mutexen[0], NULL);
+
+    for(i=0; i<8; i++)
+    {
+        thread_n[i]=i;
+        pthread_create(&threads[i], NULL, thread_main, &thread_n[i]);
+    }
+    for(i=0; i<8; i++)
+        pthread_join(threads[i], NULL);
+}
