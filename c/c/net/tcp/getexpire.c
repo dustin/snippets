becafefca56f596995579a9f6de535d498432751
Dustin Sallings <dustin@spy.net>
1998-09-02 16:05:21 +0000

    Ran through indent.
    Author: dustin
    Date: 1998/09/02 08:05:21
    
    imported from: dustin@spy.net--projects/snippets--c--1.0--patch-145

--- c/c/net/tcp/getexpire.c
+++ c/c/net/tcp/getexpire.c
@@ -2,7 +2,7 @@
  * Check Webserver Status
  * Copyright (c) 1997 SPY Internetworking
  *
- * $Id: getexpire.c,v 1.1 1998/09/02 08:02:43 dustin Exp $
+ * $Id: getexpire.c,v 1.2 1998/09/02 08:05:21 dustin Exp $
  * $Source: /Users/dustin/stuff/cvstest/c/net/tcp/getexpire.c,v $
  *
  */
@@ -19,7 +19,7 @@
 #include <assert.h>
 #include <ctype.h>
 
-#include "rsa.h"       /* SSLeay stuff */
+#include "rsa.h"		/* SSLeay stuff */
 #include "crypto.h"
 #include "x509.h"
 #include "pem.h"
@@ -30,7 +30,6 @@
 #define CHK_ERR(err,s) if ((err)==-1) { perror(s); exit(1); }
 #define CHK_SSL(err) if ((err)==-1) { ERR_print_errors_fp(stderr); exit(2); }
 
-
 /*
  * It was this or a global variable.  It'll probably change in the future
  */
@@ -40,22 +39,22 @@
  * URL request holder.
  */
 struct url {
-    char *host;
-    int port;
-    char *req;
+	char   *host;
+	int     port;
+	char   *req;
 };
 
 /*
  * Status return.
  */
 struct status {
-    int status;
-    int bytesread;
-    char *message;
+	int     status;
+	int     bytesread;
+	char   *message;
 };
 
-SSL*     ssl;
-SSL_CTX* ctx;
+SSL    *ssl;
+SSL_CTX *ctx;
 
 /* Kill Whitey(tm) */
 #define iswhitey(a) (a=='\n' || a=='\r')
@@ -64,91 +63,95 @@ SSL_CTX* ctx;
  * Open and return a tcp socket to host:port, -1 if it fails.
  */
 
-int openhost(char *host, int port)
+int
+openhost(char *host, int port)
 {
-  struct hostent *hp;
-  register int s;
-  struct sockaddr_in sin;
-  int err;
-  X509*    server_cert;
-  char*    str;
+	struct hostent *hp;
+	register int s;
+	struct sockaddr_in sin;
+	int     err;
+	X509   *server_cert;
+	char   *str;
 
-  if ((hp = gethostbyname(host)) == NULL)
-    {
-      printf("ERR: gethostbyname\n");
-      return(-1);
-    }
+	if ((hp = gethostbyname(host)) == NULL) {
+		printf("ERR: gethostbyname\n");
+		return (-1);
+	}
+	if ((s = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("socket");
+		return (-1);
+	}
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(port);
+	bcopy(hp->h_addr, &sin.sin_addr, hp->h_length);
 
-  if ((s = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-      perror("socket");
-      return(-1);
-    }
+	alarm(30);
+	if (connect(s, (struct sockaddr *) &sin, sizeof(sin)) < 0)
+		return (-1);
+	alarm(0);
 
-  sin.sin_family = AF_INET;
-  sin.sin_port = htons(port);
-  bcopy(hp->h_addr, &sin.sin_addr, hp->h_length);
+	SSLeay_add_ssl_algorithms();
+	SSL_load_error_strings();
+	/* ctx = SSL_CTX_new (SSLv23_method());                     CHK_NULL(ctx);
+	 */
+	ctx = SSL_CTX_new(SSLv2_method());
+	CHK_NULL(ctx);
 
-  alarm(30);
-  if(connect(s, (struct sockaddr *) &sin, sizeof(sin)) < 0)
-      return(-1);
-  alarm(0);
+	ssl = SSL_new(ctx);
+	CHK_NULL(ssl);
+	SSL_set_fd(ssl, s);
+	err = SSL_connect(ssl);
+	CHK_SSL(err);
 
-  SSLeay_add_ssl_algorithms();
-  SSL_load_error_strings();
-  /* ctx = SSL_CTX_new (SSLv23_method());                     CHK_NULL(ctx);
-  */
-  ctx = SSL_CTX_new (SSLv2_method());                     CHK_NULL(ctx);
+	server_cert = SSL_get_peer_certificate(ssl);
+	CHK_NULL(server_cert);
+	printf("%s:%d:%s\n", host, port,
+	    server_cert->cert_info->validity->notAfter->data);
 
-  ssl = SSL_new (ctx);                         CHK_NULL(ssl);
-  SSL_set_fd (ssl, s);
-  err = SSL_connect (ssl);                     CHK_SSL(err);
+	X509_free(server_cert);
 
-  server_cert = SSL_get_peer_certificate (ssl); CHK_NULL(server_cert);
-  printf ("%s:%d:%s\n", host, port,
-          server_cert->cert_info->validity->notAfter->data);
-
-  X509_free (server_cert);
-
-  return (s);
+	return (s);
 }
 
-void doit(char *file)
+void
+doit(char *file)
 {
-    char buf[1024];
-    FILE *f;
-    int s, i, port;
-    char host[1024];
+	char    buf[1024];
+	FILE   *f;
+	int     s, i, port;
+	char    host[1024];
 
-    f=fopen(file, "r");
-    assert(f);
+	f = fopen(file, "r");
+	assert(f);
 
-    for(;;) {
-	fgets(buf, 1024, f);
-	if(feof(f)) break;
+	for (;;) {
+		fgets(buf, 1024, f);
+		if (feof(f))
+			break;
 
-	sscanf(buf, "%s %d", host, &port);
+		sscanf(buf, "%s %d", host, &port);
 
-        s=openhost(host, port);
-        if(s>0) {
-            close(s);
-            SSL_free (ssl);
-            SSL_CTX_free (ctx);
-        }
-    }
+		s = openhost(host, port);
+		if (s > 0) {
+			close(s);
+			SSL_free(ssl);
+			SSL_CTX_free(ctx);
+		}
+	}
 
-    return;
+	return;
 }
 
-void main(int argc, char **argv)
+void
+main(int argc, char **argv)
 {
-    if(argc < 2)
-    {
-	printf("%s, copyright (c) 1997  Dustin Sallings\n"
-	    "$Id: getexpire.c,v 1.1 1998/09/02 08:02:43 dustin Exp $\n", argv[0]);
-	printf("Error, arguments required.  Usage:\n%s file\n",
-	       argv[0]);
-	exit(0);
-    }
-    doit(argv[1]);
+	if (argc < 2) {
+		printf("%s, copyright (c) 1997  Dustin Sallings\n"
+		    "$Id: getexpire.c,v 1.2 1998/09/02 08:05:21 dustin Exp $\n",
+			 argv[0]);
+		printf("Error, arguments required.  Usage:\n%s file\n",
+		    argv[0]);
+		exit(0);
+	}
+	doit(argv[1]);
 }
--- c/c/net/tcp/sslcat.c
+++ c/c/net/tcp/sslcat.c
@@ -2,7 +2,7 @@
  * Check Webserver Status
  * Copyright (c) 1997 SPY Internetworking
  *
- * $Id: sslcat.c,v 1.1 1998/06/04 16:25:38 dustin Exp $
+ * $Id: sslcat.c,v 1.2 1998/09/02 08:05:24 dustin Exp $
  * $Source: /Users/dustin/stuff/cvstest/c/net/tcp/sslcat.c,v $
  *
  */
@@ -19,7 +19,7 @@
 #include <assert.h>
 #include <ctype.h>
 
-#include "rsa.h"       /* SSLeay stuff */
+#include "rsa.h"		/* SSLeay stuff */
 #include "crypto.h"
 #include "x509.h"
 #include "pem.h"
@@ -30,7 +30,6 @@
 #define CHK_ERR(err,s) if ((err)==-1) { perror(s); exit(1); }
 #define CHK_SSL(err) if ((err)==-1) { ERR_print_errors_fp(stderr); exit(2); }
 
-
 /*
  * It was this or a global variable.  It'll probably change in the future
  */
@@ -40,22 +39,22 @@
  * URL request holder.
  */
 struct url {
-    char *host;
-    int port;
-    char *req;
+	char   *host;
+	int     port;
+	char   *req;
 };
 
 /*
  * Status return.
  */
 struct status {
-    int status;
-    int bytesread;
-    char *message;
+	int     status;
+	int     bytesread;
+	char   *message;
 };
 
-SSL*     ssl;
-SSL_CTX* ctx;
+SSL    *ssl;
+SSL_CTX *ctx;
 
 /* Kill Whitey(tm) */
 #define iswhitey(a) (a=='\n' || a=='\r')
@@ -64,114 +63,113 @@ SSL_CTX* ctx;
  * Open and return a tcp socket to host:port, -1 if it fails.
  */
 
-int openhost(char *host, int port)
+int
+openhost(char *host, int port)
 {
-  struct hostent *hp;
-  register int s;
-  struct sockaddr_in sin;
-  int err;
-  X509*    server_cert;
-  char*    str;
+	struct hostent *hp;
+	register int s;
+	struct sockaddr_in sin;
+	int     err;
+	X509   *server_cert;
+	char   *str;
 
-  if ((hp = gethostbyname(host)) == NULL)
-    {
-      printf("ERR: gethostbyname\n");
-      return(-1);
-    }
+	if ((hp = gethostbyname(host)) == NULL) {
+		printf("ERR: gethostbyname\n");
+		return (-1);
+	}
+	if ((s = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("socket");
+		return (-1);
+	}
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(port);
+	bcopy(hp->h_addr, &sin.sin_addr, hp->h_length);
 
-  if ((s = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-      perror("socket");
-      return(-1);
-    }
+	alarm(30);
+	if (connect(s, (struct sockaddr *) &sin, sizeof(sin)) < 0)
+		return (-1);
+	alarm(0);
 
-  sin.sin_family = AF_INET;
-  sin.sin_port = htons(port);
-  bcopy(hp->h_addr, &sin.sin_addr, hp->h_length);
+	SSLeay_add_ssl_algorithms();
+	SSL_load_error_strings();
+	/* ctx = SSL_CTX_new (SSLv23_method());                     CHK_NULL(ctx);
+	 */
+	ctx = SSL_CTX_new(SSLv2_method());
+	CHK_NULL(ctx);
 
-  alarm(30);
-  if(connect(s, (struct sockaddr *) &sin, sizeof(sin)) < 0)
-      return(-1);
-  alarm(0);
+	ssl = SSL_new(ctx);
+	CHK_NULL(ssl);
+	SSL_set_fd(ssl, s);
+	err = SSL_connect(ssl);
+	CHK_SSL(err);
 
-  SSLeay_add_ssl_algorithms();
-  SSL_load_error_strings();
-  /* ctx = SSL_CTX_new (SSLv23_method());                     CHK_NULL(ctx);
-  */
-  ctx = SSL_CTX_new (SSLv2_method());                     CHK_NULL(ctx);
+	printf("SSL connection using %s\n", SSL_get_cipher(ssl));
 
-  ssl = SSL_new (ctx);                         CHK_NULL(ssl);
-  SSL_set_fd (ssl, s);
-  err = SSL_connect (ssl);                     CHK_SSL(err);
+	server_cert = SSL_get_peer_certificate(ssl);
+	CHK_NULL(server_cert);
+	printf("Server certificate:\n");
 
-  printf ("SSL connection using %s\n", SSL_get_cipher (ssl));
+	str = X509_NAME_oneline(X509_get_subject_name(server_cert), NULL, 0);
+	CHK_NULL(str);
+	printf("\t subject: %s\n", str);
+	Free(str);
 
-  server_cert = SSL_get_peer_certificate (ssl); CHK_NULL(server_cert);
-  printf ("Server certificate:\n");
+	str = X509_NAME_oneline(X509_get_issuer_name(server_cert), NULL, 0);
+	CHK_NULL(str);
+	printf("\t issuer: %s\n", str);
+	Free(str);
 
-  str = X509_NAME_oneline (X509_get_subject_name (server_cert), NULL, 0);
-  CHK_NULL(str);
-  printf ("\t subject: %s\n", str);
-  Free (str);
+	X509_free(server_cert);
 
-  str = X509_NAME_oneline (X509_get_issuer_name  (server_cert), NULL, 0);
-  CHK_NULL(str);
-  printf ("\t issuer: %s\n", str);
-  Free (str);
-
-  X509_free (server_cert);
-
-  return (s);
+	return (s);
 }
 
-void doit(char *host, int port, char *inputfile)
+void
+doit(char *host, int port, char *inputfile)
 {
-    char buf[1024];
-    FILE *f;
-    int s, i;
+	char    buf[1024];
+	FILE   *f;
+	int     s, i;
 
-    s=openhost(host, port);
+	s = openhost(host, port);
 
-    if(s<0)
-    {
-       printf("Couldn't connect.\n");
-       return;
-    }
+	if (s < 0) {
+		printf("Couldn't connect.\n");
+		return;
+	}
+	f = fopen(inputfile, "r");
+	assert(f);
 
-    f=fopen(inputfile, "r");
-    assert(f);
-
-    for(;;)
-    {
-	fgets(buf, 1024, f);
-	if(feof(f)) break;
+	for (;;) {
+		fgets(buf, 1024, f);
+		if (feof(f))
+			break;
+		SSL_write(ssl, buf, strlen(buf));
+	}
 	SSL_write(ssl, buf, strlen(buf));
-    }
-    SSL_write(ssl, buf, strlen(buf));
 
-    /* Eat the rest of the page */
-    while(i=SSL_read(ssl, buf, 1024))
-    {
-	buf[i]=NULL;
-	printf(buf);
-	fflush(stdout);
-    }
+	/* Eat the rest of the page */
+	while (i = SSL_read(ssl, buf, 1024)) {
+		buf[i] = NULL;
+		printf(buf);
+		fflush(stdout);
+	}
 
-    close(s);
-    SSL_free (ssl);
-    SSL_CTX_free (ctx);
-    return;
+	close(s);
+	SSL_free(ssl);
+	SSL_CTX_free(ctx);
+	return;
 }
 
-void main(int argc, char **argv)
+void
+main(int argc, char **argv)
 {
-    if(argc < 4)
-    {
-	printf("sslcat, copyright (c) 1997  Dustin Sallings\n"
-	    "$Id: sslcat.c,v 1.1 1998/06/04 16:25:38 dustin Exp $\n");
-	printf("Error, arguments required.  Usage:\n%s host port filename\n",
-	       argv[0]);
-	exit(0);
-    }
-    doit(argv[1], atoi(argv[2]), argv[3]);
+	if (argc < 4) {
+		printf("sslcat, copyright (c) 1997  Dustin Sallings\n"
+		    "$Id: sslcat.c,v 1.2 1998/09/02 08:05:24 dustin Exp $\n");
+		printf("Error, arguments required.  Usage:\n%s host port filename\n",
+		    argv[0]);
+		exit(0);
+	}
+	doit(argv[1], atoi(argv[2]), argv[3]);
 }
