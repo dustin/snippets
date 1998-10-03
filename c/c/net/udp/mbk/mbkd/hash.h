31701ebd6cab4d9693d768596c8f6e07bf2e4c5d
Dustin Sallings <dustin@spy.net>
1998-10-03 16:20:17 +0000

    Ran through indent.
    Author: dustin
    Date: 1998/10/03 08:20:17
    
    imported from: dustin@spy.net--projects/snippets--c--1.0--patch-159

--- c/c/net/udp/mbk/mbkd/hash.h
+++ c/c/net/udp/mbk/mbkd/hash.h
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 1998  Dustin Sallings
  *
- * $Id: hash.h,v 1.2 1998/10/01 18:05:14 dustin Exp $
+ * $Id: hash.h,v 1.3 1998/10/03 08:20:19 dustin Exp $
  */
 
 #ifndef HASH_H
@@ -15,23 +15,23 @@
 #include <mbkd.h>
 
 struct hash_container {
-    char *name;
-    void *value;
-    struct hash_container *next;
+	char   *name;
+	void   *value;
+	struct hash_container *next;
 };
 
 struct hashtable {
-    int hashsize;
-    struct hash_container **buckets;
+	int     hashsize;
+	struct hash_container **buckets;
 };
 
-int _do_hash(struct hashtable *hash, char *s);
+int     _do_hash(struct hashtable *hash, char *s);
 struct hashtable *hash_init(int size);
 struct hash_container *hash_store(struct hashtable *hash,
-                                  char *key, void *data);
+    char *key, void *data);
 struct hash_container *hash_find(struct hashtable *hash, char *key);
-void hash_delete(struct hashtable *hash, char *key);
-void hash_destroy(struct hashtable *hash);
-void _hash_dump(struct hashtable *hash);
+void    hash_delete(struct hashtable *hash, char *key);
+void    hash_destroy(struct hashtable *hash);
+void    _hash_dump(struct hashtable *hash);
 
 #endif /* HASH_H */
