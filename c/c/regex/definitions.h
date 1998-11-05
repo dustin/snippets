67c61c0f16e3acb1b69e1496f961af630c30626b
Dustin Sallings <dustin@spy.net>
1998-11-05 16:54:00 +0000

    misc defines
    Author: dustin
    Date: 1998/11/05 08:54:00
    
    imported from: dustin@spy.net--projects/snippets--c--1.0--patch-179

---
+++ c/c/regex/definitions.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 1998 Beyond.com
+ * Written by Dustin Sallings
+ *
+ * $Id: definitions.h,v 1.1 1998/11/05 08:54:00 dustin Exp $
+ */
+
+#ifndef _DEFINITIONS_H
+#define _DEFINITIONS_H 1
+
+/* fields in prods.txt */
+
+#define NAMESKU   0
+#define PUBSKU    1
+#define NSKU      2
+#define DSKU      3
+#define MEDIASKU  4
+#define PLATSKU   5
+#define CATSKU    6
+#define PRICESKU  7
+#define STORESKU  8
+#define INVSKU    9
+#define SKU       10
+#define CATTXTSKU 11
+#define MPARTSKU  12
+#define NEWSKU    13
+#define NOTYETSKU 14
+#define REBATESKU 15
+#define KEYSKU    16
+
+#endif
