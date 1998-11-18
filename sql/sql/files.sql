7eb7a5360025f72fc5d3606ae64a005f17d133cf
Dustin Sallings <dustin@spy.net>
1998-11-18 03:27:51 +0000

    Mo' betta
    Author: dustin
    Date: 1998/11/17 19:27:51
    
    imported from: dustin@spy.net--projects/snippets--sql--1.0--patch-9

--- sql/sql/files.sql
+++ sql/sql/files.sql
@@ -1,4 +1,4 @@
--- $Id: files.sql,v 1.1 1998/11/17 10:22:39 dustin Exp $
+-- $Id: files.sql,v 1.2 1998/11/17 19:27:51 dustin Exp $
 --
 -- System file comparison database
 
@@ -25,6 +25,7 @@ drop table filetmp;
 -- index the hell out of it
 create index files_blocks on files(blocks);
 create index files_sum on files using hash(sum);
+create index files_sumblocks on files(sum,blocks);
 create unique index files_filename on files (filename);
 
 vacuum verbose analyze;
@@ -34,11 +35,12 @@ select sum, blocks, count(*) into table sumcount
 	from files
 	where blocks>0
 	group by sum, blocks
+	having count(*) > 1
 ;
 
-delete from sumcount where count=1;
+-- delete from sumcount where count=1;
 
--- vacuum again, clear up the deletes...
+-- vacuum again, just because
 vacuum verbose analyze;
 
 create view wasted as
