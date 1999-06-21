12df6aec918fb421728f22d0a9123cd82ca6bb66
Dustin Sallings <dustin@spy.net>
1999-06-21 15:50:34 +0000

    Image getter.
    Author: dustin
    Date: 1999/06/21 07:50:34
    
    imported from: dustin@spy.net--projects/snippets--java--1.0--patch-58

---
+++ java/java/GetImage.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.sql.*;
+import java.text.*;
+import sun.misc.*;
+
+public class GetImage
+{
+
+	static Connection photo;
+	static Statement st;
+
+	public static void main(String args[])
+		throws SQLException, ClassNotFoundException, IOException
+	{
+		String source;
+
+		// Load the postgres driver.
+		Class.forName("postgresql.Driver");
+
+		source="jdbc:postgresql://bleu/photo";
+
+		photo = DriverManager.getConnection(source, "dustin", "");
+		st = photo.createStatement();
+
+		getit(505);
+	}
+
+	private static void getit(int which) throws SQLException, IOException {
+		String query;
+		BASE64Decoder base64 = new BASE64Decoder();
+
+		query = "select * from image_store where id = " + which +
+			" order by line";
+
+		ResultSet rs = st.executeQuery(query);
+		while(rs.next()) {
+			byte data[];
+			data=base64.decodeBuffer(rs.getString(3));
+			System.out.write(data);
+		}
+	}
+}
