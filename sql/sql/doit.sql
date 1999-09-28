6293b7f87c97d02371678291b1dd4b999700133f
Dustin Sallings <dustin@spy.net>
1999-09-28 05:14:40 +0000

    More modern stuff.
    Author: dustin
    Date: 1999/09/27 21:14:40
    
    imported from: dustin@spy.net--projects/snippets--sql--1.0--patch-10

--- sql/sql/doit.sql
+++ sql/sql/doit.sql
@@ -1,6 +1,6 @@
 -- Copyright (c) 1997  Dustin Sallings
 --
--- $Id: doit.sql,v 1.2 1997/10/06 15:53:40 dustin Exp $
+-- $Id: doit.sql,v 1.3 1999/09/27 21:14:40 dustin Exp $
 --
 -- SQL routines for radius log management.
 --
@@ -9,16 +9,23 @@
 -- Create the table we're going to load the radius data into.
 --
 
+-- begin transaction;
+
 create table radius (
-          stop datetime,
-          username char16,
-          sessid char16,
+          stop datetime not null,
+          username char(16),
+          sessid char(16),
           sesstime int,
           inoct int,
           outoct int,
-          datarate int,
-          addr char16
+		  status_type char(8),
+          addr char(16),
+		  called_station char(16),
+		  calling_station char(16),
+		  authenticator char(16),
+		  terminate_cause char(16)
       );
+
 
 --
 -- Load up the tab delimited radius file from my parser.
@@ -27,20 +34,23 @@ create table radius (
 copy radius from '/tmp/input';
 
 --
--- Get rid of some junk data.
+-- Clean up
 --
 
 delete from radius where username='';
 delete from radius where sesstime=0;
-delete from radius where username ~~ '[%]'::text;
+
+-- make sure all of the usernames are the same case
+update radius set username = lower(username);
+-- delete from radius where username ~~ '[%]'::text;
 
 --
 -- I got this function from the keen list.  :)  It makes a conversion
 -- from int4 to timespan.
 --
 
-create function reltime_timespan(int4) returns timespan as '-'
-    language 'internal';
+-- create function reltime_timespan(int4) returns timespan as '-'
+--     language 'internal';
 
 --
 -- Make room for the start times.
@@ -52,7 +62,8 @@ alter table radius add column start datetime;
 -- Set all of the start times.
 --
 
-update radius set start=(stop-reltime_timespan(sesstime));
+-- update radius set start=(stop-reltime_timespan(sesstime));
+update radius set start=(stop-timespan(sesstime));
 
 --
 -- Create an index on the username so we won't have to scan the *whole*
@@ -68,7 +79,7 @@ create index rad_stop on radius using btree (stop);
 -- (this will take a while if there's a lot of data)
 --
 
--- vacuum verbose analyze;
+vacuum verbose analyze;
 
 --
 -- Create a table for general statistics on users.
@@ -80,10 +91,17 @@ select distinct username into table tmp1 from radius;
 -- Add the columns to hold the statistics.
 --
 
-alter table tmp1 add column totaltime int;
-alter table tmp1 add column avgtime int;
+alter table tmp1 add column times int;
+alter table tmp1 add column totaltime timespan;
+alter table tmp1 add column avgtime timespan;
 alter table tmp1 add column avgbpsin int;
 alter table tmp1 add column avgbpsout int;
+
+-- find out how many times they dialed up
+
+create function numtimes(text) returns int4 as
+	'select count(*) from radius where username= $1'
+	language 'sql';
 
 --
 -- This functions returns the total time the user was online for all of
@@ -91,23 +109,23 @@ alter table tmp1 add column avgbpsout int;
 -- taking a date range, but it was fun to play with.  :)
 --
 
-create function ttime(char16) returns int4 as
-       'select sum(sesstime) from radius where username= $1'
+create function ttime(text) returns timespan as
+       'select timespan(sum(sesstime)) from radius where username= $1'
        language 'sql';
 
 --
 -- This is their average session time.
 --
 
-create function atime(char16) returns int4 as
-       'select avg(sesstime) from radius where username= $1'
+create function atime(text) returns timespan as
+       'select timespan(avg(sesstime)) from radius where username= $1'
        language 'sql';
 
 --
 -- This is the average bits in per second.
 --
 
-create function avgoctin(char16) returns int4 as
+create function avgoctin(text) returns int4 as
     'select sum(inoct)/sum(sesstime) from radius where username= $1'
     language 'sql';
 
@@ -115,7 +133,7 @@ create function avgoctin(char16) returns int4 as
 -- This is the average bits out per second.
 --
 
-create function avgoctout(char16) returns int4 as
+create function avgoctout(text) returns int4 as
     'select sum(outoct)/sum(sesstime) from radius where username= $1'
     language 'sql';
 
@@ -123,8 +141,9 @@ create function avgoctout(char16) returns int4 as
 -- This sets the totals and averages for each user in the tmp1 table.
 --
 
-update tmp1 set totaltime=ttime(username), avgtime=atime(username),
-                avgbpsin=avgoctin(username), avgbpsout=avgoctout(username);
+update tmp1 set times=numtimes(username), totaltime=ttime(username),
+				avgtime=atime(username), avgbpsin=avgoctin(username),
+				avgbpsout=avgoctout(username);
 
 --
 -- Go ahead and slap an index on it for safe keeping.
@@ -171,3 +190,5 @@ create view duplicates as
      from radius a, radius b
      where a.username=b.username and
      a.start>b.start and a.start<=b.stop;
+
+-- commit;
