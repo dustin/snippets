946a101cd9988bfc4031bc450d695f39cd91fb40
Dustin Sallings <dustin@spy.net>
1999-11-24 16:50:42 +0000

    ImageData added and working.
    Author: dustin
    Date: 1999/11/24 08:50:42
    
    imported from: dustin@spy.net--projects/snippets--java--1.0--patch-154

--- java/java/spytools/net/spy/util/ImageData.java
+++ java/java/spytools/net/spy/util/ImageData.java
@@ -1,6 +1,9 @@
 // Copyright (c) 1999  Dustin Sallings <dustin@spy.net>
 //
-// $Id: ImageData.java,v 1.1 1999/11/24 08:49:34 dustin Exp $
+// $Id: ImageData.java,v 1.2 1999/11/24 08:50:43 dustin Exp $
+
+import java.lang.*;
+import java.util.*;
 
 public class ImageData {
 	// Meta stuff
