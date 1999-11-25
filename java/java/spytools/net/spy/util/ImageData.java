7b42e130e8f3c6cdf933c4669020e4b93b083416
Dustin Sallings <dustin@spy.net>
1999-11-25 17:37:00 +0000

    This is a usable object, now.
    Author: dustin
    Date: 1999/11/25 09:37:00
    
    imported from: dustin@spy.net--projects/snippets--java--1.0--patch-158

--- java/java/spytools/net/spy/util/ImageData.java
+++ java/java/spytools/net/spy/util/ImageData.java
@@ -1,17 +1,29 @@
 // Copyright (c) 1999  Dustin Sallings <dustin@spy.net>
 //
-// $Id: ImageData.java,v 1.2 1999/11/24 08:50:43 dustin Exp $
+// $Id: ImageData.java,v 1.3 1999/11/25 09:37:00 dustin Exp $
+
+package net.spy.util;
 
 import java.lang.*;
 import java.util.*;
+import java.io.Serializable;
 
-public class ImageData {
+public class ImageData extends Object implements Serializable {
 	// Meta stuff
-	int format_version=1;
-	boolean thumbnail=false;
+	public int format_version=1;
+	public boolean thumbnail=false;
 
 	// Image data.
-	Vector image_data = null;
-	int image_width = 0;
-	int image_height = 0;
+	public Vector image_data = null;
+	public int image_width = 0;
+	public int image_height = 0;
+
+	public ImageData() {
+		super();
+	}
+
+	public ImageData(Vector v) {
+		super();
+		image_data = v;
+	}
 }
