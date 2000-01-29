c173a93db1d5bf01419946243160101cd7439140
Dustin Sallings <dustin@spy.net>
2000-01-29 10:54:10 +0000

    Gotta have that watch dog.
    Author: dustin
    Date: 2000/01/29 02:54:10
    
    imported from: dustin@spy.net--projects/snippets--java--1.0--patch-213

---
+++ java/java/spytools/net/spy/WatchDog.java
@@ -0,0 +1,38 @@
+// Copyright (c) 1999 Dustin Sallings
+//
+// $Id: WatchDog.java,v 1.1 2000/01/29 02:54:12 dustin Exp $
+
+package net.spy;
+
+import java.lang.*;
+
+/**
+ * A simple watchdog timer.
+ * <p>
+ * *NOTE* this will cause entire JVM termination.  Use it where it makes
+ * sense.
+ */
+
+public class WatchDog extends Thread {
+	long lifetime=0;
+
+	/**
+	 * Constructs a WatchDog timer that will last for the given number of
+	 * milliseconds.
+	 */
+	public WatchDog(long lifetime) {
+		super();
+		this.lifetime=lifetime;
+		this.setDaemon(true);
+	}
+
+	public void run() {
+		try {
+			sleep(lifetime);
+		} catch(Exception e) {
+			// We don't care yet.
+		}
+		System.err.println("Watchdog timer expired!!!");
+		System.exit(-1);
+	}
+}
