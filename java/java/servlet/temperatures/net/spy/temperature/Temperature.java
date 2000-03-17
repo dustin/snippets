b0b9e68e18ac4ba01a9fe72ff447714bd4019bd2
Dustin Sallings <dustin@spy.net>
2000-03-17 14:14:55 +0000

    Moved the thermometer stuff into the temperature stuff....
    Author: dustin
    Date: 2000/03/17 06:14:55
    
    Moved the thermometer stuff into the temperature stuff.  Yeah, it makes
    it bigger and all that, but it should be more efficient.
    
    imported from: dustin@spy.net--projects/snippets--java--1.0--patch-223

--- java/java/servlet/temperatures/net/spy/temperature/Temperature.java
+++ java/java/servlet/temperatures/net/spy/temperature/Temperature.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 1999 Dustin Sallings
  *
- * $Id: Temperature.java,v 1.3 2000/03/16 06:20:10 dustin Exp $
+ * $Id: Temperature.java,v 1.4 2000/03/17 06:14:55 dustin Exp $
  */
 
 package net.spy.temperature;
@@ -17,10 +17,21 @@ import javax.servlet.http.*;
 import net.spy.*;
 import net.spy.net.*;
 
+import java.awt.*;
+import java.awt.image.*;
+
+import com.mongus.servlet.GifServlet;
+
 // The class
-public class Temperature extends HttpServlet
+public class Temperature extends GifServlet implements ImageObserver
 {
 	Properties temps = null;
+
+	// Image stuff.
+	static Image baseImage=null;
+	Color black=null;
+	Color white=null;
+	Font font=null;
 
 	// The once only init thingy.
 	public void init(ServletConfig config) throws ServletException {
@@ -34,6 +45,21 @@ public class Temperature extends HttpServlet
 			throw new ServletException("Error loading properties:  " + e);
 		}
 
+		log("Loaded the following:\n" + listTemps());
+
+		try {
+			log("Getting the base image.");
+			getBaseImage();
+			log("Got the base image.");
+		} catch(Exception e) {
+			throw new ServletException("Error getting base image:  " + e);
+		}
+
+		// Initializing image stuff.
+		black=new Color(0, 0, 0);
+		white=new Color(255, 255, 255);
+		font=new Font("SanSerif", Font.PLAIN, 10);
+
 	}
 
 	// Do a GET request
@@ -41,14 +67,31 @@ public class Temperature extends HttpServlet
 		HttpServletRequest request, HttpServletResponse response
 	) throws ServletException {
 		String which=request.getParameter("temp");
+		String therm=request.getParameter("therm");
 		String out="";
 		if(which==null) {
-			out=listTemps();
+			// If there's no which, check for a therm
+			if(therm!=null) {
+				// Make sure the temperature updates keep coming back...
+				long l=new java.util.Date().getTime();
+				l+=300000;
+				response.setDateHeader("Expires", l);
+				// Show the graphical representation of the temperature
+				try {
+					writeGif(response, getTherm(therm));
+				} catch(Exception e) {
+					throw new ServletException("Error sending gif:  " + e);
+				}
+			} else {
+				// If there's no therm, and no out, list the temps
+				out=listTemps();
+				send_response(response, out);
+			}
 		} else {
+			// Show the non-graphical representation of the temperature
 			out=getTemp(which);
+			send_response(response, out);
 		}
-
-		send_response(response, out);
 	}
 
 	protected String listTemps() {
@@ -75,6 +118,7 @@ public class Temperature extends HttpServlet
 			t=Double.valueOf(s).doubleValue();
 			temptmp=(int)(t*100.0);
 			t=(double)temptmp/100;
+			log("Fetching " + which + " from " + url);
 		} catch(Exception e) {
 			throw new ServletException("Error getting temperature:  " + e);
 		}
@@ -90,5 +134,78 @@ public class Temperature extends HttpServlet
 		} catch(Exception e) {
 			// Ignore.
 		}
+	}
+
+	// Graphical representation of the image.
+	protected Image getTherm(String which) throws ServletException {
+		int width=133;
+		int height=132;
+
+		// Stuff we need to calculate the temperature
+		double x2, y2;
+		int trans;
+		double rad, angle;
+		double temp=getTempD(which);
+
+		// Create an image the size we want
+		Image image = createImage(width, height);
+
+		// Get a graphics object to draw on the image
+		Graphics g = image.getGraphics();
+		// Prepare the drawing stuff.
+		g.setFont(font);
+		g.setColor(black);
+
+		// Draw the base thermometer
+		g.drawImage(baseImage, 0, 0, this);
+
+		// Translate the angle because we're a little crooked
+		trans=-90;
+
+		// Calculate the angle based on the temperature
+		angle=(temp*1.8)+trans;
+		// Calculate the angle in radians
+		rad=( (angle/360) * 2 * 3.14159265358979d );
+		// Find the extra points
+		x2=Math.sin(rad)*39;
+		y2=Math.cos(rad)*39;
+		// Negate the y, we're upside-down
+		y2=-y2;
+		// Move over to the starting points.
+		x2+=66; y2+=65;
+		// Draw the line.
+		g.drawLine(66, 65,  (int)x2, (int)y2);
+		// Draw the temperature
+		g.drawString("" + temp, 52, 82);
+
+		return(image);
+	}
+
+	// Get the temperature as a double (For the image)
+	protected double getTempD(String which) throws ServletException {
+		double t = 0.0;
+		// Get the temperature, make it a double.
+		String s=getTemp(which) + "d";
+		t=Double.valueOf(s).doubleValue();
+		return(t);
+	}
+
+	// Get the base image loaded
+	protected Image getBaseImage() throws Exception {
+		if(baseImage==null) {
+			String therm=
+				"http://bleu.west.spy.net/~dustin/images/therm.gif";
+			URL url=new URL(therm);
+			baseImage=Toolkit.getDefaultToolkit().getImage(url);
+			log("Getting image...");
+		}
+		return(baseImage);
+	}
+
+	// When imageUpdate is called...yeah, this sucks.
+	public boolean imageUpdate(Image img, int infoflags,
+		int x, int y, int width, int height) {
+		log("imageUpdate called");
+		return(true);
 	}
 }
