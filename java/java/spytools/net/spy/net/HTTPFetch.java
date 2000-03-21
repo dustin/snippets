6dfba738fb3faac03989dfcdc7f10b676ca4d5f1
Dustin Sallings <dustin@spy.net>
2000-03-21 16:07:10 +0000

    Can strip tags correctly now.
    Author: dustin
    Date: 2000/03/21 08:07:10
    
    imported from: dustin@spy.net--projects/snippets--java--1.0--patch-230

--- java/java/spytools/net/spy/net/HTTPFetch.java
+++ java/java/spytools/net/spy/net/HTTPFetch.java
@@ -6,13 +6,21 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 
+import net.spy.SpyUtil;
+
+// Regex type stuff.
+import com.oroinc.text.regex.*;
+
 /**
  * Oversimplified HTTP document fetcher.
  */
 
 // Fetch the contents of a URL
 public class HTTPFetch {
-	URL url;
+	protected URL url;
+
+	protected String contents=null;
+	protected String stripped=null;
 
 	/**
 	 * Create a new HTTPFetch object for a given string representation of a
@@ -28,20 +36,15 @@ public class HTTPFetch {
 	 * Get a vector containing the individual lines of the document
 	 * returned from the URL.
 	 *
-	 * @exception Exception Any failures keeping the data from being
-	 * retrieved will be sent from here.
+	 * @exception Exception thrown when something fails.
 	 */
 	public Vector getLines() throws Exception {
 		Vector v = new Vector();
+		getData();
 
-		try {
-			BufferedReader br = getReader();
-			String line;
-			while( (line=br.readLine()) != null) {
-				v.addElement(line);
-			}
-		} catch(Exception e) {
-			throw new Exception(e.getMessage());
+		String lines[]=SpyUtil.split("\r\n", contents);
+		for(int i=0; i<lines.length; i++) {
+			v.addElement(lines[i]);
 		}
 
 		return(v);
@@ -50,25 +53,55 @@ public class HTTPFetch {
 	/**
 	 * Return the contents of the URL as a whole string.
 	 *
-	 * @exception Exception Any failures keeping the data from being
-	 * retrieved will be sent from here.
+	 * @exception Exception thrown when something fails.
 	 */
 	public String getData() throws Exception {
-		String s = "";
-
-		try {
-			BufferedReader br = getReader();
-			String line;
-			while( (line=br.readLine()) != null) {
-				s+=line + "\n";
+		if(contents==null) {
+			contents="";
+			try {
+				BufferedReader br = getReader();
+				String line;
+				while( (line=br.readLine()) != null) {
+					contents+=line + "\n";
+				}
+			} catch(Exception e) {
+				throw new Exception(e.getMessage());
 			}
-		} catch(Exception e) {
-			throw new Exception(e.getMessage());
 		}
-		return(s);
+		return(contents);
 	}
 
-	// Get a reader for the above two routines.
+	/**
+	 * Return the contents of the URL with the HTML tags stripped out.
+	 *
+	 * @exception Exception thrown when something fails.
+	 */
+	public String getStrippedData() throws Exception {
+		getData();
+		if(stripped==null) {
+			PatternMatcher matcher = new Perl5Matcher();
+			PatternCompiler compiler = new Perl5Compiler();
+
+			// Strip off the HTML tags.
+			Pattern pattern = compiler.compile("(<(.|[\r\n])*?>)",
+				Perl5Compiler.MULTILINE_MASK);
+			stripped = Util.substitute(matcher, pattern,
+				new StringSubstitution(""), contents, Util.SUBSTITUTE_ALL);
+			// Consolidate spaces
+			pattern = compiler.compile("((?:&nbsp;|[ \t])+)",
+				Perl5Compiler.MULTILINE_MASK);
+			stripped = Util.substitute(matcher, pattern,
+				new StringSubstitution(" "), stripped, Util.SUBSTITUTE_ALL);
+			// Get rid of double newlines
+			pattern = compiler.compile("([\r\n]+)",
+				Perl5Compiler.MULTILINE_MASK);
+			stripped = Util.substitute(matcher, pattern,
+				new StringSubstitution("\n"), stripped, Util.SUBSTITUTE_ALL);
+		}
+		return(stripped);
+	}
+
+	// Get a reader for the above routines.
 	protected BufferedReader getReader() throws Exception {
 		URLConnection uc = url.openConnection();
 		InputStream i = uc.getInputStream();
