0ea8d33b221e6ad642e1168b91c17484741e8ecb
Dustin Sallings <dustin@spy.net>
2000-03-22 04:24:11 +0000

    UPS package tracking.
    Author: dustin
    Date: 2000/03/21 20:24:11
    
    imported from: dustin@spy.net--projects/snippets--java--1.0--patch-232

---
+++ java/java/spytools/net/spy/info/UPS.java
@@ -0,0 +1,112 @@
+// Copyright (c) 2000  Dustin Sallings <dustin@spy.net>
+//
+// $Id: UPS.java,v 1.1 2000/03/21 20:24:13 dustin Exp $
+
+package net.spy.info;
+
+import java.util.*;
+
+import net.spy.*;
+import net.spy.net.*;
+
+/**
+ * Get UPS info.
+ */
+
+public class UPS extends Object {
+
+	String tracking_number=null;
+	String info=null;
+
+	Hashtable hinfo=null;
+
+	/**
+	 * Get a UPS info object.
+	 *
+	 * @param tracking_number tracking number to look up
+	 */
+	public UPS(String tracking_number) {
+		super();
+		this.tracking_number = tracking_number;
+	}
+
+	/**
+	 * Get a string representation of the UPS object.
+	 */
+	public String toString() {
+		String ret="";
+		try {
+			parseInfo();
+			ret+="Status:  " + get("Package Status") + ", ";
+			ret+="Location:  " + get("Last Scanned at") + ", ";
+			ret+="When:  " + get("Last Scanned on");
+		} catch(Exception e) {
+			// Just let it return null
+		}
+		return(ret);
+	}
+
+	/**
+	 * gets the value of a variable from the tracking info
+	 * <p>
+	 * The following variables are available as of this writing:
+	 * <p>
+	 * <ul>
+	 *  <li>Package Status - UPS' idea of the state of the delivery.</li>
+	 *  <li>Last Scanned on - Last time the package was scanned.</li>
+	 *  <li>Last Scanned at - Last location where the package was scanned.</li>
+	 *  <li>Sent on - Date the package was sent.</li>
+	 *  <li>Shipped to - Destination of the package.</li>
+	 *  <li>Tracking Number - UPS tracking number of the package.</li>
+	 *  <li>UPS Service Type - Um, it's not obvious?</li>
+	 *  <li>Package Weight - Approximate weight of the package.</li>
+	 *  <li>NOTICE - Legal shite.</li>
+	 * </ul>
+	 */
+	public String get(String what) throws Exception {
+		parseInfo();
+		return( (String) hinfo.get(what));
+	}
+
+	protected void parseInfo() throws Exception {
+		if(hinfo==null) {
+			getInfo();
+			hinfo=new Hashtable();
+			String lines[]=SpyUtil.split("\n", info);
+			int section=0;
+			for(int i=0; i<lines.length; i++) {
+				// we really only care about the first section
+				if(lines[i].startsWith("Tracking Result")) {
+					section=1;
+				} else if(lines[i].startsWith("Date Time Location Activity")) {
+					section=2;
+				}
+
+				// Figure out what section we're parsing.
+				if(section==1) {
+					int sep=lines[i].indexOf(": ");
+					if(sep>0) {
+						String k, v;
+						k=lines[i].substring(0, sep);
+						v=lines[i].substring(sep+2, lines[i].length());
+						hinfo.put(k, v);
+					} // Sep index
+				} // Section one
+			} // For loop through lines
+		} // if there's a need to find it at all.
+	}
+
+	protected void getInfo() throws Exception {
+		if(info==null) {
+			String url="http://wwwapps.ups.com/tracking/tracking.cgi?tracknum=";
+			url += tracking_number;
+			HTTPFetch f = new HTTPFetch(url);
+			info=f.getStrippedData();
+		}
+	}
+
+	public static void main(String args[]) throws Exception {
+		UPS u = new UPS(args[0]);
+		System.out.println("Info:\n" + u);
+	}
+}
