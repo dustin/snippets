81e94f32649770da8f4d26d6929d9b713749c6f6
Dustin Sallings <dustin@spy.net>
2000-06-06 12:42:52 +0000

    Hmm...maybe there's a better way at some point.
    Author: dustin
    Date: 2000/06/06 04:42:52
    
    imported from: dustin@spy.net--projects/snippets--java--1.0--patch-257

--- java/java/servlet/photo/net/spy/photo/PhotoConfig.java
+++ java/java/servlet/photo/net/spy/photo/PhotoConfig.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 1999  Dustin Sallings <dustin@spy.net>
  *
- * $Id: PhotoConfig.java,v 1.6 2000/05/30 04:53:21 dustin Exp $
+ * $Id: PhotoConfig.java,v 1.7 2000/06/06 04:42:52 dustin Exp $
  */
 
 package net.spy.photo;
@@ -11,8 +11,8 @@ import net.spy.*;
 public class PhotoConfig extends SpyConfig {
 
 	public PhotoConfig() {
-		// super("/afs/spy.net/misc/web/etc/photoconfig.xml");
-		super("/usr/local/etc/photo.properties");
+		super("/afs/spy.net/misc/web/etc/photoconfig.xml");
+		// super("/usr/local/etc/photo.properties");
 		orput("dbDriverName", "org.postgresql.Driver");
 		orput("dbSource", "jdbc:postgresql://localhost/photo");
 		orput("dbUser", "nobody");
