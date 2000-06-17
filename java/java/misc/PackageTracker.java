0cf2011805b168f0077a97a3862d6fcad364b814
Dustin Sallings <dustin@spy.net>
2000-06-17 05:23:54 +0000

    Folded all necessary functionality into PackageTracker ...
    Author: dustin
    Date: 2000/06/16 21:23:54
    
    Folded all necessary functionality into PackageTracker so that it just
    needs a config file.
    
    imported from: dustin@spy.net--projects/snippets--java--1.0--patch-265

--- java/java/misc/PackageTracker.java
+++ java/java/misc/PackageTracker.java
@@ -1,27 +1,54 @@
 // Copyright (c) 2000  Dustin Sallings <dustin@spy.net>
 //
-// $Id: PackageTracker.java,v 1.3 2000/06/15 23:06:23 dustin Exp $
+// $Id: PackageTracker.java,v 1.4 2000/06/16 21:23:56 dustin Exp $
 
 import java.sql.*;
 import java.util.*;
 
 import net.spy.net.SNPP;
-import net.spy.info.*;
+import net.spy.info.PackageInfo;
 
-public abstract class PackageTracker extends Object {
+import net.spy.SpyConfig;
+
+public class PackageTracker extends Object {
 	Connection conn=null;
 
 	protected int carrier_id=0;
+	protected String infoclassname=null;
 
-	public PackageTracker(int carrier_id) {
+	// This is *only* used for the main application
+	protected static Vector trackers=null;
+
+	protected static SpyConfig conf=null;
+
+	public PackageTracker() {
 		super();
+	}
+
+	public void setCarrierID(int carrier_id) {
 		this.carrier_id=carrier_id;
+	}
+
+	public void setClassName(String classname) {
+		this.infoclassname=classname;
+	}
+
+	/**
+	 * Give the object a database connection to use...Be *very* careful
+	 * with this.  If there will be multiple instances of this object running
+	 * on different threads, they *must* have their own DB connections.  This
+	 * problem is solved very easily by not giving them one.
+	 */
+	public void setConnection(Connection conn) {
+		this.conn=conn;
 	}
 
 	protected void openDB() throws Exception {
 		if(conn==null) {
-			String source="jdbc:postgresql://dhcp-104/misc";
-			conn=DriverManager.getConnection(source, "dustin", "");
+			Class.forName(conf.get("jdbc_driver"));
+			String source=conf.get("jdbc_source");
+			conn=DriverManager.getConnection(source,
+				conf.get("jdbc_username"), conf.get("jdbc_password"));
 		}
 	}
 
@@ -38,7 +65,12 @@ public abstract class PackageTracker extends Object {
 		return(ret);
 	}
 
-	protected abstract Info getInfoObj(String id);
+	protected PackageInfo getInfoObj(String id) throws Exception {
+		Class c=Class.forName(infoclassname);
+		PackageInfo infoObj=(PackageInfo)c.newInstance();
+		infoObj.setArg(id);
+		return(infoObj);
+	}
 
 	protected Hashtable getChanges(Hashtable stati) throws Exception {
 		Hashtable ret=new Hashtable();
@@ -48,7 +80,7 @@ public abstract class PackageTracker extends Object {
 		ResultSet rs = st.executeQuery(query);
 		while(rs.next()) {
 			String n=rs.getString("tracking_number");
-			Info i = getInfoObj(n);
+			PackageInfo i = getInfoObj(n);
 			String status=i.toString();
 			String current=(String)stati.get(n);
 			if(current==null) {
@@ -83,12 +115,13 @@ public abstract class PackageTracker extends Object {
 			);
 
 			// Add it if it's new
-			Info u=(Info)changes.get(key);
+			PackageInfo u=(PackageInfo)changes.get(key);
 			st.setString(1, key);
 			st.setInt(2, carrier_id);
-			short_status = u.get("Package Status");
-			if(short_status==null) {
-				short_status="Unknown";
+			if(u.isDelivered()) {
+				short_status="Delivered";
+			} else {
+				short_status="Waiting";
 			}
 			st.setString(3, short_status);
 			st.setString(4, u.toString());
@@ -119,21 +152,19 @@ public abstract class PackageTracker extends Object {
 
 	protected void cleanup() throws Exception {
 		try {
-			conn.setAutoCommit(false);
 			Statement st = conn.createStatement();
-			String query="delete from packages\n"
-				+ " where carrier_id = " + carrier_id
-				+ " and tracking_number in "
-				+ "(select tracking_number from package_status where "
-				+   "status='Delivered')";
+			String query=null;
+			query="delete from packages\n"
+				+ " where carrier_id = 2 and exists\n"
+				+ "  (select carrier_id from package_status s\n"
+				+ "   where status='Delivered' and carrier_id=2\n"
+				+ "    and packages.tracking_number=s.tracking_number)";
 			st.executeUpdate(query);
 			query="delete from package_status where status='Delivered'";
 			st.executeUpdate(query);
-			conn.commit();
 		} catch(Exception e) {
-			conn.rollback();
-		} finally {
-			conn.setAutoCommit(true);
+			System.err.println("Error cleaning up:  " + e);
+			e.printStackTrace();
 		}
 	}
 
@@ -145,20 +176,72 @@ public abstract class PackageTracker extends Object {
 		Hashtable stati=getCurrentStatus();
 		Hashtable changed = getChanges(stati);
 		saveChanges(changed);
+		System.out.println("Completed package carrier " + carrier_id);
 	}
 
-	public void run() {
+	public static void debug(String what) {
+		// System.out.println("DEBUG:  " + what);
+	}
+
+	// /////////////////////////////////////////////////////////////////////
+	// Below this line is application-mode stuff
+	// /////////////////////////////////////////////////////////////////////
+
+	public static void run(int naptime) {
 		// Loop forever
 		for(;;) {
+			for(Enumeration en=trackers.elements(); en.hasMoreElements(); ) {
+				PackageTracker pt=(PackageTracker)en.nextElement();
+				try {
+					pt.process();
+				} catch(Exception e) {
+					System.err.println("PackageTracker exception:  " + e);
+					e.printStackTrace();
+				}
+			} // package loop
 			try {
-				process();
-				// Run once per hour
-				Thread.sleep(60 * 60 * 1000);
+				Thread.sleep(naptime);
 			} catch(Exception e) {
-				System.err.println("PackageTracker exception:  " + e);
-				// Clear the db connection
-				conn=null;
+				// Ignore, we'll just go faster
+			}
+		} // Infinite loop
+	}
+
+	public static void getObjs() {
+		// Initialize the vector
+		trackers=new Vector();
+
+		String tracker_list=conf.get("trackers");
+		StringTokenizer st=new StringTokenizer(tracker_list);
+		while(st.hasMoreTokens()) {
+			String type=st.nextToken();
+			try {
+				// Add the new object to the list
+				PackageTracker tracker=new PackageTracker();
+				String key=null;
+				key=type+ ".carrier_id";
+				tracker.setCarrierID(Integer.parseInt(conf.get(key)));
+				key=type+ ".infoobj";
+				tracker.setClassName(conf.get(key));
+				trackers.addElement(tracker);
+			} catch(Exception e) {
+				System.err.println("Error instantiating " + type + " tracker");
+				System.out.println("Message:  " + e);
+				e.printStackTrace();
 			}
 		}
+	}
+
+	public static void main(String args[]) throws Exception {
+		conf=new SpyConfig(args[0]);
+		int naptime=0;
+
+		getObjs();
+		try {
+			naptime=Integer.parseInt(conf.get("naptime"));
+		} catch(Exception e) {
+			naptime=3600000;
+		}
+		run(naptime);
 	}
 }
