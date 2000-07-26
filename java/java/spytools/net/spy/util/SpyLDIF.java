85793a442e5b9ff2870a21349780a2be41edd29d
Dustin Sallings <dustin@spy.net>
2000-07-26 08:26:41 +0000

    More LDIF stuff.
    Author: dustin
    Date: 2000/07/26 00:26:41
    
    imported from: dustin@spy.net--projects/snippets--java--1.0--patch-314

--- java/java/spytools/net/spy/util/SpyLDIF.java
+++ java/java/spytools/net/spy/util/SpyLDIF.java
@@ -1,5 +1,5 @@
 // Copyright (c) 2000  Dustin Sallings <dustin@spy.net>
-// $Id: SpyLDIF.java,v 1.1 2000/07/25 21:52:58 dustin Exp $
+// $Id: SpyLDIF.java,v 1.2 2000/07/26 00:26:41 dustin Exp $
 
 package net.spy.util;
 
@@ -11,6 +11,44 @@ public class SpyLDIF extends Hashtable {
 		super();
 
 		parseLDIF(ldif);
+	}
+
+	public String getString(String key) {
+		String res=(String)super.get(key);
+		return(res);
+	}
+
+	public int getInt(String key) {
+		String res=(String)super.get(key);
+		return(Integer.parseInt(res));
+	}
+
+	protected void decodeAndStore(String chunk) {
+		boolean decode=true;
+		int colon=chunk.indexOf("::");
+		if(colon<0) {
+			decode=false;
+			colon=chunk.indexOf(":");
+		}
+		// Only process the segment if it's valid.
+		if(colon>0) {
+			String k=chunk.substring(0, colon);
+			String v=chunk.substring(colon+1).trim();
+
+			if(decode) {
+				try {
+					String tmp=v.substring(1).trim();
+					this.put(k + ":encoded", tmp);
+					BASE64Decoder base64 = new BASE64Decoder();
+					byte data[]=base64.decodeBuffer(tmp);
+					v=new String(data);
+				} catch(Exception e) {
+					System.err.println(
+						"LDIF: error while decoding: " + e);
+				}
+			}
+			this.put(k, v);
+		}
 	}
 
 	protected void parseLDIF(String ldif) {
@@ -26,35 +64,13 @@ public class SpyLDIF extends Hashtable {
 				chunk+=line.trim();
 			} else {
 				if(chunk.length()>0) {
-					boolean decode=true;
-					int colon=chunk.indexOf("::");
-					if(colon<0) {
-						decode=false;
-						colon=chunk.indexOf(":");
-					}
-
-					// Only process the segment if it's valid.
-					if(colon>0) {
-						String k=chunk.substring(0, colon);
-						String v=chunk.substring(colon+1).trim();
-
-						if(decode) {
-							try {
-								String tmp=v.substring(1).trim();
-								BASE64Decoder base64 = new BASE64Decoder();
-								byte data[]=base64.decodeBuffer(tmp);
-								v=new String(data);
-							} catch(Exception e) {
-								System.err.println(
-									"LDIF: error while decoding: " + e);
-							}
-						}
-
-						this.put(k, v);
-					}
+					decodeAndStore(chunk);
 				}
 				chunk=line;
 			} // End of else
 		} // Read all tokens.
+		if(chunk.length()>0) {
+			decodeAndStore(chunk);
+		}
 	} // parseLDIF
 }
