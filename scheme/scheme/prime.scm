6c7c569517a830a2920dab4985b5b8fb096c3a91
Dustin Sallings <dustin@spy.net>
2000-08-05 04:15:26 +0000

    More scheme stuff.
    Author: dustin
    Date: 2000/08/04 20:15:26
    
    imported from: dustin@spy.net--projects/snippets--scheme--1.0--patch-2

--- scheme/scheme/prime.scm
+++ scheme/scheme/prime.scm
@@ -1,4 +1,4 @@
-; Find out whether a number is prime.
+; Prime number stuff
 
 (define is-even?
 	(lambda (x)
@@ -22,10 +22,11 @@
 (define isprime
 	(lambda (x)
 		(cond
+			( (< x 2) #f)
 			( (= x 2) #t)
 			( (is-even? x) #f)
 			( else
-				(let ( (done #f) (prime #t) )
+				(let ( (done #f) (prime #t) (stoppoint (sqrt x)) )
 					(do
 						( (m 3 (+ m 2)) )
 
@@ -35,7 +36,7 @@
 						; The calculation
 						(cond
 							((= x m) (set! prime #t) (set! done #t))
-							((> m (sqrt x)) (set! prime #t) (set! done #t))
+							((> m stoppoint) (set! prime #t) (set! done #t))
 							((divisible? x m) (set! prime #f) (set! done #t))
 						)
 					)
@@ -43,6 +44,49 @@
 				prime
 				)
 			)
+		)
+	)
+)
+
+; Find the next prime number
+(define nextprime
+	(lambda (x)
+		(if (isprime x) (set! x (+ 1 x)) x )
+		(cond
+			((= x 1) (set! x 2))
+			((= x 2) x)
+			((is-even? x) (set! x (+ 1 x)))
+		)
+
+		(let ((nextp x)(done #f))
+
+			(do
+				( (p x (+ p 2)))
+
+				(done)
+
+				(cond
+					((isprime p) (set! done #t) (set! nextp p))
+				)
+			)
+			nextp
+		)
+	)
+)
+
+; Find all the prime numbers from x to y
+(define listprimes
+	(lambda (x y)
+		(if (isprime x) x (set! x (nextprime x)) )
+		(let ((plist '()))
+			(do
+				( (i x (nextprime i)))
+				((> i y))
+
+				(set! plist (append plist (list i)))
+			)
+
+			plist
 		)
 	)
 )
