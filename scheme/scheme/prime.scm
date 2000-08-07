893e1666d67e9fb3411c38a1b5ef988198e536d7
Dustin Sallings <dustin@spy.net>
2000-08-07 15:30:53 +0000

    This is the stuff I wrote on the palm.
    Author: dustin
    Date: 2000/08/07 07:30:53
    
    imported from: dustin@spy.net--projects/snippets--scheme--1.0--patch-6

--- scheme/scheme/prime.scm
+++ scheme/scheme/prime.scm
@@ -1,74 +1,86 @@
-; Prime number stuff
+; Prime stuff
 
-(define is-even?
-	(lambda (x)
-		(divisible? x 2)
-	)
-)
+(define
+  divisible?
+    (lambda (x y)
+	(cond
+	    ( (= y x) #t)
+	    ( (< x 16383 )
+		(zero? (modulo x y)))
+	    ( else (
+		(let ((d (/ x y)))
+		(= d (round d))))))))
 
-(define is-odd?
-	(lambda (x)
-		(not (is-even? x))
-	)
-)
+(define
+	even?
+		(lambda (x)
+			(divisible? x 2)))
 
-(define divisible?
-	(lambda (x y)
-		(= 0 (modulo x y))
-	)
-)
-
-; Find out whether a given number is prime
-(define isprime
-	(lambda (x)
+(define
+  prime?
+    (lambda (x)
+      (cond
+	((< x 2) #f)
+	((= x 2) #t)
+	((even? x) #f)
+	(else
+	  (let ((stopat (sqrt x))
+		(done #f) (p #t))
+	    (do ((i 3 (+ i 2)))
+		( done p )
 		(cond
-			( (< x 2) #f)
-			( (= x 2) #t)
-			( (is-even? x) #f)
-			( else
-				(let ( (done #f) (prime #t) (stoppoint (sqrt x)) )
-					(do
-						( (m 3 (+ m 2)) )
+		  ( (> i stopat)
+			(set! done #t))
+		  ( (divisible? x i)
+			(set! done #t)
+			(set! p #f))
+		  ( else #f )))))
+		)))
 
-						; Loop until done
-						(done prime)
+(define
+  next-prime
+   (lambda (x)
+      (set! x (cond
+	((< x 2) 2)
+	((even? x) (+ x 1))
+	((prime? x) (+ x 2))
+	(else x)))
+      (do
+	((i x (+ i 2)))
+	( (prime? i) i)
+	)))
 
-						; The calculation
-						(cond
-							((= x m) (set! prime #t) (set! done #t))
-							((> m stoppoint) (set! prime #t) (set! done #t))
-							((divisible? x m) (set! prime #f) (set! done #t))
-						)
-					)
-				)
-			)
-		)
-	)
-)
+(define
+	primes
+	(lambda (start-n stop-n)
+	    (let ((prms '()))
+		(set! start-n
+			(next-prime start-n))
+		(do
+			( (i start-n
+			    (next-prime i)))
+			( (> i stop-n) prms )
+			(set! prms (append
+			  prms (list i)))))))
 
-; Find the next prime number
-(define nextprime
-	(lambda (x)
-		(if (isprime x) (set! x (+ 1 x)) x )
-		(cond
-			((= x 1) (set! x 2))
-			((= x 2) x)
-			((is-even? x) (set! x (+ 1 x))))
+(define
+  sum
+     (lambda (l)
+	(let ((s 0))
+	  (for-each (lambda (i)
+		(set! s (+ s i) ) ) l )
+		  s)))
 
-		(let ((nextp x)(done #f))
-			(do
-				( (p x (+ p 2)))
-				(done nextp)
-				(cond
-					((isprime p) (set! done #t) (set! nextp p)))))))
-
-; Find all the prime numbers from x to y
-(define listprimes
-	(lambda (x y)
-		(if (isprime x) x (set! x (nextprime x)) )
-		(let ((plist '()))
-			(do
-				( (i x (nextprime i)))
-				((> i y) plist)
-
-				(set! plist (append plist (list i)))))))
+(define
+  deltas
+  (lambda (l)
+	(let ((lastn #f) (tmp '()))
+	  (for-each (lambda(i)
+				  (cond
+					((number? lastn)
+					 (set! tmp
+					   (append tmp
+							   (list (- i lastn)))))
+					(else #t))
+				  (set! lastn i)) l)
+	  tmp)))
