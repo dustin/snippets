3df2de400fcd2d5b7e4989a8b2f1bbdfe2daa939
Dustin Sallings <dustin@spy.net>
2001-03-02 06:42:14 +0000

    [PATCH 1053/2759] RSS!
    Author: dustin
    Date: 2001/03/01 22:42:14
    
    imported from: dustin@spy.net--projects/snippets--java--1.0--patch-396

---
+++ java/java/servlet/rss/net/spy/rss/RSSServlet.java
@@ -0,0 +1,66 @@
+// Copyright (c) 2001  Dustin Sallings <dustin@spy.net>
+//
+// $Id: RSSServlet.java,v 1.1 2001/03/01 22:42:15 dustin Exp $
+
+package net.spy.rss;
+
+import java.io.*;
+import net.spy.net.*;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import com.caucho.transform.*;
+import com.caucho.xsl.*;
+
+public class RSSServlet extends HttpServlet {
+
+	// Process get requests only.
+	public void doGet(
+		HttpServletRequest request, HttpServletResponse response
+	) throws ServletException, IOException {
+		try {
+			// Get it
+			String url=request.getParameter("url");
+			String xsl=request.getParameter("xsl");
+			String html=getHTML(url, xsl);
+
+			// Set a cache time
+			java.util.Date d=new java.util.Date();
+			long l=d.getTime();
+			// Make it valid for an hour.
+			l+=3600000L;
+			response.setDateHeader("Expires", l);
+
+			// Give it
+			response.setContentType("text/html");
+			PrintWriter out = response.getWriter();
+			out.print(html);
+			out.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+			throw new ServletException("Error processing RSS:  " + e);
+		}
+	}
+
+	protected static String getHTML(String url, String stylesheet)
+		throws Exception {
+
+		HTTPFetch hf=new HTTPFetch(url);
+		String xml=hf.getData();
+		OutputStream out=new ByteArrayOutputStream();
+		StylesheetFactory factory=new Xsl();
+		Stylesheet style=factory.newStylesheet(stylesheet);
+		StreamTransformer transformer=style.newStreamTransformer();
+		transformer.transformString(xml, out);
+
+		return(out.toString());
+	}
+
+	public static void main(String args[]) throws Exception {
+		String html=getHTML(args[0], args[1]);
+
+		System.out.println(html);
+	}
+
+}
