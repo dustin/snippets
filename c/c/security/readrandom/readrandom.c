16475ccb064161ef3afc5b77a2d4049c0591950e
Dustin Sallings <dustin@spy.net>
2001-06-30 17:24:08 +0000

    Better error messages and stuff.
    Author: dustin
    Date: 2001/06/30 09:24:08
    
    imported from: dustin@spy.net--projects/snippets--c--1.0--patch-340

--- c/c/security/readrandom/readrandom.c
+++ c/c/security/readrandom/readrandom.c
@@ -1,14 +1,21 @@
 /*
  * Copyright (c) 2001  Dustin Sallings <dustin@spy.net>
  *
- * $Id: readrandom.c,v 1.1 2001/06/30 09:09:06 dustin Exp $
+ * $Id: readrandom.c,v 1.2 2001/06/30 09:24:08 dustin Exp $
  */
 
 #include <stdio.h>
+#include <unistd.h>
 #include <fcntl.h>
 #include <assert.h>
 
 #define BLOCKSIZE 8192
+
+void usage(const char *name)
+{
+	fprintf(stderr, "Usage:  %s size indev outfile\n", name);
+	exit(1);
+}
 
 int main(int argc, char **argv)
 {
@@ -18,13 +25,21 @@ int main(int argc, char **argv)
 	int writesize=0;
 	unsigned char data[BLOCKSIZE];
 
-	assert(argc>3);
+	if(argc<4) {
+		usage(argv[0]);
+	}
 
 	infile=open(argv[2], O_RDONLY, 0444);
-	assert(infile>=0);
+	if(infile<0) {
+		perror(argv[2]);
+		exit(1);
+	}
 
 	outfile=open(argv[3], O_WRONLY|O_CREAT|O_EXCL, 0400);
-	assert(outfile>=0);
+	if(outfile<0) {
+		perror(argv[3]);
+		exit(1);
+	}
 
 	sizetoread=atoi(argv[1]);
 	assert(sizetoread>0);
@@ -33,6 +48,9 @@ int main(int argc, char **argv)
 		readsize=-1;
 		while(readsize<0) {
 			readsize=read(infile, data, BLOCKSIZE);
+			if(readsize<0) {
+				usleep(2600); /* Wait a bit before reading again (on error) */
+			}
 		}
 		amountread+=readsize;
 		writesize=write(outfile, data, readsize);
