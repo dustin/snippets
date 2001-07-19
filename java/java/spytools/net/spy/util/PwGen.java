480bec09b6d127a4900082ed44155050d1cdc4b7
Dustin Sallings <dustin@spy.net>
2001-07-19 14:36:22 +0000

    Added a password generator.
    Author: dustin
    Date: 2001/07/19 06:36:22
    
    imported from: dustin@spy.net--projects/snippets--java--1.0--patch-485

---
+++ java/java/spytools/net/spy/util/PwGen.java
@@ -0,0 +1,71 @@
+// Copyright (c) 2001  Dustin Sallings <dustin@spy.net>
+//
+// $Id: PwGen.java,v 1.1 2001/07/19 06:36:23 dustin Exp $
+
+package net.spy.util;
+
+import java.security.*;
+
+/**
+ * Password generator.
+ */
+public class PwGen extends Object {
+
+	/**
+	 * Character set containing numeric and uppercase letters.
+	 */
+	public static final char NUMBER_UPPER[]={
+		'2', '3', '4', '5', '6', '7', '8', '9',
+		'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N',
+		'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'
+	};
+
+	/**
+	 * Character set containing numeric and lowercase characters.
+	 */
+	public static final char NUMBER_LOWER[]={
+		'2', '3', '4', '5', '6', '7', '8', '9',
+		'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n',
+		'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'
+	};
+
+	/**
+	 * Character set containing numeric, upper, and lowercase letters.
+	 */
+	public static final char NUMBER_UPPER_LOWER[]={
+		'2', '3', '4', '5', '6', '7', '8', '9',
+		'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N',
+		'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z',
+		'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n',
+		'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'
+	};
+
+	/**
+	 * Get a random string at the given length using the NUMBER_UPPER set.
+	 */
+	public static String getPass(int length) {
+		return(getPass(length, NUMBER_UPPER));
+	}
+
+	/**
+	 * Get a random string with the given length and character set.
+	 */
+	public static String getPass(int length, char charset[]) {
+		SecureRandom sr=new SecureRandom();
+		StringBuffer sb=new StringBuffer();
+		for(int i=0; i<length; i++) {
+			// Stick a random character in our result.
+			sb.append(charset[sr.nextInt(charset.length)]);
+		}
+		return(sb.toString());
+	}
+
+	/**
+	 * Testing and what not.
+	 */
+	public static void main(String args[]) throws Exception {
+		String s=getPass(Integer.parseInt(args[0]));
+		System.out.println("Got " + s);
+	}
+
+}
