d3c466cba51c21d60f25135aa7a5aa261c1ccd6d
Dustin Sallings <dustin@spy.net>
2001-07-28 10:03:10 +0000

    Basic multicast send and receive.
    Author: dustin
    Date: 2001/07/28 02:03:10
    
    imported from: dustin@spy.net--projects/snippets--java--1.0--patch-488

---
+++ java/java/net/multicast/MCastRecv.java
@@ -0,0 +1,35 @@
+// Copyright (c) 2001  Dustin Sallings <dustin@spy.net>
+//
+// $Id: MCastRecv.java,v 1.1 2001/07/28 02:03:10 dustin Exp $
+
+import java.net.*;
+
+/**
+ *
+ */
+public class MCastRecv extends Object {
+
+	/**
+	 * Testing and what not.
+	 */
+	public static void main(String args[]) throws Exception {
+		InetAddress group = InetAddress.getByName(args[0]);
+		MulticastSocket s = new MulticastSocket(6789);
+		s.joinGroup(group);
+
+		byte data[]=new byte[1500];
+		DatagramPacket recv = new DatagramPacket(data, data.length);
+		s.receive(recv);
+
+		byte tmp_d[]=new byte[recv.getLength()];
+		System.arraycopy(data, 0, tmp_d, 0, tmp_d.length);
+
+		String tmp=new String(tmp_d);
+		System.out.println("Got:\n\t" + tmp
+			+ "\n (" + tmp.length() + " bytes)");
+
+		s.leaveGroup(group);
+	}
+
+}
+
---
+++ java/java/net/multicast/MCastSend.java
@@ -0,0 +1,27 @@
+// Copyright (c) 2001  Dustin Sallings <dustin@spy.net>
+//
+// $Id: MCastSend.java,v 1.1 2001/07/28 02:03:11 dustin Exp $
+
+import java.net.*;
+
+/**
+ *
+ */
+public class MCastSend extends Object {
+
+	/**
+	 * Testing and what not.
+	 */
+	public static void main(String args[]) throws Exception {
+		String msg=args[1];
+		InetAddress group = InetAddress.getByName(args[0]);
+		MulticastSocket s = new MulticastSocket(6789);
+		s.joinGroup(group);
+		DatagramPacket hi = new DatagramPacket(msg.getBytes(),
+			msg.length(), group, 6789);
+		s.send(hi);
+		s.leaveGroup(group);
+	}
+
+}
+
