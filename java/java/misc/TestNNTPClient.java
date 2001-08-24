11052a52597a83386dfd789e1dc96723b1c7d89a
Dustin Sallings <dustin@spy.net>
2001-08-24 17:16:39 +0000

    more parallel
    Author: dustin
    Date: 2001/08/24 09:16:39
    
    imported from: dustin@spy.net--projects/snippets--java--1.0--patch-507

--- java/java/misc/TestNNTPClient.java
+++ java/java/misc/TestNNTPClient.java
@@ -1,6 +1,6 @@
 // Copyright (c) 2001  Dustin Sallings <dustin@spy.net>
 //
-// $Id: TestNNTPClient.java,v 1.1 2001/08/09 09:15:09 dustin Exp $
+// $Id: TestNNTPClient.java,v 1.2 2001/08/24 09:16:39 dustin Exp $
 
 import java.io.*;
 import net.spy.util.*;
@@ -11,7 +11,7 @@ import com.bigfoot.bugar.nntp.*;
  */
 public class TestNNTPClient extends Object {
 
-	public static final int MAX_ARTICLES=100;
+	public static final int MAX_ARTICLES=500;
 
 	/**
 	 * Get an instance of TestClient.
@@ -24,10 +24,14 @@ public class TestNNTPClient extends Object {
 	 * Testing and what not.
 	 */
 	public static void main(String args[]) throws Exception {
+		// First, make sure the NNTP portion has the highest priority.
+		Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
+		ThreadPool tp=new ThreadPool(5);
+
 		NNTPConnection nntp=new NNTPConnection(
 			new NNTPClientAdapter(), "juan");
-
 		Newsgroup group=nntp.getNewsgroup("alt.binaries.pictures.erotica");
+
 		System.out.println(group);
 		int first=group.getFirst();
 		int last=group.getLast();
@@ -41,29 +45,46 @@ public class TestNNTPClient extends Object {
 		System.out.println("Articles:  " + count
 			+ " from " + first + " to " + last);
 
-		for(int i=first; ((i-first) < MAX_ARTICLES) && i<=last; i++) {
-			Article a=nntp.getArticle(group.toString(), i);
-			System.out.println(a);
+		for(int i=first; ((i-first) < MAX_ARTICLES) && i<=last; i+=10) {
+			tp.waitForTaskCount(10);
+
+			tp.addTask(new Decoder(group.toString(), i));
+		}
+	}
+
+	private static class Decoder implements Runnable {
+		private String groupName=null;
+		private int number=0;
+		public Decoder(String groupName, int i) {
+			super();
+			this.groupName=groupName;
+			this.number=i;
+		}
+
+		public void run() {
 			try {
+				NNTPConnection nntp=new NNTPConnection(
+					new NNTPClientAdapter(), "juan");
+				Newsgroup group=nntp.getNewsgroup(groupName);
+				Article a=nntp.getArticle(group.toString(), number);
+				// System.out.println(a);
 				ByteArrayInputStream bais=
 					new ByteArrayInputStream(a.getBody().getBytes());
+
 				UUInputStream uu=new UUInputStream(bais);
 				String name=uu.getFilename().toLowerCase();
-
 				if(name.endsWith(".jpg")) {
 					StringBuffer sb=new StringBuffer();
 					char chars[]=name.toCharArray();
 					for(int j=0; j<chars.length; j++) {
 						if(chars[j]=='.'
 							|| Character.isLetterOrDigit(chars[j])) {
-
 							sb.append(chars[j]);
 						}
 					}
-
 					String filename=sb.toString();
 					if(filename.length() < 5) {
-						filename=i + ".jpg";
+						filename=number + ".jpg";
 					}
 					FileOutputStream fos=new FileOutputStream(filename);
 					System.out.println("Writing " + filename);
@@ -75,13 +96,13 @@ public class TestNNTPClient extends Object {
 					}
 					fos.close();
 				}
-
 				uu.close();
-			} catch(IOException e) {
-				e.printStackTrace();
+				bais.close();
+				nntp.close();
+			} catch(Throwable t) {
+				t.printStackTrace();
 			}
 		}
 	}
 
 }
-
