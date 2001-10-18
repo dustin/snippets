416f25b3dcf4d1fc1b24438d5e49ab6f9610ff94
Dustin Sallings <dustin@spy.net>
2001-10-18 05:48:51 +0000

    Added that report view.
    Author: dustin
    Date: 2001/10/17 21:48:51
    
    imported from: dustin@spy.net--projects/snippets--java--1.0--patch-528

--- java/java/servlet/dsservlet/ds.sql
+++ java/java/servlet/dsservlet/ds.sql
@@ -1,6 +1,6 @@
 -- Copyright (c) 2000  Dustin Sallings <dustin@spy.net>
 --
--- $Id: ds.sql,v 1.2 2000/11/07 10:48:58 dustin Exp $
+-- $Id: ds.sql,v 1.3 2001/10/17 21:48:51 dustin Exp $
 
 -- This is one big fat table where we post movies and have users pick them
 -- up
@@ -15,3 +15,10 @@ create table show_distribution (
 create index show_distbyuser on show_distribution(submitted_to);
 
 grant all on show_distribution to nobody;
+
+-- Distribution time report
+create view show_dist_times as
+	select show_id, submitted, submitted_to, completed,
+		completed-submitted as time_to_delivery,
+		now()-submitted as submitted_to_now
+	from show_distribution;
