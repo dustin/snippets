29a32bda24340c5be5986b39e4d8eaa808a45565
Dustin Sallings <dustin@spy.net>
2001-11-03 09:49:52 +0000

    Emacs LISP toy buffer
    Author: dustin
    Date: 2001/11/03 01:49:52
    
    imported from: dustin@spy.net--projects/snippets--lisp--1.0--patch-1

---
+++ lisp/lisp/play.el
@@ -0,0 +1,31 @@
+; $Id: play.el,v 1.1 2001/11/03 01:49:52 dustin Exp $
+
+(defun dustin-for-each (p l) "Dustin's for-each test."
+  (if l
+      (progn
+	(funcall p (car l))
+	(dustin-for-each p (cdr l))))
+  'Done)
+
+
+(if '() 'true 'false)
+
+(dustin-for-each 'print '(1 2 3))
+
+(symbol-function 'print)
+
+; recursive
+(defun pow (x y) "Calculate x^y"
+  (if (< y 1)
+      1
+    (* x (pow x (- y 1)))))
+
+; Iterative
+(defun pow (x y) "Calculate x^y"
+  (let ((rv 1.0))
+    (loop for i from 1 to y do
+	  (setq rv (* rv x)))
+    rv))
+
+(pow 2.0 128)
+
