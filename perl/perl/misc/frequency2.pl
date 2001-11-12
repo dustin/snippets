64c378cec24f813f9a83b947d599d5c5d5f20d82
Dustin Sallings <dustin@spy.net>
2001-11-12 20:23:08 +0000

    Calculate more properlyer
    Author: dustin
    Date: 2001/11/12 12:23:08
    
    imported from: dustin@spy.net--projects/snippets--perl--1.0--patch-143

--- perl/perl/misc/frequency2.pl
+++ perl/perl/misc/frequency2.pl
@@ -57,48 +57,39 @@ sub getTone
 {
 	# Frequency, duration
 	my($freq, $t)=@_;
-	my($r, $current, $step);
+	my(@r, $current, $step);
 	# Figure out how many bytes we need to get the given duration
 	my $rbytes=$fout*$t;
 	# Calculate the step
 	$step=($freq/$fout)*$fin;
 	# print STDERR "Step is $step for $freq, bytes is $rbytes for $t\n";
 	$current=0;
-	$r="";
-	while(length($r) < $rbytes) {
+	@r=();
+	while(@r < $rbytes) {
 		if($current > $fin) {
+			# print STDERR "current exceeded $fin:  $current\n";
+			# $current = ($current % $fin);
 			$current = ($current%$fin)+1;
 		}
 
-		my $v=128+($basewave[$current]*128);
-		$r.=pack("c", $v);
-		# my $v=32768+($basewave[$current]*32768);
-		# $r.=pack("s", $v);
-		# $r.=pack("s", $v);
+		my $v=256+($basewave[$current]*256);
+		push(@r, $v);
 
 		$current+=$step;
 	}
 	# Only return the requested amount
-	return($r);
+	return(@r);
 }
 
 #
 # Main, play an A minor scale
 #
 
-print ".snd";
-print pack("N", 56);
-print pack("N", 120000);
-print pack("N", 2); # format
-print pack("N", $fout); # frequency
-print pack("N", 1); # channels
-print "DLS\0";
-
 @basewave=getWave($fin);
 
-my @notes=qw(A B C D E F GS);
-for(@notes) {
-	print getTone($notes{$_}, 1);
+print "float wave[]={\n";
+# for(getTone($notes{'C'}*2, 1)) {
+for(getTone(10000, 1)) {
+	print "\t$_,\n";
 }
-# last note in a minor
-print getTone($notes{'A'} * 2, 1);
+print "};\n";
