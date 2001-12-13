74fc4d33781ea186f3131282cee29f492b0e2218
Dustin Sallings <dustin@spy.net>
2001-12-13 17:58:23 +0000

    View views in emacs.
    Author: dustin
    Date: 2001/12/13 09:58:23
    
    imported from: dustin@spy.net--projects/snippets--lisp--1.0--patch-11

---
+++ lisp/lisp/moneyviews.el
@@ -0,0 +1,21 @@
+; View views from my money database.
+; $Id: moneyviews.el,v 1.1 2001/12/13 09:58:23 dustin Exp $
+
+(require 'pg)
+
+(defun get-money-data (v) "Get the data for the given view."
+  (with-pg-connection con ("money" "dustin" "blah" "dhcp-104")
+		      (let ((res (pg:exec con "select * from " v)))
+			(list (pg:result res :attributes)
+			      (pg:result res :tuples)))))
+
+(defun shove-data-in (b s) "Shove data in a given buffer."
+  (save-excursion
+    (if (not (get-buffer b))
+	(generate-new-buffer b))
+    (set-buffer b)
+    (insert s)))
+
+(defun show-data (d) "Show the data from the postgres result."
+
+(shove-data-in "*results*" (get-money-data "show_account_values"))
