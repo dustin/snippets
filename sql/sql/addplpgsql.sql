1b3f8a5c743df18e495e38fd57bd7e8f28075711
Dustin Sallings <dustin@spy.net>
2002-02-09 07:41:02 +0000

    Don't give the full path.
    Author: dustin
    Date: 2002/02/08 23:41:02
    
    imported from: dustin@spy.net--projects/snippets--sql--1.0--patch-13

--- sql/sql/addplpgsql.sql
+++ sql/sql/addplpgsql.sql
@@ -1,7 +1,7 @@
 -- Add PL/pgsql language to a database
 
 CREATE FUNCTION plpgsql_call_handler () RETURNS OPAQUE AS
-	'/usr/local/pgsql/lib/plpgsql.so' LANGUAGE 'C';
+	'plpgsql.so' LANGUAGE 'C';
 
 CREATE TRUSTED PROCEDURAL LANGUAGE 'plpgsql'
 	HANDLER plpgsql_call_handler
