b1b3c3ea0abd5b298f21025e3d3e43900a4d4815
Dustin Sallings <dustin@spy.net>
2002-02-14 04:27:17 +0000

    TTL monitoring stuff.
    Author: dustin
    Date: 2002/02/13 20:27:17
    
    imported from: dustin@spy.net--projects/snippets--java--1.0--patch-540

---
+++ java/java/spytools/net/spy/db/DBTTL.java
@@ -0,0 +1,106 @@
+// Copyright (c) 2001  Dustin Sallings <dustin@spy.net>
+//
+// $Id: DBTTL.java,v 1.1 2002/02/13 20:27:17 dustin Exp $
+
+package net.spy.db;
+
+/**
+ * Used to track checked out DB connections to report on connections that
+ * have been checked out longer than we expect them to be.
+ */
+public class DBTTL extends Object {
+
+	private long ttl=0;
+	private long startTime=0;
+	private Exception e=null;
+	private Object extraInfo=null;
+
+	private boolean isClosed=false;
+	private long lastReport=0;
+	private int nReports=0;
+
+	/**
+	 * Get an instance of DBTTL.
+	 */
+	public DBTTL(long ttl) {
+		super();
+		this.ttl=ttl;
+		this.startTime=System.currentTimeMillis();
+		this.e=new Exception("DB TTL Expired");
+	}
+
+	/**
+	 * String me.
+	 */
+	public String toString() {
+		return("DBTTL:  " + ttl);
+	}
+
+	/**
+	 * Provide extra information for the TTL report.
+	 */
+	public void setExtraInfo(Object o) {
+		this.extraInfo=o;
+	}
+
+	/**
+	 * Calling this method states that we are no longer interested in the
+	 * progress of this TTL.
+	 */
+	public void close() {
+		isClosed=true;
+	}
+
+	/**
+	 * Return true if this TTL is no longer interesting.
+	 */
+	public boolean isClosed() {
+		return(isClosed);
+	}
+
+	private boolean isExpired() {
+		boolean rv=false;
+		long now=System.currentTimeMillis();
+
+		if(!isClosed()) {
+			if(now > startTime+ttl) {
+				rv=true;
+			}
+		}
+
+		return(rv);
+	}
+
+	// Has it been long enough since our last report?
+	private boolean readyForReport() {
+		long now=System.currentTimeMillis();
+
+		return( (now-lastReport) > REPORT_INTERVAL);
+	}
+
+	/**
+	 * Request a report of the TTL.  This will only print if the TTL has
+	 * expired and we haven't shown a report for this particular TTL within
+	 * the last REPORT_INTERVAL milliseconds.
+	 */
+	public void report() {
+		if(isExpired() && readyForReport()) {
+			long now=System.currentTimeMillis();
+			// Basic idea of what's going on.
+			System.err.println("This DB query has been running for "
+				+ (now-startTime) + "ms with a max expectancy of "
+				+ ttl + "ms");
+			if(extraInfo!=null) {
+				System.err.println(extraInfo);
+			}
+			e.printStackTrace();
+			lastReport=now;
+
+			// If we've reported enough times, give up.
+			if(nReports++ >= N_REPORTS) {
+				close();
+			}
+		}
+	}
+
+}
---
+++ java/java/spytools/net/spy/db/DBTTLMonitor.java
@@ -0,0 +1,81 @@
+// Copyright (c) 2001  Dustin Sallings <dustin@spy.net>
+//
+// $Id: DBTTLMonitor.java,v 1.1 2002/02/13 20:27:18 dustin Exp $
+
+package net.spy.db;
+
+import java.util.*;
+
+/**
+ * Monitors TTLs.
+ */
+public class DBTTLMonitor extends Thread {
+
+	private Vector ttls=null;
+
+	private static final long NAPTIME=5000;
+	private long lastAddition=0;
+	private static final long MAX_QUIESCENCE=300000;
+
+	/**
+	 * Get an instance of DBTTLMonitor.
+	 */
+	public DBTTLMonitor() {
+		super();
+		ttls=new Vector();
+		lastAddition=System.currentTimeMillis();
+		setName("DB TTL Monitor");
+		setDaemon(true);
+		start();
+	}
+
+	/**
+	 * Add a new TTL to the list we're monitoring.
+	 */
+	public void addTTL(DBTTL ttl) {
+		lastAddition=System.currentTimeMillis();
+		synchronized(ttls) {
+			ttls.addElement(ttl);
+		}
+	}
+
+	/**
+	 * String me.
+	 */
+	public String toString() {
+		StringBuffer sb=new StringBuffer();
+		sb.append(super.toString());
+		sb.append(" - Outstanding TTLs:  ");
+		sb.append(ttls.size());
+		return(sb.toString());
+	}
+
+	private boolean shouldIKeepRunning() {
+		long now=System.currentTimeMillis();
+		return((lastAddition+MAX_QUIESCENCE) > now);
+	}
+
+	/**
+	 * Monitor the TTLs.
+	 */
+	public void run() {
+		while(shouldIKeepRunning()) {
+			synchronized(ttls) {
+				for(Iterator i=ttls.iterator(); i.hasNext(); ) {
+					DBTTL ttl=(DBTTL)i.next();
+					ttl.report();
+					if(ttl.isClosed()) {
+						i.remove();
+					} // closed
+				} // Iterator
+			} // Lock
+
+			try {
+				sleep(NAPTIME);
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
