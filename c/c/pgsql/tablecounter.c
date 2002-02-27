17ebc02f9a12d0787d69a1709536473174df6196
Dustin Sallings <dustin@spy.net>
2002-02-27 18:32:39 +0000

    aaaaand this one actually does work.
    Author: dustin
    Date: 2002/02/27 10:32:39
    
    imported from: dustin@spy.net--projects/snippets--c--1.0--patch-369

--- c/c/pgsql/tablecounter.c
+++ c/c/pgsql/tablecounter.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2002  Dustin Sallings <dustin@spy.net>
  *
- * $Id: tablecounter.c,v 1.3 2002/02/27 10:31:19 dustin Exp $
+ * $Id: tablecounter.c,v 1.4 2002/02/27 10:32:39 dustin Exp $
  */
 
 #include <stdio.h>
@@ -52,7 +52,7 @@ void printResults(struct checkspec query, time_t t, int nrows)
 /*
 	printf("%lu %s.%s: %d\n", (int)t, query.dbname, query.table, nrows);
 */
-	printf("rrdupdate %s.%s.rrd %d:%d\n",
+	printf("update %s.%s.rrd %d:%d\n",
 		query.dbname, query.table, (int)t, nrows);
 	fflush(stdout);
 }
