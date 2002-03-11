cf61a06f2b359067bd274721a431a893fc7c16a8
Dustin Sallings <dustin@spy.net>
2002-03-11 18:44:33 +0000

    new stats module
    Author: dustin
    Date: 2002/03/11 10:44:33
    
    imported from: dustin@spy.net--projects/snippets--python--1.0--patch-5

---
+++ python/python/tiger/Stats.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import time
+
+class Stats:
+	def __init__(self, total):
+		self.done=0
+		self.left=total
+		self.startTime=time.time()
+		self.totaltime=0
+		self.lastproctime=0
+		self.lasttime=0
+
+	def click(self):
+		self.left=self.left-1
+
+	def start(self):
+		self.lasttime = time.time()
+
+	def stop(self):
+		t=time.time()
+		self.lastproctime = t-self.lasttime
+		self.done= self.done +1
+		self.totaltime = self.totaltime + self.lastproctime
+
+	def getLastTime(self):
+		return str(self.lastproctime) + "s"
+
+	def getStats(self):
+		avgproctime= self.totaltime / self.done
+		estimate = avgproctime * self.left
+
+		print "Total time is " + str(self.totaltime)
+
+		rv="Avg=%.2fs, Remaining: %d, est %.2fs (%s)" % \
+			(avgproctime, self.left, estimate, \
+				time.ctime( time.time() + estimate ))
+		return(rv)
+
+def main():
+	stats=Stats(60)
+	for i in range(60):
+		stats.start()
+		time.sleep(1)
+		stats.stop()
+		stats.click()
+		print stats.getStats()
+
+if __name__ == "__main__":
+	main()
