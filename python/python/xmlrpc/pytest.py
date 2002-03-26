70971907b0c0586b352c1e07eb0aa3ac94d612d9
Dustin Sallings <dustin@spy.net>
2002-03-26 19:10:05 +0000

    Round trip time testing.
    Author: dustin
    Date: 2002/03/26 11:10:05
    
    imported from: dustin@spy.net--projects/snippets--python--1.0--patch-43

--- python/python/xmlrpc/pytest.py
+++ python/python/xmlrpc/pytest.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 #
-# $Id: pytest.py,v 1.1 2002/03/08 02:02:35 dustin Exp $
+# $Id: pytest.py,v 1.2 2002/03/26 11:10:05 dustin Exp $
 
 from sys import argv
 import xmlrpclib
-import hmac
+import time
 
 server=xmlrpclib.Server('http://localhost:8000/RPC2')
 
+totaltime=0
+runs=100
+
 method = getattr(server, argv[1])
-print method(argv[2:], 'other arg')
+for i in range(runs):
+	starttime=time.time()
+	print method('other arg call #' + str(i))
+	stoptime=time.time()
+	print "RTT:  " + str(stoptime-starttime)
+	totaltime = totaltime + (stoptime-starttime)
+
+print "Average RTT:  " + str(totaltime/runs)
