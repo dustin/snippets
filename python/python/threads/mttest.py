7cc7888f8d942c0d517c9213340e725972fd669f
Dustin Sallings <dustin@spy.net>
2002-03-26 03:55:00 +0000

    Woo, figured out how static works.
    Author: dustin
    Date: 2002/03/25 19:55:00
    
    imported from: dustin@spy.net--projects/snippets--python--1.0--patch-42

--- python/python/threads/mttest.py
+++ python/python/threads/mttest.py
@@ -17,10 +17,15 @@ class TestThing:
 
 class TestThread(threading.Thread):
 
-	def __init__(self, id, m, tt):
+	mutex=None
+
+	def __init__(self, id, tt):
 		threading.Thread.__init__(self)
+		# Instantiate once
+		if TestThread.mutex == None:
+			TestThread.mutex = threading.Lock()
+
 		self._threadId=id
-		self.mutex=m
 		self.tt=tt
 		self.setName("TestThread #" + str(self._threadId))
 		self.setDaemon(1)
@@ -28,13 +33,13 @@ class TestThread(threading.Thread):
 	def getLockedResource(self):
 		# print self.getName() + " waiting for lock."
 
-		self.mutex.acquire()
+		TestThread.mutex.acquire()
 		# print self.getName() + " obtained lock."
 		self.tt.increment()
 		# time.sleep(.25)
 		# print self.getName() + " unlocking."
 		self.tt.decrement()
-		self.mutex.release()
+		TestThread.mutex.release()
 
 	def run(self):
 		i=0
@@ -42,12 +47,10 @@ class TestThread(threading.Thread):
 			self.getLockedResource()
 			i+=1
 			# print "Thread running:  " + self.getName() + " i=" + str(i)
-			# time.sleep(1)
 
 if __name__ == '__main__':
-	m=threading.Lock()
 	tt=TestThing()
 	for i in range(0,10):
-		t=TestThread(i, m, tt)
+		t=TestThread(i, tt)
 		t.start()
 	time.sleep(60)
