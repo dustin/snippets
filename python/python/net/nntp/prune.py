0747e4ba95d8e1f736ef030de2822c343a902ee9
Dustin Sallings <dustin@spy.net>
2002-04-23 16:33:44 +0000

    New Prune.
    Author: dustin
    Date: 2002/04/23 08:33:44
    
    imported from: dustin@spy.net--projects/snippets--python--1.0--patch-87

--- python/python/net/nntp/prune.py
+++ python/python/net/nntp/prune.py
@@ -1,26 +1,33 @@
 #!/usr/bin/env python
 
+from __future__ import generators
+
 import anydbm
 import time
+
+def walkDbm(dbm):
+	finished=None
+	k,v=dbm.first()
+	while 1:
+		yield k,v
+		try:
+			k,v=dbm.next()
+		except KeyError:
+			raise StopIteration
 
 olddb=anydbm.open("newsdb")
 newdb=anydbm.open("newsdb.new", "c")
 
 n=time.time()
-for k in olddb.keys():
-	try:
-		if k[0]=='a':
-			age=n-float(olddb[k])
-			# print "Age of " + k + ":  " + str(age)
-			# Only remember stuff less than 14 days
-			if age < (14*86400):
-				newdb[k]=olddb[k]
-			else:
-				print "Age of " + k + ":  " + str(age) + ", removing."
+for k,v in walkDbm(olddb):
+	if k[0]=='a':
+		age=n-float(v)
+		# print "Age of " + k + ":  " + str(age)
+		# Only remember stuff less than 14 days
+		if age < (14*86400):
+			newdb[k]=v
 		else:
-			# Not an article
-			newdb[k]=olddb[k]
-	except KeyError, ke:
-		print "Key error on " + k + "(" + str(ke) + ")"
-	except ValueError, ve:
-		print "Value error on " + k + "(" + str(ve) + ")"
+			print "Age of " + k + ":  " + str(age) + ", removing."
+	else:
+		# Not an article
+		newdb[k]=v
