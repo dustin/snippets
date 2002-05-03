ad8ea008e2e8f87e7d3ac4dacfebde0eb39a440a
Dustin Sallings <dustin@spy.net>
2002-05-03 07:01:51 +0000

    Allocator!
    Author: dustin
    Date: 2002/05/02 23:01:51
    
    imported from: dustin@spy.net--projects/snippets--c--1.0--patch-388

--- c/c/misc/allocator.c
+++ c/c/misc/allocator.c
@@ -1,10 +1,15 @@
+/*
+ * Copyright (c) 2002  Dustin Sallings <dustin@spy.net>
+ *
+ * $Id: allocator.c,v 1.2 2002/05/02 23:01:51 dustin Exp $
+ */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-#ifndef MAX_SIZE
-#define MAX_SIZE 1024*1024*1024
-#endif /* MAX_SIZE */
+#ifndef DEFAULT_MAX_SIZE
+#define DEFAULT_MAX_SIZE 1024*1024*1024
+#endif /* DEFAULT_MAX_SIZE */
 
 #define SUCCESS 2
 #define FAIL 3
@@ -34,15 +39,28 @@ main(int argc, char **argv)
 {
 	int i=0;
 	int incramount=128*1024*1024;
+	int maxsize=DEFAULT_MAX_SIZE;
 
-	for(i=incramount; incramount>=1 && i<MAX_SIZE; i+=incramount) {
+	/* Stay within the constraints of the user */
+	if(argc>1) {
+		maxsize=atoi(argv[1]);
+		if(maxsize<1) {
+			maxsize=DEFAULT_MAX_SIZE;
+		}
+	}
+
+	/* Make sure we aren't starting out asking for too much memory */
+	while(incramount >= maxsize) {
+		incramount>>=1;
+	}
+
+	printf("Allocating no more than %dk of memory in increments of %dk\n",
+		maxsize/1024, incramount/1024);
+
+	for(i=incramount; incramount>=1 && i<=maxsize; i+=incramount) {
 		if(allocate(i) != SUCCESS) {
 			i-=incramount;
-			if(incramount==1) {
-				incramount=0;
-			} else {
-				incramount/=2;
-			}
+			incramount>>=1;
 			printf("Counting up from %dk (by %d)\n", i/1024, incramount);
 		}
 	}
