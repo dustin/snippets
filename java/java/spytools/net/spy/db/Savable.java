ae86f6b4205a79d37f4bd1d994b26dd82bdcf8cc
Dustin Sallings <dustin@spy.net>
2002-07-22 14:41:41 +0000

    SQLException is also acceptable
    Author: dustin
    Date: 2002/07/22 06:41:41
    
    imported from: dustin@spy.net--projects/snippets--java--1.0--patch-610

--- java/java/spytools/net/spy/db/Savable.java
+++ java/java/spytools/net/spy/db/Savable.java
@@ -1,11 +1,12 @@
 // Copyright (c) 2001  Dustin Sallings <dustin@spy.net>
 //
-// $Id: Savable.java,v 1.1 2002/07/22 06:11:44 dustin Exp $
+// $Id: Savable.java,v 1.2 2002/07/22 06:41:41 dustin Exp $
 
 package net.spy.db;
 
 import java.util.Enumeration;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 /**
  * Interface for transactionally savable objects.
@@ -28,7 +29,8 @@ public interface Savable {
 	 * @param conn the connection to use for saving
 	 * @param context SaveContext being used in this Saver session
 	 */
-	void save(Connection conn, SaveContext context) throws SaveException;
+	void save(Connection conn, SaveContext context)
+		throws SaveException, SQLException;
 
 	/**
 	 * Get a list of all of the Savables this Savable is holding that will
