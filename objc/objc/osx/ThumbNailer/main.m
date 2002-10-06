69167c7e775d183edb7e54c69eaf8e689c3201f2
Dustin Sallings <dustin@spy.net>
2002-10-06 16:21:19 +0000

    ThumbNailer, initial version.
    Author: dustin
    Date: 2002/10/06 08:21:19
    
    imported from: dustin@spy.net--projects/snippets--objc--1.0--patch-29

---
+++ objc/objc/osx/ThumbNailer/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ThumbNailer
+//
+//  Created by Dustin Sallings on Sat Oct 05 2002.
+//  Copyright (c) 2002 SPY internetworking. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char *argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
