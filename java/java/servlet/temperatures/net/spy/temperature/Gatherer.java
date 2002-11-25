6d642d61c3672da1ccc3a4fa9d6d8282749d4849
Dustin Sallings <dustin@spy.net>
2002-11-25 15:23:08 +0000

    Better toString()
    Author: dustin
    Date: 2002/11/25 07:23:08
    
    imported from: dustin@spy.net--projects/snippets--java--1.0--patch-714

--- java/java/servlet/temperatures/net/spy/temperature/Gatherer.java
+++ java/java/servlet/temperatures/net/spy/temperature/Gatherer.java
@@ -1,6 +1,6 @@
 // Copyright (c) 2002  Dustin Sallings <dustin@spy.net>
 //
-// $Id: Gatherer.java,v 1.2 2002/11/25 01:54:19 dustin Exp $
+// $Id: Gatherer.java,v 1.3 2002/11/25 07:23:08 dustin Exp $
 
 package net.spy.temperature;
 
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.ResourceBundle;
 import java.util.MissingResourceException;
 import java.util.StringTokenizer;
@@ -34,6 +35,8 @@ public class Gatherer extends SpyThread {
 	private ResourceBundle serials=null;
 
 	private static Gatherer instance=null;
+
+	private static final long MAX_AGE=900000;
 
 	/**
 	 * Get an instance of Gatherer.
@@ -102,7 +105,19 @@ public class Gatherer extends SpyThread {
 	 * unavailable
 	 */
 	public Double getSeen(String name) {
-		Double rv=(Double)seen.get(name);
+		Double rv=null;
+		Sample s=(Sample)seen.get(name);
+
+		if(s != null) {
+			if(s.age() > MAX_AGE) {
+				getLogger().warn("Reading for "
+					+ name + " is too old, removing");
+				seen.remove(name);
+			} else {
+				// This one is of a reasonable vintage, return it
+				rv=s.getSample();
+			}
+		}
 
 		return(rv);
 	}
@@ -121,20 +136,46 @@ public class Gatherer extends SpyThread {
 		String serial = st.nextToken();
 		String sample_str = st.nextToken();
 
-		Double sample=new Double(sample_str);
+		Double sample_val=new Double(sample_str);
 
+		// The key for the storage
+		String key=null;
+
+		// figure out if there's a key mapping
 		try {
-			seen.put(serials.getString(serial), sample);
+			key=serials.getString(serial);
 			if(getLogger().isDebugEnabled()) {
-				getLogger().debug("Got sample for "
-					+ serials.getString(serial) + " " + sample);
+				getLogger().debug("Got sample for " + key + " " + sample_val);
 			}
 		} catch(MissingResourceException e) {
 			getLogger().warn("Unknown serial number seen:  " + serial, e);
-			seen.put(serial, sample);
+			key=serial;
 		}
 
+		// See if we need to update an existing key, or add a new one
+		Sample sample=(Sample)seen.get(key);
+		if(sample==null) {
+			sample=new Sample(key);
+		}
+		sample.setSample(sample_val);
+
+		// Store it
+		seen.put(key, sample);
+
 		updates++;
+	}
+
+	// Clean up any old entries
+	private void cleanup() {
+		for(Iterator i=seen.entrySet().iterator(); i.hasNext(); ) {
+			Map.Entry me=(Map.Entry)i.next();
+
+			Sample s=(Sample)me.getValue();
+			if(s.age() > MAX_AGE) {
+				getLogger().warn("Removing old entry:  " + s);
+				i.remove();
+			}
+		}
 	}
 
 	/**
@@ -148,11 +189,45 @@ public class Gatherer extends SpyThread {
 				s.receive(recv);
 				process(recv);
 
+				cleanup();
+
 			} catch(IOException e) {
 				getLogger().error("Exception processing temperature packet", e);
 			} catch(Throwable t) {
 				getLogger().fatal("UNEXPECTED ERROR IN GATHERER", t);
 			}
+		}
+	}
+
+	private class Sample extends Object {
+
+		private String name=null;
+		private long birthday=0;
+		private long modtime=0;
+		private Double sample=null;
+
+		private Sample(String name) {
+			super();
+			this.name=name;
+			birthday=System.currentTimeMillis();
+		}
+
+		public String toString() {
+			return("{Sample:  " + name + " val=" + sample
+				+ " age=" + age() + "}");
+		}
+
+		public long age() {
+			return(System.currentTimeMillis()-modtime);
+		}
+
+		public Double getSample() {
+			return(sample);
+		}
+
+		public void setSample(Double s) {
+			sample=s;
+			modtime=System.currentTimeMillis();
 		}
 	}
 
