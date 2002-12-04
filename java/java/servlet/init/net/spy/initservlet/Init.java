909d5fadde9a160ccea2e10580da2f1027534777
Dustin Sallings <dustin@spy.net>
2002-12-04 17:25:54 +0000

    Added cron stuff. Got rid of Makefile. Added build.xml
    Author: dustin
    Date: 2002/12/04 09:25:54
    
    imported from: dustin@spy.net--projects/snippets--java--1.0--patch-718

--- java/java/servlet/init/net/spy/initservlet/Init.java
+++ java/java/servlet/init/net/spy/initservlet/Init.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 1999 Dustin Sallings
  *
- * $Id: Init.java,v 1.1 2002/02/20 09:17:34 dustin Exp $
+ * $Id: Init.java,v 1.2 2002/12/04 09:25:57 dustin Exp $
  */
 
 package net.spy.initservlet;
@@ -13,11 +13,20 @@ import java.util.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+import net.spy.cron.Job;
+import net.spy.cron.MainJob;
+import net.spy.cron.JobQueue;
+import net.spy.cron.Cron;
+import net.spy.cron.SimpleTimeIncrement;
+
+import net.spy.pagermusic.RunSubs;
+
 /**
  * Perform global servlet engine initialization.
  */
 public class Init extends HttpServlet
 {
+	private Cron cron=null;
 
 	/**
 	 * Just perform basic initialization.
@@ -29,5 +38,36 @@ public class Init extends HttpServlet
 		Locale l=Locale.getDefault();
 
 		log("Default locale is now " + l);
+
+		startCron();
 	}
+
+	private void startCron() {
+		JobQueue jq=new JobQueue();
+		cron=new Cron(jq);
+		jq.addJob(getJob());
+	}
+
+	private Job getJob() {
+		Calendar c=Calendar.getInstance();
+		c.setTime(new Date());
+		c.set(Calendar.HOUR_OF_DAY, 9);
+		c.set(Calendar.MINUTE, 0);
+		c.set(Calendar.SECOND, 0);
+		c.set(Calendar.MILLISECOND, 0);
+		Date d=c.getTime();
+
+		String args[]={};
+		MainJob j=new MainJob(RunSubs.class.getName(), args,
+			d, new SimpleTimeIncrement(86400000));
+		return(j);
+	}
+
+	/**
+	 * Shut down.
+	 */
+	public void destroy() {
+		cron.shutdown();
+	}
+
 }
