f71325515365beae362058222fef6f886964e2a2
Dustin Sallings <dustin@spy.net>
2002-12-09 17:27:28 +0000

    Beginning of my Time class in OCaml.
    Author: dustin
    Date: 2002/12/09 09:27:28
    
    imported from: dustin@spy.net--projects/snippets--ocaml--1.0--patch-1

---
+++ ocaml/ocaml/misc/Time.ml
@@ -0,0 +1,31 @@
+(* Time parsing *)
+
+module Time =
+
+	struct
+
+	class time =
+		object (self)
+			val mutable year = 0
+			val mutable month = 0
+			val mutable day = 0
+			val mutable hour = 0
+			val mutable minute = 0
+			val mutable second = 0
+
+			method getYear = year
+			method getMonth = month
+			method getDay = day
+			method getHour = hour
+			method getMinute = minute
+			method getSecond = second
+
+			method setYear y = year <- y
+			method setMonth m = month <- m
+			method setDay d = day <- d
+			method setHour h = hour <- h
+			method setMinute m = minute <- m
+			method setSecond s = second <- s
+	end;;
+
+end;;
