9584ceb5c98eb846a1b66e5de2df45934a7b3c0b
Dustin Sallings <dustin@spy.net>
2002-12-11 17:55:57 +0000

    Interface for string utils.
    Author: dustin
    Date: 2002/12/11 09:55:57
    
    imported from: dustin@spy.net--projects/snippets--ocaml--1.0--patch-9

---
+++ ocaml/ocaml/lib/stringutils.mli
@@ -0,0 +1,8 @@
+(*
+ * Copyright (c) 2002  Dustin Sallings <dustin@spy.net>
+ *
+ * $Id: stringutils.mli,v 1.1 2002/12/11 09:55:57 dustin Exp $
+ *)
+
+(* Split a string *)
+val split : string char -> '_a array;;
