9f209d7b4d8af2bb501457421f3a93d820bbde37
Dustin Sallings <dustin@spy.net>
2002-12-11 17:49:58 +0000

    Tail recursive version not using streams..
    Author: dustin
    Date: 2002/12/11 09:49:58
    
    imported from: dustin@spy.net--projects/snippets--ocaml--1.0--patch-8

--- ocaml/2wire/ocaml/lib/stringutils.ml
+++ ocaml/2wire/ocaml/lib/stringutils.ml
@@ -1,46 +1,57 @@
 (*
  * Copyright (c) 2002  Dustin Sallings <dustin@spy.net>
  *
- * $Id: stringutils.ml,v 1.2 2002/12/11 09:18:01 dustin Exp $
+ * $Id: stringutils.ml,v 1.3 2002/12/11 09:49:58 dustin Exp $
  *)
 
 (* Private function to skip the next n occurrences of this character in
- * this stream
+ * this stream.  Return the new offset.
  *)
-let pvt_skip_char(t, c) =
-	while (Stream.npeek 1 t) = [ c ] do
-		Stream.next t
-	done
+let rec pvt_skip_char(s, c, i): int =
+	if (i >= String.length s ) then
+		String.length s
+	else
+		if ((String.get s i) = c) then
+			pvt_skip_char(s, c, i+1)
+		else
+			i
 ;;
 
 (* Private recursive function for splitting a stream in a buffer *)
-let rec pvt_rec_split_stream(str, rv, t, c): '_a list =
-	if Stream.peek t != None then
+let rec pvt_rec_split(buf, rv, str, c, i): '_a list =
+	if i < String.length str then
 	begin
-		let ch = Stream.next t in
+		let ch = String.get str i in
 		if ch = c then
 			begin
-				pvt_skip_char(t, c);
-				pvt_rec_split_stream("", (rv @ [ str ]), t, c);
+				let s = (Buffer.contents buf) in
+				Buffer.reset buf;
+				pvt_rec_split(buf, (rv @ [ s ]), str, c,
+					pvt_skip_char(str, c, i));
 			end
 		else
 			begin
-				pvt_rec_split_stream(str ^ (String.make 1 ch), rv, t, c);
+				Buffer.add_char buf ch;
+				pvt_rec_split(buf, rv, str, c, (i + 1));
 			end
 	end
 	else
-		rv @ [ str ]
-;;
-
-(* Split a stream on a character. *)
-let split_stream(t, c): '_a list =
-	pvt_rec_split_stream("", [], t, c)
+		if String.length (Buffer.contents buf) > 0 then
+			rv @ [ Buffer.contents buf ]
+		else
+			rv
 ;;
 
 (* Split a string into a list of Strings *)
 let split(s, c) =
-	split_stream(Stream.of_string(s), c)
+	pvt_rec_split((Buffer.create 128), [], s, c, 0)
 ;;
+
+(*
+ * Test:
+ * split("123 456   789", ' ');;
+ * split("123 456   789  ", ' ');;
+ *)
 
 (* Locate a string in another string *)
 let strstr(source, dest): int =
