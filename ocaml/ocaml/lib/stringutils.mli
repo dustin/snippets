375eb4b891aca8364ee4c6e15b86ddbba7660f01
Dustin Sallings <dustin@spy.net>
2002-12-11 19:19:14 +0000

    Fixed this syntax.
    Author: dustin
    Date: 2002/12/11 11:19:14
    
    imported from: dustin@spy.net--projects/snippets--ocaml--1.0--patch-13

--- ocaml/ocaml/lib/stringutils.mli
+++ ocaml/ocaml/lib/stringutils.mli
@@ -1,11 +1,11 @@
 (*
  * Copyright (c) 2002  Dustin Sallings <dustin@spy.net>
  *
- * $Id: stringutils.mli,v 1.2 2002/12/11 11:13:23 dustin Exp $
+ * $Id: stringutils.mli,v 1.3 2002/12/11 11:19:14 dustin Exp $
  *)
 
 (* Split a string *)
 val split : string char -> '_a array;;
 
 (* Split a string on any character found in the provided list *)
-val split_chars : string '_a array -> '_a array;;
+val split_chars : string -> '_a array -> '_a array;;
