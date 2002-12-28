0936e182bcf3d87ee2a967e6ff8e4d73c73932e0
Dustin Sallings <dustin@spy.net>
2002-12-28 13:37:16 +0000

    Port argument is optional.
    Author: dustin
    Date: 2002/12/28 05:37:16
    
    imported from: dustin@spy.net--projects/snippets--scheme--1.0--patch-27

--- scheme/scheme/misclib.scm
+++ scheme/scheme/misclib.scm
@@ -1,21 +1,40 @@
 ; Copyright (c) 2002  Dustin Sallings <dustin@spy.net>
 ;
-; $Id: misclib.scm,v 1.2 2002/12/28 03:34:23 dustin Exp $
+; $Id: misclib.scm,v 1.3 2002/12/28 05:37:16 dustin Exp $
 
 (module misclib
 		(export input-loop))
 
-; Loop on input, pass each line to function f
-(define (input-loop f p)
+; Loop on input, pass each line to function f.
+; Optional argument should be either a port, or a filename.  If not
+; provided, (current-input-port) will be used.
+(define (input-loop f . other)
   ; Check argument types
-  (if (not (port? p))
-	(error "input-loop" "not a port" p))
   (if (not (procedure? f))
 	(error "input-loop" "not a procedure" f))
+  ; If a port was passed in, use it, otherwise use stdin
+  (let ((p (if (null? other)
+			 (current-input-port)
+			 (car other))))
+	; Figure out what it was, make it a port
+	(set! p
+	  (cond
+		((input-port? p) p)
+		((string? p) (open-input-file p))
+		(else (error "input-loop" "not a port or filename" p))))
+	; Make sure
+	(if (not (input-port? p))
+	  (error "input-loop" "couldn't turn argument into a port" (car other)))
 
-  ; Do the loop
-  (let loop ((line (read-line p)))
-	(if (not (eof-object? line))
-	  (begin
-		(f line)
-		(loop (read-line p))))))
+	; Do the loop
+	(let loop ((line (read-line p)))
+	  (if (not (eof-object? line))
+		(begin
+		  (f line)
+		  (loop (read-line p)))))))
+
+; Example
+(define (misclib-main args)
+  (if (> (length args) 1)
+	(input-loop print (cadr args))
+	(input-loop print)))
