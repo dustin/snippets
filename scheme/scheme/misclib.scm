9f1057e6bf99878483dc73b59e8d949bcc5cf09e
Dustin Sallings <dustin@spy.net>
2002-12-28 11:34:23 +0000

    Updated the comments a bit.
    Author: dustin
    Date: 2002/12/28 03:34:23
    
    imported from: dustin@spy.net--projects/snippets--scheme--1.0--patch-26

--- scheme/scheme/misclib.scm
+++ scheme/scheme/misclib.scm
@@ -1,17 +1,19 @@
 ; Copyright (c) 2002  Dustin Sallings <dustin@spy.net>
 ;
-; $Id: misclib.scm,v 1.1 2002/12/28 02:06:23 dustin Exp $
+; $Id: misclib.scm,v 1.2 2002/12/28 03:34:23 dustin Exp $
 
 (module misclib
 		(export input-loop))
 
 ; Loop on input, pass each line to function f
 (define (input-loop f p)
+  ; Check argument types
   (if (not (port? p))
 	(error "input-loop" "not a port" p))
   (if (not (procedure? f))
 	(error "input-loop" "not a procedure" f))
 
+  ; Do the loop
   (let loop ((line (read-line p)))
 	(if (not (eof-object? line))
 	  (begin
