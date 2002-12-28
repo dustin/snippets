11f869aa3525866af30e79e0b7f7fcd23f1c4d7b
Dustin Sallings <dustin@spy.net>
2002-12-28 19:47:18 +0000

    Added some more time stuff.
    Author: dustin
    Date: 2002/12/28 11:47:18
    
    imported from: dustin@spy.net--projects/snippets--scheme--1.0--patch-34

--- scheme/scheme/misc/loglib.scm
+++ scheme/scheme/misc/loglib.scm
@@ -1,12 +1,15 @@
 ; Copyright (c) 2002  Dustin Sallings <dustin@spy.net>
 ;
-; $Id: loglib.scm,v 1.1 2002/12/28 06:24:38 dustin Exp $
+; $Id: loglib.scm,v 1.2 2002/12/28 11:47:18 dustin Exp $
 
 (module loglib
 	  (import
 		(stringlib "../stringlib.scm")
 		(dates "../dates.scm"))
-		(export parse-2wire-date))
+		(export
+		  parse-2wire-date
+		  parse-2wire-date-withmillis
+		  approx-time))
 
 ; Parse the date out of the given line
 (define (parse-2wire-date line)
@@ -17,3 +20,12 @@
 				'(#\: #\space #\-)
 				19)))
 	 28800))
+
+; Parse the date out of the given line (including milliseconds)
+(define (parse-2wire-date-withmillis line)
+  (+ (parse-2wire-date line)
+	(/ (string->real (substring line 20 23)) 1000.0)))
+
+; Truncate a date to the nearest minute
+(define (approx-time x)
+  (* 60 (truncate (/ x 60))))
