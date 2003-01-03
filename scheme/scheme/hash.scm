a738b78590b7a97e18f8135246d62149b97d7164
Dustin Sallings <dustin@spy.net>
2003-01-03 07:27:53 +0000

    Listing stuff and a new hashing algorithm.
    Author: dustin
    Date: 2003/01/02 23:27:53
    
    imported from: dustin@spy.net--projects/snippets--scheme--1.0--patch-46

--- scheme/scheme/hash.scm
+++ scheme/scheme/hash.scm
@@ -1,6 +1,6 @@
 ; Copyright (c) 2002  Dustin Sallings <dustin@spy.net>
 ;
-; $Id: hash.scm,v 1.1 2003/01/02 08:27:12 dustin Exp $
+; $Id: hash.scm,v 1.2 2003/01/02 23:27:53 dustin Exp $
 
 ; Generic hash table thing
 
@@ -8,6 +8,15 @@
 
 ; Crappy routine to compute a hash value for a string
 (define (hash-string s)
+  (abs (let loop ((l (string->list s)))
+	(if (null? l)
+	  0
+	  (+ (char->integer (car l))
+			  (* 31 (loop (cdr l))))))))
+
+; Old string hasher.  Not compatible with the new one, but similar...I'm
+; keeping this around because lispme doesn't support that let loop construct
+(define (old-hash-string s)
   (let ((rv 0))
 	(for-each
 	  (lambda (c)
@@ -39,9 +48,13 @@
 (define (hashtbl? o)
   (and (pair? o) (eq? 'hashtbl (car o))))
 
-(define (hashtbl-put! ht k v)
+; Verify a hashtbl is a hashtbl
+(define (hashtbl-assert-ht ht whence)
   (if (not (hashtbl? ht))
-	(error "hashtbl-put!" "That's not a hashtbl" ht))
+	(error whence "That's not a hashtbl" ht)))
+
+(define (hashtbl-put! ht k v)
+  (hashtbl-assert-ht ht "hashtbl-put!")
 
   (let* ((bucket-id (modulo (hash k) (vector-length (cdr ht)))))
 	(vector-set! (cdr ht) bucket-id
@@ -50,8 +63,7 @@
 
 ; Get the key/value pair for the given key (or #f)
 (define (hashtbl-get-pair ht k)
-  (if (not (hashtbl? ht))
-	(error "hashtbl-get" "That's not a hashtbl" ht))
+  (hashtbl-assert-ht ht "hashtbl-get-pair")
 
   (let* ((bucket-id (modulo (hash k) (vector-length (cdr ht))))
 		 (bucket (vector-ref (cdr ht) bucket-id)))
@@ -81,3 +93,15 @@
 						   (not (equal? (car x) k)))
 						 (vector-ref (cdr ht) bucket-id)))))
 
+; Get all keys and values from the given hashtable
+(define (hashtbl-keys-values ht)
+  (hashtbl-assert-ht ht "hashtbl-keys-values")
+  (apply append (vector->list (cdr ht))))
+
+; Get the keys from the given hashtable
+(define (hashtbl-keys ht)
+  (map car (hashtbl-keys-values ht)))
+
+; Get the keys from the given hashtable
+(define (hashtbl-values ht)
+  (map cdr (hashtbl-keys-values ht)))
