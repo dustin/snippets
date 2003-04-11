89fe4074f47e4317c8e0cb81b5fee9b5726225b0
Dustin Sallings <dustin@spy.net>
2003-04-11 05:30:14 +0000

    More Haskell samples.
    Author: dustin
    Date: 2003/04/10 21:30:14
    
    imported from: dustin@spy.net--projects/snippets--haskell--1.0--patch-2

--- haskell/haskell/samples/dbquote.hs
+++ haskell/haskell/samples/dbquote.hs
@@ -1,7 +1,7 @@
 
 -- Quote a string for DB inclusion
 dbQuote :: String -> String
-dbQuote s = "'" ++ (concat (map (dbQuoteHelper) s)) ++ "'"
+dbQuote s = "'" ++ concat(map dbQuoteHelper s) ++ "'"
 	where dbQuoteHelper c
 		| c == '\'' = "''"
 		| otherwise = [c]
---
+++ haskell/haskell/samples/prime.hs
@@ -0,0 +1,14 @@
+
+isPrime :: Integer -> Bool
+
+isPrime 0 = False
+isPrime 1 = False
+isPrime 2 = True
+isPrime x
+	| even(x)   = False
+	| otherwise = rprime (x - 2)
+	where rprime a
+		| (a ^ 2) < x    = True
+		| mod x a == 0   = False
+		| otherwise      = rprime (a - 2)
+
