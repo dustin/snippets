e9da8ea82ebe2debc9b75d6fb52dbfcd0570fc25
Dustin Sallings <dustin@spy.net>
2003-04-12 15:07:42 +0000

    Wow, that was dumb, count up, not down.
    Author: dustin
    Date: 2003/04/12 07:07:42
    
    imported from: dustin@spy.net--projects/snippets--haskell--1.0--patch-3

--- haskell/haskell/samples/prime.hs
+++ haskell/haskell/samples/prime.hs
@@ -6,9 +6,9 @@ isPrime 1 = False
 isPrime 2 = True
 isPrime x
 	| even(x)   = False
-	| otherwise = rprime (x - 2)
-	where rprime a
-		| (a ^ 2) < x    = True
+	| otherwise = rprime(3)
+	where rprime(a)
+		| (a ^ 2) > x    = True
 		| mod x a == 0   = False
-		| otherwise      = rprime (a - 2)
+		| otherwise      = rprime(a + 2)
 
