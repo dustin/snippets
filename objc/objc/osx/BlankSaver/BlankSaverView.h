305a1a21a3811bd04b99a825c9407cd840706d2c
Dustin Sallings <dustin@spy.net>
2003-05-30 02:01:47 +0000

    Blank screensaver (doesn't eat the CPU).
    Author: dustin
    Date: 2003/05/29 18:01:47
    
    imported from: dustin@spy.net--projects/snippets--objc--1.0--patch-91

---
+++ objc/objc/osx/BlankSaver/BlankSaverView.h
@@ -0,0 +1,17 @@
+//
+//  BlankSaverView.h
+//  BlankSaver
+//
+//  Created by Dustin Sallings on Thu May 29 2003.
+//  Copyright (c) 2003, __MyCompanyName__. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+
+@interface BlankSaverView : ScreenSaverView
+{
+
+}
+
+@end
