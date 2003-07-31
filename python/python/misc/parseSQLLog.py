526ede59d758c35e05015fb8810cd7b35fd1de7b
Dustin Sallings <dustin@spy.net>
2003-07-31 12:43:43 +0000

    Stuff that hadn't been checked in.
    Author: dustin
    Date: 2003/07/31 04:43:43
    
    imported from: dustin@spy.net--projects/snippets--python--1.0--patch-136

--- python/python/misc/parseSQLLog.py
+++ python/python/misc/parseSQLLog.py
@@ -6,69 +6,77 @@ import string
 
 class LogEntry:
 
-    timeFormat = "%Y-%m-%d %H:%M:%S"
+	timeFormat = "%Y-%m-%d %H:%M:%S"
 
-    def __init__(self, line):
-        self.line=line
+	def __init__(self, line):
+		self.line=line
 
-        # Parse the timestamp
-        tmp=line[:19]
-        self.__timestamp=time.mktime(time.strptime(tmp, LogEntry.timeFormat))
+		# Parse the timestamp
+		tmp=line[:19]
+		self.__timestamp=time.mktime(time.strptime(tmp, LogEntry.timeFormat))
 
-        # OK, now verify it's the right type of data
-        string.index(line, "database.DBManager.sql")
+		# OK, now verify it's the right type of data
+		string.index(line, "database.DBManager.sql")
 
-        parts=string.split(line)
-        timings=parts[10]
+		parts=string.split(line)
+		timings=parts[10]
 
-        tparts=string.split(timings, '/')
-        self.__calls=int(tparts[1])
+		tparts=string.split(timings, '/')
+		self.__calls=int(tparts[1])
 
-        self.__calltime=int(tparts[0][:-2])
+		self.__calltime=int(tparts[0][:-2])
 
-    def getTimestamp(self):
-        """Get the time at which this log entry occurred."""
-        return self.__timestamp
+	def __str__(self):
+		"""Print a reasonable string version of this thing."""
+		return "Log entry:  " + str(self.__timestamp) \
+			+ " (" + str(self.getTimestampNearest(60)) + ") " \
+			+ str(self.__calls) + "/" + str(self.__calltime)
 
-    def getTimestampNearest(self, accuracy):
-        """Get the timestamp truncated to the given number of seconds."""
-        return( int(self.__timestamp/accuracy) * accuracy)
+	def getTimestamp(self):
+		"""Get the time at which this log entry occurred."""
+		return self.__timestamp
 
-    def getNumCalls(self):
-        return self.__calls
+	def getTimestampNearest(self, accuracy):
+		"""Get the timestamp truncated to the given number of seconds."""
+		return( int(self.__timestamp/accuracy) * accuracy)
 
-    def getCallTime(self):
-        return (self.__calltime / self.__calls)
+	def getNumCalls(self):
+		return self.__calls
+
+	def getCallTime(self):
+		return (self.__calltime / self.__calls)
 
 if __name__ == '__main__':
 
-    # lf=open(sys.argv[1])
+	# lf=open(sys.argv[1])
 
-    lastTime=0
-    totalCalls=0
-    totalTime=0
+	lastTime=0
+	totalCalls=0
+	totalTime=0
 
-    l=sys.stdin.readline()
-    while l != '':
-        try:
-            le=LogEntry(l)
+	l=sys.stdin.readline()
+	while l != '':
+		try:
+			le=LogEntry(l)
+			# print str(le)
 
-            t=le.getTimestampNearest(60)
+			t=le.getTimestampNearest(60)
 
-            if t!=lastTime and totalCalls > 0:
-                print "update " + sys.argv[1] + " " \
-                    + string.join(  (str(lastTime),
-                                    str(totalCalls),
-                                    str(totalTime)), ':')
-                totalCalls=0
-                totalTime=0
+			if t!=lastTime and totalCalls > 0:
+				print "update " + sys.argv[1] + " " \
+					+ string.join(	(str(lastTime),
+									str(totalCalls),
+									str(totalTime)), ':')
+				totalCalls=0
+				totalTime=0
 
-            lastTime=t
-            totalCalls = totalCalls + 1
-            totalTime = totalTime + le.getCallTime()
+			if le.getNumCalls() > 0:
+				lastTime=t
+				totalCalls = totalCalls + 1
+				totalTime = totalTime + le.getCallTime()
 
-        except:
-            e=sys.exc_info()
-            # print "Exception with " + l, str(e[1])
+		except:
+			e=sys.exc_info()
+			# print "Exception with " + l, str(e[1])
 
-        l=sys.stdin.readline()
+		l=sys.stdin.readline()
