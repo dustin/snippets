6104803cf9ebd1988fcdff0b6c8a423d34018ed5
Dustin Sallings <dustin@spy.net>
2003-09-04 14:54:26 +0000

    MDC scraper test.
    Author: dustin
    Date: 2003/09/04 06:54:26
    
    imported from: dustin@spy.net--projects/snippets--python--1.0--patch-140

---
+++ python/python/jython/mdc-test.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env jython
+
+import jarray
+import base64
+
+from java.lang import System
+
+from org.apache.commons.httpclient import HttpClient
+from org.apache.commons.httpclient import UsernamePasswordCredentials
+from org.apache.commons.httpclient.methods import GetMethod
+
+from org.apache.commons.httpclient import UsernamePasswordCredentials
+
+# SSL stuff
+from com.sun.net.ssl import SSLContext
+from com.sun.net.ssl import TrustManager
+from org.apache.commons.httpclient.protocol import SecureProtocolSocketFactory
+
+from com.sun.net.ssl import TrustManagerFactory
+from com.sun.net.ssl import TrustManager
+from com.sun.net.ssl import X509TrustManager
+from org.apache.commons.httpclient.protocol import Protocol
+
+class EasyX509TrustManager(X509TrustManager):
+
+	def isClientTrusted(self, certs):
+		return 1
+
+	def isServerTrusted(self, certs):
+		return 1
+
+	def getAcceptedIssuers(self):
+		return self.standardTrustManager.getAcceptedIssuers()
+
+class MySSLSocketFactory(SecureProtocolSocketFactory):
+
+	def createSocket(self, *args):
+		context = SSLContext.getInstance("SSL")
+		managers=jarray.array([EasyX509TrustManager()], X509TrustManager)
+		context.init(None, managers, None)
+		sf=context.getSocketFactory()
+
+		rv=None
+		if len(args) == 4:
+			rv=sf.createSocket(args[0], args[1], args[2], args[3])
+		elif len(args) == 2:
+			rv=sf.createSocket(args[0], args[1])
+		else:
+			raise "Don't know about " + `len(args)` + " args."
+		return rv
+
+# Main
+
+System.setProperty("org.apache.commons.logging.Log",
+	"org.apache.commons.logging.impl.SimpleLog")
+System.setProperty("org.apache.commons.logging.simplelog.showdatetime", "true")
+# System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.commons.httpclient.Authenticator", "trace")
+# System.setProperty("org.apache.commons.logging.simplelog.log.httpclient.wire",
+	# "debug")
+# System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.commons.httpclient", "debug");
+# System.setProperty("org.apache.commons.logging.simplelog.log", "trace")
+System.setProperty("org.apache.commons.logging.simplelog.defaultlog", "trace")
+System.setProperty("org.apache.commons.logging.simplelog.log.httpclient.wire",
+	"debug")
+
+Protocol.registerProtocol("https", Protocol("https", MySSLSocketFactory(), 443))
+client = HttpClient();
+# auth
+# client.getState().setAuthenticationPreemptive(1)
+client.getState().setCredentials("remote", "208.35.230.144",
+	UsernamePasswordCredentials("462211000079", "syhtob1ZmavwmayI8Y6GRg=="))
+
+get = GetMethod("https://208.35.230.144:50001/management/")
+# get = GetMethod("https://462211000079:syhtob1ZmavwmayI8Y6GRg%3D%3D@208.35.230.144:50001/management/")
+get.setHttp11(1)
+get.setDoAuthentication(1)
+
+status=client.executeMethod(get)
+
+# print get.getResponseBodyAsString()
+# print "Auth realm:  " + str(get.getAuthenticationRealm())
+get.releaseConnection()
