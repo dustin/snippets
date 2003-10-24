6488d48dd5ac6b87747bcee4a8c16def23c919c5
Dustin Sallings <dustin@spy.net>
2003-10-24 16:33:38 +0000

    I got a lot of the log processed. Need to work on output.
    Partials are also not supported.  There's a start and end that we were also
    keeping for something.  I should probably figure that one out, too.
    
    imported from: dustin@spy.net--projects/snippets--ocaml--1.0--patch-28

--- ocaml/2wire/misc/parseTimingLog.ml
+++ ocaml/2wire/misc/parseTimingLog.ml
@@ -6,6 +6,7 @@
 
 open Unix;;
 open Hashtbl;;
+open List;;
 open Stringutils;;
 
 (* The type for log entries *)
@@ -16,6 +17,7 @@ type log_entry = {
 	le_state: string;
 };;
 
+(* A timing log entry (start to stop) *)
 type log_timing = {
 	lt_start: float;
 	lt_stop: float;
@@ -23,11 +25,18 @@ type log_timing = {
 	lt_serial: string;
 };;
 
+(* The stuff we store in the massive per-block hash table *)
+type per_block = {
+	mutable pb_count: int;
+	mutable pb_time: float;
+};;
+
+(* The types of logs we consider *)
+let logTypes = ["HB"; "BOOT"; "KICK"; "XMLRPC";];;
+
 (* This is the hashtable that will hold the state and stuff *)
 let eventCache=Hashtbl.create 1;;
-(*
 let perBlock=Hashtbl.create 1;;
-*)
 
 (* Parse time from the given timestamp *)
 let parse_time(l: string): float =
@@ -45,9 +54,7 @@ let parse_time(l: string): float =
 		}) +. (float_of_string(List.nth times 6) /. 1000.0)
 ;;
 
-let approx_time(lt: log_timing): int =
-	60 * ((int_of_float lt.lt_start) / 60)
-;;
+let approx_time t = 60 * ((int_of_float t) / 60) ;;
 
 (* Get a log entry from the line *)
 let get_log_entry(l: string): log_entry =
@@ -71,8 +78,28 @@ let get_log_timing le1 le2 =
 	}
 ;;
 
+(* Block maker *)
+let make_block() = { pb_count = 0; pb_time = 0.0; }
+
+(* Get the block that contains the counts for the given log entry.
+ * This will also create any tables it needs.
+ *)
+let get_block le =
+	try
+		(* Look for the thing that matches this timestamp *)
+		let dateBlock = Hashtbl.find perBlock (approx_time le.le_time) in
+		Hashtbl.find dateBlock le.le_ttype;
+	with Not_found ->
+		(* If we didn't have a match, build it all out *)
+		let tmpHash = Hashtbl.create 1 in
+		List.iter (function x -> Hashtbl.add tmpHash x (make_block())) logTypes;
+		Hashtbl.add perBlock (approx_time le.le_time) tmpHash;
+		Hashtbl.find tmpHash le.le_ttype;
+;;
+
 (* Record the timing for this mofo *)
 let record_timing le_old le_new =
+	(*
 	prerr_string("Recording " ^ le_old.le_ttype ^ " of "
 		^ le_old.le_serial ^ " from ");
 	prerr_float(le_old.le_time);
@@ -81,18 +108,13 @@ let record_timing le_old le_new =
 	prerr_string(" ");
 	prerr_float(le_new.le_time -. le_old.le_time);
 	prerr_endline("s");
+	*)
 
-(*
 	let lt = get_log_timing le_old le_new in
-	try
-		let pb = Hashtbl.find perBlock lt.lt_serial in
-	with Not_found ->
-		Hashtbl.add perBlock approx_time(lt) {
-				pb_ts = approx_time(lt);
-				pb_counts = 1;
-				pb_times = (lt.lt_stop - lt.lt_start);
-			}
-*)
+	let pb = get_block le_old in
+	pb.pb_count <- pb.pb_count + 1;
+	pb.pb_time <- pb.pb_time +. (lt.lt_stop -. lt.lt_stop);
+	();
 ;;
 
 (* Process a given log entry *)
@@ -119,6 +141,22 @@ let process(le: log_entry) =
 	| _ -> raise (Invalid_argument le.le_state);
 ;;
 
+let hashtbl_keys h = Hashtbl.fold (fun key _ l -> key :: l) h [];;
+
+let print_block_header filename =
+	print_string("update " ^ filename ^ " -t ");
+	print_string(String.concat ":" (List.concat
+		(List.map (function x ->
+			(List.map (function y -> x ^ y) ["time";"count";"start";"end"]))
+			logTypes)));
+	print_string(" ");
+;;
+
+let print_output filename =
+	print_block_header filename;
+	(* XXX:  Print map here *)
+;;
+
 (* do it *)
 let main() =
 	let rrd = (Array.get Sys.argv 1) in
@@ -129,6 +167,7 @@ let main() =
 				process(get_log_entry(l))
 		done;
 	with End_of_file -> ();
+	print_output rrd;
 ;;
 
 (* Start main if we're interactive. *)
