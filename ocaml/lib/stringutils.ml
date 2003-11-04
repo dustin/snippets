817811dc247a6923fe068597a572663a72665c8b
Dustin Sallings <dsallings@2wire.com>
2003-11-04 16:07:19 +0000

    Updated with a better understanding of ocaml (see notes)
    function -> fun (function is for patterns)
    
    curried all the string functions and got rid of parameter definitions
    
    ocaml 3.07pl2 needed a couple of build changes
    
    imported from: dustin@spy.net--projects/snippets--ocaml--1.0--patch-33

--- ocaml/lib/stringutils.ml
+++ ocaml/lib/stringutils.ml
@@ -8,50 +8,51 @@
 Various string utilities that seemed to be missing from ocaml.
 *)
 
-let rec is_my_letter(l, c): bool =
+let rec is_my_letter l c =
 	if l = [] then
 		false
 	else
 		if (List.hd l) == c then
 			true
 		else
-			is_my_letter(List.tl l, c)
+			is_my_letter (List.tl l) c
 ;;
 
 (* Private function to skip the next n occurrences of any character in this
  * list in this string.  Return the new offset.
  *)
-let rec pvt_skip_chars(s, l, i): int =
+let rec pvt_skip_chars s l i =
 	if (i >= String.length s ) then
 		String.length s
 	else
-		if is_my_letter(l, String.get s i) then
-			pvt_skip_chars(s, l, i+1)
+		if is_my_letter l (String.get s i) then
+			pvt_skip_chars s l (i+1)
 		else
 			i
 ;;
 
 (* Find the index of one of these characters, or -1 if it doesn't exist *)
-let rec str_index_of_one(str, l, i): int =
+let rec str_index_of_one str l i =
 	if i < String.length str then
-		if is_my_letter(l, String.get str i) then
+		if is_my_letter l (String.get str i) then
 			i
 		else
-			str_index_of_one(str, l, (i + 1))
+			str_index_of_one str l (i + 1)
 	else
 		-1
 ;;
 
 (* Private recursive function for splitting a stream in a buffer *)
-let rec pvt_rec_split_chars(rv, str, l, i, limit): '_a list =
+let rec pvt_rec_split_chars rv str l i limit =
 	if (List.length rv < (limit-1)) && (i < String.length str) then
 	begin
-		let pos = str_index_of_one(str, l, i) in
+		let pos = str_index_of_one str l i in
 		if pos != -1 then
-			pvt_rec_split_chars(
-				(rv @ [ String.sub str i (pos - i)]),
-				str, l,
-				pvt_skip_chars(str, l, pos), limit)
+			pvt_rec_split_chars
+				(rv @ [ String.sub str i (pos - i)])
+				str l
+				(pvt_skip_chars str l pos)
+				limit
 		else
 			rv @ [ String.sub str i ((String.length str) - i) ]
 	end
@@ -65,28 +66,26 @@ let rec pvt_rec_split_chars(rv, str, l, i, limit): '_a list =
 (* Private function to skip the next n occurrences of this character in
  * this string.  Return the new offset.
  *)
-let rec pvt_skip_char(s, c, i): int =
+let rec pvt_skip_char s c i =
 	if (i >= String.length s ) then
 		String.length s
 	else
 		if ((String.get s i) == c) then
-			pvt_skip_char(s, c, i+1)
+			pvt_skip_char s c (i+1)
 		else
 			i
 ;;
 
 (* Private recursive function for splitting a stream in a buffer *)
-let rec pvt_rec_split(rv, str, c, i, limit): '_a list =
+let rec pvt_rec_split rv str c i limit =
 	if (List.length rv < (limit - 1)) && (i < String.length str) then
 	begin
 		if String.contains_from str i c then
-		begin
 			let o = String.index_from str i c in
-			pvt_rec_split(
-				(rv @ [ String.sub str i (o - i)]),
-				str, c,
-				pvt_skip_char(str, c, o), limit)
-		end
+			pvt_rec_split (rv @ [ String.sub str i (o - i)])
+				str c
+				(pvt_skip_char str c o)
+				limit;
 		else
 			rv @ [ String.sub str i ((String.length str) - i) ]
 	end
@@ -105,7 +104,7 @@ let rec pvt_rec_split(rv, str, c, i, limit): '_a list =
  @return an array of strings of the split elements
 *)
 let split s c limit =
-	pvt_rec_split([], s, c, 0, limit)
+	pvt_rec_split [] s c 0 limit
 ;;
 
 (**
@@ -116,7 +115,7 @@ let split s c limit =
  @return an array of strings
 *)
 let split_chars s l limit =
-	pvt_rec_split_chars([], s, l, 0, limit)
+	pvt_rec_split_chars [] s l 0 limit
 ;;
 
 (*
@@ -134,7 +133,7 @@ let split_chars s l limit =
  @param offset where to begin the search (0 for the beginning)
  @return the offset of the first match, or -1 if there is no match
  *)
-let rec strstr haystack needle offset: int =
+let rec strstr haystack needle offset =
 	if ((String.length needle) + offset) > (String.length haystack) then
 		-1
 	else
