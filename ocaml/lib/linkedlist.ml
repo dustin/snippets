8af70547365f72e5ed91a62cea2b873fe28ec2de
Dustin Sallings <dsallings@2wire.com>
2003-11-09 13:36:29 +0000

    A bit closer, fully circular list.
    imported from: dustin@spy.net--projects/snippets--ocaml--1.0--patch-51

--- ocaml/lib/linkedlist.ml
+++ ocaml/lib/linkedlist.ml
@@ -6,59 +6,58 @@
 
 type 'a t = {
 	data: 'a;
-	mutable prev: 'a t option;
-	mutable next: 'a t option;
+	mutable prev: 'a t;
+	mutable next: 'a t;
 }
 ;;
 
+(** Exception thrown when a single-item list is attempted to be emptied. *)
+exception Empty_list;;
+
 (** Create a linked list. *)
-let create x = { data=x; next=None; prev=None };;
+let create x =
+	let tmp = { data=x; prev=Obj.magic 0; next=Obj.magic 0} in
+	tmp.next <- tmp;
+	tmp.prev <- tmp;
+	tmp
+;;
 
-(** Prepend to this list.
+(** Append to this list.
 
-@param x the value to prepend
-@param l the list to which to prepend
-@return the new list
+@param x the value to append
+@param l the list to which to append
 *)
-let prepend x l =
-	let newnode = { data=x; next=None; prev=None } in
-	l.prev <- Some newnode;
-	newnode.next <- Some l;
-	newnode
-;;
-
-(** Get the data from the head of this list. *)
-let hd l = l.data ;;
-
-let rec end_node l =
-	match l.next with
-		None -> l
-		| Some(n) -> end_node n
-;;
-
 let append x l =
-	(end_node l).next <- Some { data=x; next=None; prev=None };
-	l
-;;
-
-let create_from_list l =
-	let rec rec_cfl ll = function
-		[] -> ll
-		| h::t ->
-			append h (rec_cfl ll t) in
-	rec_cfl (create (List.hd l)) (List.tl l)
+	let tmp = create x in
+	tmp.prev <- l.prev;
+	tmp.next <- l;
+	l.prev.next <- tmp;
+	l.prev <- tmp;
 ;;
 
 (** Iterate the given linked list. *)
-let rec iter f l =
-	f l.data;
-	match l.next with
-		None -> ()
-		| Some(n) -> iter f n;
+let iter f l =
+	let rec rec_iter ltmp start =
+		f ltmp.data;
+		if ltmp.next != start then
+			rec_iter ltmp.next start;
+		in
+	rec_iter l l
+;;
+
+(** Create a linked list from a list. *)
+let create_from_list l =
+	let rv = create (List.hd l) in
+	List.iter (fun x -> append x rv) (List.tl l);
+	rv
+;;
+
+(** Delete this item from the list it contains. *)
+let delete_item l =
+	if (l.prev = l) then raise Empty_list;
+	l.next.prev <- l.prev;
+	l.prev.next <- l.next;
+	l.next;
 ;;
 
 let debug_print = (fun x -> print_endline("Node: " ^ (string_of_int x)));;
-
-let l = create 1;;
-
-let l2 = prepend 2 l;;
