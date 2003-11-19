3894e6233db2cf9bd26bc390e759cba44c99c8ef
Dustin Sallings <dsallings@2wire.com>
2003-11-19 10:00:57 +0000

    Added the gateway auth indexer.
    imported from: dustin@spy.net--projects/snippets--ocaml--1.0--patch-68

---
+++ ocaml/2wire/2wire/indexGWAuth.ml
@@ -0,0 +1,17 @@
+(*
+ * Copyright (c) 2003  Dustin Sallings <dustin@spy.net>
+ *
+ * arch-tag: d264779f-6b46-41f5-9c5e-b7d1c82194a5
+ *)
+
+(* for every line of stdin, index by the first element (serial number) *)
+let main() =
+	let db = Dbm.opendbm "authdb" [Dbm.Dbm_create;Dbm.Dbm_rdwr] 0o644 in
+	Fileutils.iter_lines (fun l ->
+							Dbm.add db (List.hd (Stringutils.split l '\t' 1)) l)
+						stdin;
+	Dbm.close db;
+;;
+
+(* Start main if we're interactive. *)
+if !Sys.interactive then () else begin main() end;;
