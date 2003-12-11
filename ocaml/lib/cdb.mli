3013b1f9ddb3e6fd48670b1fd39961b7f54f5f55
Dustin Sallings <dsallings@2wire.com>
2003-12-11 08:07:05 +0000

    Added Cdb.iter
    imported from: dustin@spy.net--projects/snippets--ocaml--1.0--patch-87

--- ocaml/lib/cdb.mli
+++ ocaml/lib/cdb.mli
@@ -3,6 +3,8 @@
 	http://cr.yp.to/cdb/cdb.txt
  *)
 
+(** {1 Building a CDB } *)
+
 type cdb_creator = {
   table_count : int array;
   mutable pointers : (int * int) list;
@@ -11,6 +13,10 @@ type cdb_creator = {
 val open_out : string -> cdb_creator
 val add : cdb_creator -> string -> string -> unit
 val close_cdb_out : cdb_creator -> unit
+
+(** {1 Iterating a CDB } *)
+
+val iter : (string -> string -> unit) -> string -> unit
 
 (*
  * arch-tag: 55F4CBF0-2B50-11D8-BEDC-000393CFE6B8
