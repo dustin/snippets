5b47204717cf0c95fe467067896946c19904a978
Dustin Sallings <dustin@spy.net>
2003-12-12 16:16:27 +0000

    Some documentation fixups for extstream
    imported from: dustin@spy.net--projects/snippets--ocaml--1.0--patch-96

--- ocaml/lib/extstream.ml
+++ ocaml/lib/extstream.ml
@@ -4,8 +4,19 @@
  * arch-tag: 77D66570-2C78-11D8-BCEB-000393CB0F1E
  *)
 
-(** Convert a stream of something that can be converted to a stream of
+(** Extra stream utilities. *)
+
+(**
+  Convert a stream of something that can be converted to a stream of
   something else using function f into that stream of something else.
+
+  For example, if you have a Stream of strings that you want to convert to a
+  stream of chars, you can use the following construct:
+
+  {[stream_convert Stream.of_string stream_of_strings]}
+
+  @param f the function to convert each chunk
+  @param source the source stream
  *)
 let stream_convert f source =
 	let chunk = ref (f (Stream.next source)) in
