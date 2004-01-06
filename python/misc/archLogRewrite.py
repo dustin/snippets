fa38c6cefa631767563aee4b48ac296327be17f3
Dustin Sallings <dustin@spy.net>
2004-01-06 18:18:21 +0000

    Added something to fix the dates pulled in from cscvs.
    imported from: dustin@spy.net--projects-2004/snippets--python--1.0--patch-2

---
+++ python/misc/archLogRewrite.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+"""
+
+Copyright (c) 2003  Dustin Sallings <dustin@spy.net>
+$Id: skeleton.py,v 1.2 2003/01/03 08:43:26 dustin Exp $
+"""
+
+import time
+import sys
+import os
+import string
+import traceback
+
+f=open(sys.argv[1])
+lines=f.readlines()
+f.close()
+
+# find the date
+ttable=string.maketrans("/:", "  ")
+lines.reverse()
+headers={}
+for l in lines:
+	if l.startswith("Date: "):
+		try:
+			# Date: 1998/10/19 03:32:40
+			d=l[6:]
+			dp=map(int, string.split(string.translate(d, ttable)))
+			headers["Standard-date"]="%04d-%02d-%02d %02d:%02d:%02d GMT" % \
+				tuple(dp)
+			os.environ["TZ"]="UTC"
+			time.tzset()
+			t=time.mktime((dp[0], dp[1], dp[2], dp[3],
+				dp[4], dp[5], 0, 0, -1))
+			del os.environ["TZ"]
+			time.tzset()
+			# headers["Date"]=time.ctime(t)
+			# Fri Oct 17 00:51:34 PDT 2003
+			headers["Date"]=time.strftime("%a %b %d %H:%M:%S %Z %Y",
+				time.localtime(t))
+		except ValueError:
+			pass
+		except:
+			traceback.print_exc()
+
+sys.stderr.write(repr(headers) + "\n")
+
+lines.reverse()
+allheaders=0
+for l in lines:
+	if allheaders == 0:
+		h=string.split(l, ":", 2)
+		if headers.has_key(h[0]):
+			sys.stdout.write("%s: %s\n" % (h[0], headers[h[0]]))
+		else:
+			sys.stdout.write(l)
+		if l == "\n":
+			allheaders=1
+	else:
+		sys.stdout.write(l)
