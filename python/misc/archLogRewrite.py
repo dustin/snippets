ef673956119e480e991100d45a971b94db89427e
Dustin Sallings <dustin@spy.net>
2004-01-07 16:29:35 +0000

    Perform arch log rewriting internally.
    imported from: dustin@spy.net--projects-2004/snippets--python--1.0--patch-3

--- python/misc/archLogRewrite.py
+++ python/misc/archLogRewrite.py
@@ -11,49 +11,59 @@ import os
 import string
 import traceback
 
-f=open(sys.argv[1])
-lines=f.readlines()
-f.close()
+def rewriteFile(filename):
 
-# find the date
-ttable=string.maketrans("/:", "  ")
-lines.reverse()
-headers={}
-for l in lines:
-	if l.startswith("Date: "):
-		try:
-			# Date: 1998/10/19 03:32:40
-			d=l[6:]
-			dp=map(int, string.split(string.translate(d, ttable)))
-			headers["Standard-date"]="%04d-%02d-%02d %02d:%02d:%02d GMT" % \
-				tuple(dp)
-			os.environ["TZ"]="UTC"
-			time.tzset()
-			t=time.mktime((dp[0], dp[1], dp[2], dp[3],
-				dp[4], dp[5], 0, 0, -1))
-			del os.environ["TZ"]
-			time.tzset()
-			# headers["Date"]=time.ctime(t)
-			# Fri Oct 17 00:51:34 PDT 2003
-			headers["Date"]=time.strftime("%a %b %d %H:%M:%S %Z %Y",
-				time.localtime(t))
-		except ValueError:
-			pass
-		except:
-			traceback.print_exc()
+	f=open(filename)
+	lines=f.readlines()
+	f.close()
 
-sys.stderr.write(repr(headers) + "\n")
+	out=open(filename + ".tmp", "w")
 
-lines.reverse()
-allheaders=0
-for l in lines:
-	if allheaders == 0:
-		h=string.split(l, ":", 2)
-		if headers.has_key(h[0]):
-			sys.stdout.write("%s: %s\n" % (h[0], headers[h[0]]))
+	# find the date
+	ttable=string.maketrans("/:", "  ")
+	lines.reverse()
+	headers={}
+	for l in lines:
+		if l.startswith("Date: "):
+			try:
+				# Date: 1998/10/19 03:32:40
+				d=l[6:]
+				dp=map(int, string.split(string.translate(d, ttable)))
+				headers["Standard-date"]="%04d-%02d-%02d %02d:%02d:%02d GMT" % \
+					tuple(dp)
+				os.environ["TZ"]="UTC"
+				time.tzset()
+				t=time.mktime((dp[0], dp[1], dp[2], dp[3],
+					dp[4], dp[5], 0, 0, -1))
+				del os.environ["TZ"]
+				time.tzset()
+				# headers["Date"]=time.ctime(t)
+				# Fri Oct 17 00:51:34 PDT 2003
+				headers["Date"]=time.strftime("%a %b %d %H:%M:%S %Z %Y",
+					time.localtime(t))
+			except ValueError:
+				pass
+			except:
+				traceback.print_exc()
+
+	sys.stderr.write(repr(headers) + "\n")
+
+	lines.reverse()
+	allheaders=0
+	for l in lines:
+		if allheaders == 0:
+			h=string.split(l, ":", 2)
+			if headers.has_key(h[0]):
+				out.write("%s: %s\n" % (h[0], headers[h[0]]))
+			else:
+				out.write(l)
+			if l == "\n":
+				allheaders=1
 		else:
-			sys.stdout.write(l)
-		if l == "\n":
-			allheaders=1
-	else:
-		sys.stdout.write(l)
+			out.write(l)
+
+	os.rename(filename + ".tmp", filename)
+
+# MAIN
+for f in sys.argv[1:]:
+	rewriteFile(f)
