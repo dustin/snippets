9c6a356b0fa3fd2c2ea788d78bac13316981c730
Dustin Sallings <dsallings@2wire.com>
2004-02-05 04:55:57 +0000

    Added exception template.
    imported from: dustin@spy.net--projects-2004/snippets--java--1.0--patch-2

---
+++ java/templates/Exception.java
@@ -0,0 +1,26 @@
+// Copyright (c) 2004  2Wire, Inc.
+
+package PACKAGE;
+
+import com.twowire.rpc.CMSException;
+
+/**
+ *
+ */
+public class CLASSNAME extends CMSException {
+
+	/**
+	 * Get an instance of CLASSNAME.
+	 */
+	public CLASSNAME(String msg) {
+		super(msg);
+	}
+
+	/**
+	 * Get an instance of CLASSNAME with a root cause.
+	 */
+	public CLASSNAME(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+}
