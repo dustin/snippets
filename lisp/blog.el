2eff5b299c3793151d800c0a8c743934bcd12332
Dustin Sallings <dustin@spy.net>
2004-02-10 15:12:40 +0000

    Got the multiple directories in the blog thing.
    imported from: dustin@spy.net--projects-2004/snippets--lisp--1.0--patch-4

--- lisp/blog.el
+++ lisp/blog.el
@@ -5,9 +5,8 @@
 (defvar blog-outdir "~/tmp/" "The blog directory.")
 (defvar blog-index "~/tmp/index.shtml" "The index file.")
 (defvar blog-ddateformat "%Y/%m/%d %H:%M" "Visible timestamp format.")
-(defvar blog-fndateformat "%Y%m%d%H%M" "Filename timestamp format.")
+(defvar blog-fndateformat "%Y/%m/%d.%H%M.shtml" "Filename timestamp format.")
 
-; Add an entry to an index.
 (defun blog-add-index (ixpath newfile)
   "Add a reference to the new file in the given index."
   (let ((inf (find-file ixpath)))
@@ -18,24 +17,26 @@
     (insert "\n<!--#include virtual=\"" newfile "\"-->")
     (save-buffer inf)))
 
+(defun blog-create-blogfile (newfile)
+  "Create the new blog file in a new buffer.  Return the new buffer."
+  ; (make-directory (file-name-directory newfile) 1)
+  (let ((nef (find-file (concat blog-outdir newfile))))
+    (switch-to-buffer nef)
+    (insert-file-contents blog-template nil nil nil 1)
+    (goto-char (point-min))
+    (while (re-search-forward "%THEDATE%" nil t)
+      (replace-match (format-time-string blog-ddateformat) nil nil))
+    (save-buffer nef)
+    nef))
+
 ; Do the work
 (defun blog-new-entry ()
   "Make a new blog entry."
-   (let* ((thedate (format-time-string blog-ddateformat))
-	 (newfile (concat (format-time-string blog-fndateformat) ".shtml"))
-	 (inf (find-file blog-index))
-	 (nef (find-file (concat blog-outdir newfile))))
-     ; Create the new file and fill in the template
-     (switch-to-buffer nef)
-     (insert-file-contents blog-template nil nil nil 1)
-     (goto-char (point-min))
-     (while (re-search-forward "%THEDATE%" nil t)
-       (replace-match thedate nil nil))
-     (save-buffer nef)
-     ; Update the index
-     (blog-add-index blog-index newfile)
-     (switch-to-buffer nef)
-     nil))
+  (let ((newfile (format-time-string blog-fndateformat)))
+	; Update the index
+	(blog-add-index blog-index newfile)
+	; Create the new file
+	(blog-create-blogfile newfile)))
 
 (defun blog-make-entry ()
   "Make a new blog entry."
