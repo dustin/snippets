f1ff3e7cc033c1b5108ead27e759a7791c5fc4df
Dustin Sallings <dustin@spy.net>
2004-04-10 13:59:15 +0000

    Server logs connections.
    imported from: dustin@spy.net--projects-2004/snippets--objc--1.0--patch-13

--- objc/osx/TestTool/main.m
+++ objc/osx/TestTool/main.m
@@ -1,6 +1,30 @@
 #import <Foundation/Foundation.h>
 #import "RemoteObject.h"
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+@interface Authenticator : NSObject {
+}
+@end
+
+@implementation Authenticator
+- (BOOL)connection:(NSConnection *)parentConnection
+  shouldMakeNewConnection:(NSConnection *)newConnection
+{
+	// NSLog(@"Connection wants to make a new connection to %@", newConnection);
+	struct sockaddr_in saddr;
+
+	id remotePort=[newConnection sendPort];
+	NSData *d=[remotePort address];
+	[d getBytes:&saddr];
+	NSLog(@"Remote address is %s", inet_ntoa(saddr.sin_addr));
+	return(YES);
+}
+@end
+
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
@@ -9,9 +33,12 @@ int main (int argc, const char * argv[]) {
 
     id ro=[[RemoteObject alloc] init];
 
+	Authenticator *auth=[[Authenticator alloc] init];
+
     // set up remote object
     NSConnection *theConnection=[NSConnection
 		connectionWithReceivePort:myPort sendPort:nil];
+	[theConnection setDelegate:auth];
 
     // insert code here...
     NSLog(@"Connection is listening on %d", thePort);
