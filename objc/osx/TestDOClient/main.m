8aa6d44faf30c36a2811f1ec60e4ad35203b8eee
Dustin Sallings <dustin@spy.net>
2004-04-10 13:27:37 +0000

    Working distributed objects over TCP.
    imported from: dustin@spy.net--projects-2004/snippets--objc--1.0--patch-11

--- objc/osx/TestDOClient/main.m
+++ objc/osx/TestDOClient/main.m
@@ -1,19 +1,24 @@
 #import <Foundation/Foundation.h>
 
+@protocol RemoteObject
+-(int)access;
+@end
+
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
-	id host=nil;
+	NSSocketPort *myPort=[[NSSocketPort alloc] initRemoteWithTCPPort:3838
+		host:@"rubik"];
 
-    id theProxy=[NSConnection
-		rootProxyForConnectionWithRegisteredName:@"RemoteObject"
-		host:host];
-	NSLog(@"Got %@\n", theProxy);
-	[theProxy retain];
-    // [theProxy setProtocolForProxy:@protocol(ServerProtocol)];
+	NSConnection *myConn=[NSConnection connectionWithReceivePort:nil
+		sendPort:myPort];
+
+    id theProxy=[[myConn rootProxy] retain];
+	// NSLog(@"Got %@\n", theProxy);
+    [theProxy setProtocolForProxy:@protocol(RemoteObject)];
 
     NSLog(@"Calling remote method\n");
-    int rv=(int)[theProxy access];
+    int rv=[theProxy access];
     NSLog(@"Returned %d\n", rv);
 
     [pool release];
