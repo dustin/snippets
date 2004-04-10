e16b18242a43ca15182035f3cb257713b60eea32
Dustin Sallings <dustin@spy.net>
2004-04-10 14:05:39 +0000

    Got safer connection handling with timeouts and all that.
    imported from: dustin@spy.net--projects-2004/snippets--objc--1.0--patch-14

--- objc/osx/TestDOClient/main.m
+++ objc/osx/TestDOClient/main.m
@@ -12,10 +12,19 @@ int main (int argc, const char * argv[]) {
 
 	NSConnection *myConn=[NSConnection connectionWithReceivePort:nil
 		sendPort:myPort];
+	[myConn setRequestTimeout: 5];
+	[myConn setReplyTimeout: 5];
 
-    id theProxy=[[myConn rootProxy] retain];
-	// NSLog(@"Got %@\n", theProxy);
-    [theProxy setProtocolForProxy:@protocol(RemoteObject)];
+    id theProxy=nil;
+	NS_DURING
+		theProxy=[[myConn rootProxy] retain];
+		// NSLog(@"Got %@\n", theProxy);
+	[theProxy setProtocolForProxy:@protocol(RemoteObject)];
+	NS_HANDLER
+		NSLog(@"Error getting proxy from connection:  %@",
+			[localException description]);
+		theProxy=nil;
+	NS_ENDHANDLER
 
     NSLog(@"Calling remote method\n");
     int rv=[theProxy access];
