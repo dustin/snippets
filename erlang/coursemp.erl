aa00d7fa147ba1401641a629a0958c95f9b0d3fd
Dustin Sallings <dustin@spy.net>
2004-04-19 04:49:44 +0000

    nm circle exercise
    imported from: dustin@spy.net--projects-2004/snippets--erlang--1.0--patch-4

--- erlang/coursemp.erl
+++ erlang/coursemp.erl
@@ -3,7 +3,7 @@
 %%
 
 -module(coursemp).
--export([twoprocess/1, echo/0]).
+-export([twoprocess/1, echo/0, nprocess/2, ringProcess/1]).
 
 echo() ->
 	receive {From, N} ->
@@ -22,7 +22,48 @@ loop(N, With) ->
 	end,
 	loop(N - 1, With).
 
+% Bounce N messages between two proceses
 twoprocess(N) ->
 	W = spawn(coursemp, echo, []),
 	link(W),
 	loop(N, W).
+
+%
+% nm stuff
+%
+
+ringProcess(S) ->
+	receive
+		{From, N} ->
+			io:format("Sending ~p from ~p to ~p\n", [N, self(), S]),
+			S ! {self(), N},
+			ringProcess(S);
+		stop ->
+			io:format("Shutting down ~p\n", [self()]),
+			S ! stop,
+			true
+	end.
+
+spawnLinked(M, F, A) ->
+	P = spawn(M, F, A),
+	link(P),
+	P.
+
+processMaker(S, 1) ->
+	spawnLinked(coursemp, ringProcess, [S]);
+processMaker(S, M) ->
+	processMaker(spawnLinked(coursemp, ringProcess, [S]), M-1).
+
+nmloop(0, Start) -> 0;
+nmloop(N, Start) ->
+	Start ! {self(), N},
+	receive
+		Rv -> io:format("Received ~p\n", [Rv])
+	end,
+	nmloop(N-1, Start).
+
+% Bounce N messages around a ring of M processes
+nprocess(N, M) ->
+	Start = processMaker(self(), M),
+	nmloop(N, Start),
+	Start ! stop.
