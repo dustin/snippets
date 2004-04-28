6e97bad45b9261106bb0fd6bf1c13851dd293446
Dustin Sallings <dustin@spy.net>
2004-04-28 02:23:30 +0000

    Added some comments. Use error_logger for logging errors.
    imported from: dustin@spy.net--projects-2004/snippets--erlang--1.0--patch-19

--- erlang/mrecv.erl
+++ erlang/mrecv.erl
@@ -6,9 +6,11 @@
 -export([start/0, getdict/0, getval/1]).
 -export([init/0]).
 
+% Spawn the process.
 start() ->
 	spawn(?MODULE, init, []).
 
+% Initialize the multicast group and start the loop
 init() ->
 	register(mrecv, self()),
 	process_flag(trap_exit, true),
@@ -17,31 +19,41 @@ init() ->
 	{ok, Port} = gen_udp:open(6789, [{add_membership,{GAddr,LAddr}}]),
 	loop(Port, dict:new()).
 
+% Look for messages via multicast and keep a dict of the current values.
+% Also, look for requests from other processes that want current temperature
+% inforamtion.
 loop(Port, Dict) ->
 	receive
+		% A UDP message
 		{udp, Port, Raddr, Rport, S} ->
 			% io:format("~p\n", [lists:sublist(S, (length(S)-1))]),
 			Vals = string:tokens(S, "\t"),
 			Key  = lists:nth(2, Vals),
 			Val  = list_to_float(lists:nth(3, Vals)),
 			loop(Port, dict:update(Key, fun(_) -> Val end, Val, Dict));
+		% A lookup message for a specific serial number
 		{lookup, From, SN} ->
 			From ! dict:fetch(SN, Dict),
 			loop(Port, Dict);
+		% Just get the whole dict (useful for debugging)
 		{getdict, From} ->
 			From ! Dict,
 			loop(Port, Dict);
+		% Anything else
 		Unhandled ->
-			io:format("Unhandled message:  ~p\n", [Unhandled]),
+			error_logger:error_msg("mrecv: Unhandled message:  ~p\n",
+				[Unhandled]),
 			loop(Port, Dict)
 	end.
 
+% Get the dict from the mrecv
 getdict() ->
 	mrecv ! {getdict, self()},
 	receive
 		Rv -> Rv
 	end.
 
+% Get the current reading for the specific serial number
 getval(SN) ->
 	mrecv ! {lookup, self(), SN},
 	receive
