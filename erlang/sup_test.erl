7a78e34bcc702bd572d8451c1c9e2fa900374a8b
Dustin Sallings <dustin@spy.net>
2004-04-28 15:33:47 +0000

    Added code implementing supervisor behavior.
    imported from: dustin@spy.net--projects-2004/snippets--erlang--1.0--patch-20

---
+++ erlang/sup_test.erl
@@ -0,0 +1,33 @@
+%%
+%% arch-tag: BA1719BD-98D8-11D8-9DF7-000A957659CC
+%%
+
+-module(sup_test).
+-behavior(supervisor).
+
+% supevisor stuff
+-export([start_link/0, init/1]).
+
+% child stuff
+-export([new_child/0]).
+
+%
+% Supervisor stuff
+%
+
+start_link() ->
+	supervisor:start_link(sup_test, []).
+
+init(_Args) ->
+	{ok, {{one_for_one, 2, 60},
+			[{sup_test_child, {sup_test, new_child, []},
+				permanent, 5000, worker, [sup_test]}]}}.
+
+%
+% Child stuff
+%
+
+new_child() ->
+	io:format("Spawning a new child...\n"),
+	{ok, spawn_link(coursemp, echo, [])}.
+
