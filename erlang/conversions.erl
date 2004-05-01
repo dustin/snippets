68095f314f9fa5a8cad12b82fa54c3e3d692f981
Dustin Sallings <dustin@spy.net>
2004-05-01 07:58:39 +0000

    Working authentication and basic packet handling.
    imported from: dustin@spy.net--projects-2004/snippets--erlang--1.0--patch-24

---
+++ erlang/conversions.erl
@@ -0,0 +1,32 @@
+%%
+%% arch-tag: 1474638B-9AE5-11D8-B81D-000393CFE6B8
+%%
+
+%%
+%% Data converisons
+%%
+
+-module(conversions).
+-export([hex/1, md5_hex/1]).
+
+% Convert something to a hex string
+hex(B) when binary(B) ->
+	hex(binary_to_list(B));
+hex(L) when list(L) ->
+	lists:flatten([hex(I) || I <- L]);
+hex(I) when I > 16#f ->
+	[hex0((I band 16#f0) bsr 4), hex0((I band 16#0f))];
+hex(I) ->
+	[$0, hex0(I)].
+
+hex0(10) -> $a;
+hex0(11) -> $b;
+hex0(12) -> $c;
+hex0(13) -> $d;
+hex0(14) -> $e;
+hex0(15) -> $f;
+hex0(I) ->  $0 +I.
+
+% Get an md5 as a hex string.
+md5_hex(Data) ->
+	hex(erlang:md5(Data)).
--- erlang/postgres.erl
+++ erlang/postgres.erl
@@ -13,11 +13,13 @@
 -export([start/4]).
 
 % States
--export([connected/3]).
+-export([authenticated/2, ready_for_query/2]).
 
 % callbacks
 -export([handle_event/3, handle_sync_event/4,
 	handle_info/3, init/1, terminate/3]).
+
+-record(pginfo, {host,port,user,pass,db,socket}).
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Client functions
@@ -70,17 +72,35 @@ init([Host,Port,User,Database]) ->
 		"user", binary_to_list(<<0>>), User, binary_to_list(<<0>>),
 		"database", binary_to_list(<<0>>), Database, binary_to_list(<<0,0>>)]),
 	ok = gen_tcp:send(Socket, makeString(Announcement)),
-	{ok, connected, Socket}.
+	{ok, connected, #pginfo{host=Host, port=Port, user=User,
+		pass="blahblah", db=Database, socket=Socket}}.
 
-% Nothing useful
-connected(X, Pid, Info) ->
-	io:format("connected/3 called~n", []).
+% Data arriving after authentication
+
+authenticated({data_arrived, Type, Extra}, Info) ->
+	% io:format("Data arrived in authenticated state, type ~c~n", [Type]),
+	handle_packet([Type|Extra], authenticated, Info).
+
+ready_for_query({data_arrived, Type, Extra}, Info) ->
+	io:format("Data arrived in ready_for_query state, type ~c~n", [Type]),
+	handle_packet([Type|Extra], authenticated, Info).
 
 %%%%% authentication handlers
 
+% Perform MD5 authentication
 perform_auth(auth_md5, Data, connected, Info) ->
-	io:format("Performing MD5 auth~n", []),
+	Salt = lists:nthtail(4, Data),
+	UplusP = conversions:md5_hex(Info#pginfo.pass ++ Info#pginfo.user),
+	% io:format("hexed password/user (phase 1):  ~p~n", [UplusP]),
+	Digested = conversions:md5_hex(UplusP ++ Salt),
+	% io:format("digested password (phase 2):  ~p~n", [Digested]),
+	ok = gen_tcp:send(Info#pginfo.socket,
+		[$p|makeString("md5" ++ Digested ++ "\0")]),
 	{next_state, connected, Info};
+% Auth OK message, change to authenticated state
+perform_auth(auth_ok, Data, connected, Info) ->
+	{next_state, authenticated, Info};
+% No other auth type is allowed
 perform_auth(Type, Data, connected, Info) ->
 	exit("Trying to use unhandled auth type " ++ atom_to_list(Type)).
 
@@ -101,21 +121,72 @@ handle_packet($R, Length, Data, connected, Info) ->
 	io:format("Got auth request (~p):  ~p bytes:  ~p~n", [Type, Length, Rest]),
 	perform_auth(Type, Data, connected, Info);
 
+% Errors
+handle_packet($E, Length, Data, State, Info) ->
+	error_logger:error_msg("Got an error in state ``~p'':  ~p~n",
+		[State, lists:flatten(
+			lists:map(fun(P) -> case P of 0 -> " - "; _ -> P end end, Data))]),
+	{next_state, State, Info};
+
+% Cancellation key
+handle_packet($K, Length, Data, State, Info) ->
+	{Pid, Key} = getInt32(Data),
+	io:format("Got cancellation key (~p) for pid (~p)~n", [Key, Pid]),
+	{next_state, State, Info};
+
+% Ready for query
+handle_packet($Z, Length, Data, State, Info) ->
+	{next_state, ready_for_query, Info};
+
+% parameter status
+handle_packet($S, Length, Data, State, Info) ->
+	[Key,Val] = string:tokens(Data, "\0"),
+	io:format(" - ~p = ~p~n", [Key, Val]),
+	{next_state, State, Info};
+
 % Any other packet
 handle_packet(Type, Length, Data, State, Info) ->
-	io:format("Got packet ~p length ~p [~p] in state ~p~n",
+	io:format("Got unhandled packet ~c length ~p [~p] in state ~p~n",
 		[Type, Length, Data, State]),
 	{next_state, State, Info}.
+
+dealWithRemaining([Type|Data], State, Info) ->
+	% io:format("Remaining data:  ~c (~p bytes)~n", [Type, length(Data)]),
+	gen_fsm:send_event(self(), {data_arrived, Type, Data});
+dealWithRemaining([], _State, _Info) ->
+	[].
+
+handle_packet([Type|Data], State, Info) ->
+	{Len, Rest} = getInt32(Data),
+	% io:format("Got packet of type ~c, ~p bytes~n", [Type,Len]),
+	{FullData, Extra} = getAllData(Len, Rest, Info),
+	assert(length(FullData)+4 == Len, "Incorrect data length"),
+	dealWithRemaining(Extra, State, Info),
+	handle_packet(Type, Len, FullData, State, Info).
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Callbacks
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+getAllData(Len, Rest, Info) ->
+	DLen=length(Rest) + 4,
+	Remaining = Len - DLen,
+	if (Remaining < 1) ->
+			if (Remaining == 0) ->
+					{Rest, []}; % Nothing left, nothing over
+				true ->
+					lists:split(Len-4, Rest)
+			end;
+		true ->
+			io:format("Need to read ~p more bytes~n", [Remaining]),
+			{ok, Pkt} = gen_tcp:recv(Info#pginfo.socket, Remaining),
+			io:format("Read ~p~n", [Pkt]),
+			getAllData(Len, Rest ++ Pkt, Info)
+	end.
+
 % Auth request
-handle_info({tcp, _Port, [Type|Data]}, State, Info) ->
-	{Len, Rest} = getInt32(Data),
-	assert(length(Rest)+4 == Len, "Incorrect data length"),
-	handle_packet(Type, Len, Rest, State, Info).
+handle_info({tcp, _Port, Data}, State, Info) ->
+	handle_packet(Data, State, Info).
 
 % XXX  Doesn't do anything useful yet
 handle_sync_event(X, Pid, AnyState, Info) ->
