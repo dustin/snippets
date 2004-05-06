a6a52308cf2a441feba8bf1463259c772bfcfd7e
Dustin Sallings <dustin@spy.net>
2004-05-06 04:39:25 +0000

    use cdb instead of dbm for the rrd index thing
    imported from: dustin@spy.net--projects-2004/snippets--ocaml--1.0--patch-19

--- ocaml/2wire/rrdIndex.ml
+++ ocaml/2wire/rrdIndex.ml
@@ -6,18 +6,16 @@
 
 let index_one fn =
 	print_endline("Indexing " ^ fn);
-	(* Get the DBM *)
-	let db = Dbm.opendbm fn [Dbm.Dbm_create;Dbm.Dbm_rdwr] 0o644 in
+	(* Get the CDB *)
+	let cdb = Cdb.open_out fn in
 	Fileutils.iter_file_lines (fun l ->
 		let a = Extstring.split l '\t' 2 in
 		try
-			Dbm.add db (List.hd a) l
+			Cdb.add cdb (List.hd a) l
 		with
-			Dbm.Dbm_error("Entry already exists") ->
-				print_endline("Duplicate: " ^ l)
 			| Failure("hd") ->
 				print_endline("Format problem:  " ^ l)) fn;
-	Dbm.close db;
+	Cdb.close_cdb_out cdb;
 ;;
 
 let main() =
