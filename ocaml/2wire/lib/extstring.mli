afe7bcae5c7ff16f182adaf3184b116d3bff8314
Dustin Sallings <dustin@spy.net>
2004-05-12 05:20:26 +0000

    Added split_all and split_chars_all. Little refactoring.
    imported from: dustin@spy.net--projects-2004/snippets--ocaml--1.0--patch-31

--- ocaml/2wire/lib/extstring.mli
+++ ocaml/2wire/lib/extstring.mli
@@ -3,6 +3,8 @@
 val str_index_of_one : string -> char list -> int -> int
 val split : string -> char -> int -> string list
 val split_chars : string -> char list -> int -> string list
+val split_all : string -> char -> int -> string list
+val split_chars_all : string -> char list -> int -> string list
 val strstr : string -> string -> int -> int
 val ends_with : string -> string -> bool
 val begins_with : string -> string -> bool
