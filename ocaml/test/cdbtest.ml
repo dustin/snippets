d5041ad342504a70d33fcfc41740725196c13278
Dustin Sallings <dustin@spy.net>
2004-05-13 10:13:26 +0000

    Turns out I needed the extra bit in the cdb lib.
    imported from: dustin@spy.net--projects-2004/snippets--ocaml--1.0--patch-38

--- ocaml/test/cdbtest.ml
+++ ocaml/test/cdbtest.ml
@@ -6,14 +6,20 @@
 
 open Cdb;;
 
+let myadd c a b =
+	Printf.eprintf "Writing %s = %s\n" a b;
+	add c a b
+;;
+
 (** test app to create ``test.cdb'' and put some stuff in it *)
 let main() =
 	let c = open_out "test.cdb" in
-	add c "a" "1";
-	add c "b" "2";
-	add c "c" "3";
-	add c "c" "4";
-	add c "dustin" "We're number one!";
+	myadd c "a" "1";
+	myadd c "b" "2";
+	myadd c "c" "3";
+	myadd c "c" "4";
+	add c "1,473114043983" "1,473114043983";
+	myadd c "dustin" "We're number one!";
 	close_cdb_out c;
 	iter (fun k v -> print_endline(k ^ " -> " ^ v)) "test.cdb";
 	print_endline("*** Searching a ***");
