0e7742ae98c586e9aaad73d52df3836df75bc666
Dustin Sallings <dustin@spy.net>
2004-08-09 06:30:56 +0000

    Added the music cleanup script.
    imported from: dustin@spy.net--projects-2004/snippets--python--1.0--patch-29

---
+++ python/misc/musicCleanup.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+"""
+
+Copyright (c) 2004  Dustin Sallings <dustin@spy.net>
+"""
+
+import sys
+import os
+import time
+
+def manualProcess(v):
+	print "------"
+	for i in range(len(v)):
+		print "\t", i, v[i]
+	sys.stdout.write("Which do you want to keep?  ")
+	sys.stdout.flush()
+	x=int(sys.stdin.readline().strip())
+	print "Keeping", x
+	for i in range(len(v)):
+		print "\t", i, v[i]
+		if i != x:
+			print "**** Deleting", v[i]
+			os.unlink(v[i])
+
+def process(v):
+	rv=None
+	# try to do this automatically
+	tmpv=filter(lambda x: x.find("./rip") != 0, v)
+	if(len(tmpv) == 1):
+		print "Can autoclean", v, "to", tmpv
+		for fn in v:
+			if(fn.find("./rip") == 0):
+				print "Removing", fn
+				time.sleep(0.75)
+				os.unlink(fn)
+	else:
+		# manualProcess(v)
+		rv=v
+	return rv
+
+# Load them
+files={}
+f=open(sys.argv[1])
+for l in f.readlines():
+	l=l.strip()
+	m,fn=l.split("\t")
+	if(files.has_key(m)):
+		files[m].append(fn)
+	else:
+		files[m]=[fn]
+
+f.close()
+
+thelist=[]
+for k,v in files.iteritems():
+	vals=filter(os.path.exists, v)
+	vals.sort()
+	if(len(vals) > 1):
+		thelist.append(vals)
+	else:
+		# print k, "is cleaned up with only", vals
+		pass
+
+thelist.sort()
+newlist=[]
+
+print len(thelist), "songs to clean up."
+# Loop and cleanup
+for v in thelist:
+	rv=process(v)
+	if rv is not None:
+		newlist.append(rv)
+
+for v in newlist:
+	manualProcess(v)
