b3f8a58b92b8788446d618f84f7fb251e3dd5957
Dustin Sallings <dustin@spy.net>
2004-10-10 06:21:33 +0000

    Added getMbmonTemp.py to translate purple temperatures to temp bus
    imported from: dustin@spy.net--projects-2004/snippets--python--1.0--patch-35

---
+++ python/net/getMbmonTemp.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+"""
+
+Copyright (c) 2004  Dustin Sallings <dustin@spy.net>
+arch-tag: F5D13F18-1A3E-11D9-8DEA-000A957659CC
+"""
+import time
+import socket
+
+def sockLines(host, port):
+    s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    f=s.makefile('rb')
+
+    rv=f.readlines()
+
+    f.close()
+    s.close()
+
+    return rv
+
+def getTemps(host, port):
+    lines=sockLines(host, port)
+    temps=lines[1][:lines[1].index(';')]
+
+    temps=map(float, temps[7:].split(', '))
+
+    return temps
+
+def mcast(msg):
+    # print "Multicasting: " + msg
+    s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.sendto(msg, ('225.0.0.37', 6789))
+
+def getAndReport():
+    t=time.time()
+    ts=time.strftime("%Y/%m/%d %H:%M:%S")
+    names=['mb', 'cpu', 'chip']
+    temps=getTemps('purple', 1984)
+    for i in range(len(temps)):
+        msg=ts + "\t" + "purple_" + names[i] + "\t" + `temps[i]`
+        mcast(msg)
+
+if __name__ == '__main__':
+    getAndReport()
