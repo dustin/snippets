5dfe1537256f8c1e5cb792a96dab771f8a00bf2c
Dustin Sallings <dustin@spy.net>
2004-11-14 18:00:53 +0000

    Open and close the db for each access.
    imported from: dustin@spy.net--projects-2004/snippets--python--1.0--patch-66

--- python/db/lockeddbm.py
+++ python/db/lockeddbm.py
@@ -14,51 +14,65 @@ class LockedDBM(object):
 
     def __init__(self, path, flag='r', mode=0666):
         """Initialize an anydbm with a lock."""
-        self.db=anydbm.open(path, flag, mode)
+        self.path=path
+        self.flag=flag
+        self.mode=mode
         self.lockfile=open(path + ".lock", "w")
+
+    def __getdb(self):
+        return anydbm.open(self.path, self.flag, self.mode)
 
     def __contains__(self, key):
         return (self.has_key(key))
 
     def __getitem__(self, key):
         rv=None
+        db=None
         try:
             fcntl.flock(self.lockfile, fcntl.LOCK_SH)
-            rv = self.db[key]
+            db=self.__getdb()
+            rv = db[key]
         finally:
             fcntl.flock(self.lockfile, fcntl.LOCK_UN)
+            if db is not None:
+                db.close()
         return rv
 
     def __setitem__(self, key, value):
         rv=None
+        db=None
         try:
             fcntl.flock(self.lockfile, fcntl.LOCK_EX)
-            self.db[key] = value
+            db=self.__getdb()
+            db[key] = value
         finally:
             fcntl.flock(self.lockfile, fcntl.LOCK_UN)
+            if db is not None:
+                db.close()
 
     def keys(self):
         rv=None
+        db=None
         try:
             fcntl.flock(self.lockfile, fcntl.LOCK_SH)
-            rv = self.db.keys()
+            db=self.__getdb()
+            rv = db.keys()
         finally:
             fcntl.flock(self.lockfile, fcntl.LOCK_UN)
+            if db is not None:
+                db.close()
         return rv
 
     def has_key(self, k):
         rv=None
+        db=None
         try:
             fcntl.flock(self.lockfile, fcntl.LOCK_SH)
-            rv = self.db.has_key(k)
+            db=self.__getdb()
+            rv = db.has_key(k)
         finally:
             fcntl.flock(self.lockfile, fcntl.LOCK_UN)
         return rv
 
     def close(self):
-        self.db.close()
         self.lockfile.close()
-
-    # Deal with anything else
-    def __getattr__(self, name):
-        return getattr(self.db, name)
