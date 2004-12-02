8b9e7c9f24f255ff75797aa4dde25398fde9e041
Dustin Sallings <dustin@spy.net>
2004-12-02 14:50:48 +0000

    Allow multiple prefixes and postfixes to be returned to the state machine
    imported from: dustin@spy.net--projects-2004/snippets--python--1.0--patch-75

--- python/misc/stateMachine.py
+++ python/misc/stateMachine.py
@@ -13,16 +13,16 @@ import unittest
 class StateRv(object):
     """Return values for state machines."""
 
-    def __init__(self, state, prefix=None, postfix=None):
-        """Instantiate a state rv with a state and an optional prefix and
-        postfix state machine to adjust the values.
+    def __init__(self, state, prefixes=None, postfixes=None):
+        """Instantiate a state rv with a state and optional lists of prefix
+        and postfix state machines to adjust the values.
 
         A prefix state machine will be run beginning in the init state, and the
         provided state will be used to continue the current machine after the
         prefixed machine completes."""
         self.state=state
-        self.prefix=prefix
-        self.postfix=postfix
+        self.prefixes=prefixes
+        self.postfixes=postfixes
 
 class StateMachine(object):
     """State machine interface."""
@@ -34,42 +34,6 @@ class StateMachine(object):
         """This method should do whatever it needs to do in the given state and
            return the new state."""
         raise NotImplementedError()
-
-class TestMachine(StateMachine):
-    """A test state machine that does nothing but push through a sequence of
-    states."""
-
-    def __init__(self):
-        self.stateMap={StateMachine.STATE_INIT: StateRv(1),
-            1: StateRv(2),
-            2: StateRv(3),
-            3: StateRv(StateMachine.STATE_DONE)}
-
-    def runMachine(self, context, state):
-        return self.stateMap[state]
-
-class TestPrefixingMachine(TestMachine):
-    """A test state machine that runs through a sequence of states, but
-       prefixes a new state machine in the middle."""
-
-    def __init__(self):
-        self.stateMap={StateMachine.STATE_INIT: StateRv(1),
-            1: StateRv(2),
-            2: StateRv(3, prefix=TestMachine()),
-            3: StateRv(4),
-            4: StateRv(StateMachine.STATE_DONE)}
-
-class TestPostfixingMachine(TestMachine):
-    """A test state machine that runs through a sequence of states, but
-       prefixes a new state machine in the middle."""
-
-    def __init__(self):
-        self.stateMap={StateMachine.STATE_INIT: StateRv(1),
-            1: StateRv(2),
-            2: StateRv(3, prefix=None, postfix=TestMachine()),
-            3: StateRv(4),
-            4: StateRv(5),
-            5: StateRv(StateMachine.STATE_DONE)}
 
 #
 # Client stuff
@@ -93,6 +57,10 @@ class StateMachineList(object):
         """Add a machine to be processed in the future."""
         self.machines.append(machine)
 
+    def addPrefixMachine(self, machine):
+        """Add a machine in the beginning."""
+        self.machines.insert(0, machine)
+
     def runMachine(self):
         """Run the current state machine.  Return True if there's more to do."""
         if len(self.machines) == 0:
@@ -103,15 +71,23 @@ class StateMachineList(object):
         newState = self.machines[0].runMachine(self.context, self.state)
 
         # Check for a postfix
-        if newState.postfix is not None:
-            self.addMachine(newState.postfix)
+        if newState.postfixes is not None:
+            for sm in newState.postfixes:
+                self.addMachine(sm)
 
-        # Check for a prefix
-        if newState.prefix is not None:
+        # Check for prefixes
+        if newState.prefixes is not None and len(newState.prefixes) > 0:
             # We're prefixing a state machine to the list.  Preserve the
-            # starting state for the current machine and add the new state
+            # starting state for the current machine and add the new state.
+            # The list is reversed because we want to have them run in returned
+            # order
+            newState.prefixes.reverse()
             self.stateStack.append(newState.state)
-            self.machines.insert(0, newState.prefix)
+            self.addPrefixMachine(newState.prefixes[0])
+            # For any additional state machines, add them with the init state
+            for sm in newState.prefixes[1:]:
+                self.stateStack.append(StateMachine.STATE_INIT)
+                self.addPrefixMachine(sm)
             # Set the state to init for the new state machine item
             self.state=StateMachine.STATE_INIT
         elif newState.state == StateMachine.STATE_DONE:
@@ -133,6 +109,32 @@ class StateMachineList(object):
 #
 ## Testing
 #
+class TestMachine(StateMachine):
+    """A test state machine that does nothing but push through a sequence of
+    states."""
+
+    def __init__(self, stateMap=None):
+        if stateMap is None:
+            stateMap={StateMachine.STATE_INIT: StateRv(1),
+                1: StateRv(2),
+                2: StateRv(3),
+                3: StateRv(StateMachine.STATE_DONE)}
+        self.stateMap=stateMap
+
+    def runMachine(self, context, state):
+        return self.stateMap[state]
+
+class TestPrefixingMachine(TestMachine):
+    """A test state machine that runs through a sequence of states, but
+       prefixes a new state machine in the middle."""
+
+    def __init__(self):
+        TestMachine.__init__(self, {StateMachine.STATE_INIT: StateRv(1),
+            1: StateRv(2),
+            2: StateRv(3, prefixes=[TestMachine()]),
+            3: StateRv(4),
+            4: StateRv(StateMachine.STATE_DONE)})
+
 class StateMachineTest(unittest.TestCase):
 
     def assertState(self, v, st):
@@ -232,7 +234,7 @@ class StateMachineTest(unittest.TestCase):
             StateMachine.STATE_INIT, 1, 2, 3
             ])
 
-    def testPrefixing(self):
+    def testPostfixing(self):
         """Test a state machine list including a state machine that will prefix
         another state machine in the list"""
         slist=StateMachineList(None)
@@ -240,7 +242,14 @@ class StateMachineTest(unittest.TestCase):
         # Add two TestMachines
         slist.addMachine(TestMachine())
         slist.addMachine(TestPrefixingMachine())
-        slist.addMachine(TestPostfixingMachine())
+        # Try it postfixing
+        slist.addMachine(TestMachine({StateMachine.STATE_INIT: StateRv(1),
+            1: StateRv(2),
+            2: StateRv(3, prefixes=None, postfixes=[TestMachine()]),
+            3: StateRv(4),
+            4: StateRv(5),
+            5: StateRv(StateMachine.STATE_DONE)}))
+        # And another plain test machine
         slist.addMachine(TestMachine())
 
         self.assertStateSequence(slist, [
@@ -258,6 +267,52 @@ class StateMachineTest(unittest.TestCase):
             StateMachine.STATE_INIT, 1, 2, 3,
             # And then the TestMachine that was brought in by the postfixer
             StateMachine.STATE_INIT, 1, 2, 3
+            ])
+
+    def testDoublePrefixing(self):
+        """Test a state machine list including a state machine that will prefix
+        two state machines in the list"""
+        slist=StateMachineList(None)
+
+        aStates={StateMachine.STATE_INIT: StateRv(10),
+            10: StateRv(11), 11: StateRv(12),
+                12: StateRv(StateMachine.STATE_DONE)}
+        bStates={StateMachine.STATE_INIT: StateRv(20),
+            20: StateRv(21), 21: StateRv(22),
+                22: StateRv(StateMachine.STATE_DONE)}
+
+        # Add two TestMachines
+        slist.addMachine(TestMachine())
+        slist.addMachine(TestPrefixingMachine())
+        # Try it postfixing
+        slist.addMachine(TestMachine({StateMachine.STATE_INIT: StateRv(1),
+            1: StateRv(2),
+            2: StateRv(3,
+                prefixes=[TestMachine(aStates), TestMachine(bStates)]),
+            3: StateRv(4),
+            4: StateRv(StateMachine.STATE_DONE)}))
+        # And another plain test machine
+        slist.addMachine(TestMachine())
+
+        self.assertStateSequence(slist, [
+            # TestMachine
+            StateMachine.STATE_INIT, 1, 2, 3,
+            # TestPrefixingMachine - 2, then ...
+            StateMachine.STATE_INIT, 1, 2,
+            # ... a TestMachine that got prefixed
+            StateMachine.STATE_INIT, 1, 2, 3,
+            # follwed by the continuation of the TestPrefixingMachine
+            3, 4,
+            # Now for the weird double prefixer
+            StateMachine.STATE_INIT, 1, 2,
+            # First test
+            StateMachine.STATE_INIT, 10, 11, 12,
+            # Second test
+            StateMachine.STATE_INIT, 20, 21, 22,
+            # Finish the weird machine
+            3, 4,
+            # And one more Test machine
+            StateMachine.STATE_INIT, 1, 2, 3,
             ])
 
 if __name__ == '__main__':
