709c2734eb0e53d30dec7a0559e1dd29cb52b9fd
Dustin Sallings <dsallings@2wire.com>
2004-12-08 16:17:42 +0000

    Updated soapfilepost for python 2.4
    imported from: dustin@spy.net--projects-2004/snippets--python--1.0--patch-86

--- python/net/http/soapfilepost.py
+++ python/net/http/soapfilepost.py
@@ -6,8 +6,15 @@ Copyright (c) 2004  Dustin Sallings <dustin@spy.net>
 # arch-tag: 0E5E491A-4313-11D9-A1C3-000393CFE6B8
 
 import sys
-import ClientCookie
-
+import urllib2
+try:
+    import cookielib
+    cj = cookielib.CookieJar()
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+except ImportError:
+    import ClientCookie
+    cj = ClientCookie.LWPCookieJar()
+    opener = ClientCookie.build_opener(ClientCookie.HTTPCookieProcessor(cj))
 
 if __name__ == '__main__':
 
@@ -17,9 +24,9 @@ if __name__ == '__main__':
     for fn in sys.argv[2:]:
         sys.stderr.write("*** Sending " + fn + "\n")
         f=open(fn)
-        req=ClientCookie.Request(sys.argv[1], f.read(), headers)
+        req=urllib2.Request(sys.argv[1], f.read(), headers)
         f.close()
-        response=ClientCookie.urlopen(req)
+        response=opener.open(req)
         sys.stdout.writelines(response.info().headers)
         print ""
         print response.read()
