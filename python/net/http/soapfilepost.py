9a448220d15c28ba28ac610f3e8c0b2e444a688c
Dustin Sallings <dustin@spy.net>
2005-05-17 03:01:13 +0000

    Updated my soapfilepost thingy to print out error bodies, too.
    imported from: dustin@spy.net--snippets/python--main--1.0--patch-16

--- python/net/http/soapfilepost.py
+++ python/net/http/soapfilepost.py
@@ -7,19 +7,29 @@ Copyright (c) 2004  Dustin Sallings <dustin@spy.net>
 
 import sys
 import urllib2
+import traceback
 try:
     import cookielib
-    cj = cookielib.CookieJar()
-    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+    cookieJar = cookielib.CookieJar()
+    cookieProcessor=urllib2.HTTPCookieProcessor(cookieJar)
+    openerFactory = urllib2.build_opener
 except ImportError:
     import ClientCookie
-    cj = ClientCookie.MozillaCookieJar()
-    opener = ClientCookie.build_opener(ClientCookie.HTTPCookieProcessor(cj))
+    cookieJar = ClientCookie.MozillaCookieJar()
+    cookieProcessor=ClientCookie.HTTPCookieProcessor(cookieJar)
+    openerFactory = ClientCookie.build_opener
+
+class ErrorHandler(urllib2.HTTPDefaultErrorHandler):
+    def http_error_default(self, req, fp, code, msg, hdrs):
+        print "*** Got an error %d ***" % (code, )
+        # print self, req, fp, code, msg, headers
+        return fp
 
 if __name__ == '__main__':
 
     headers={'SOAPAction': 'Inform', 'Content-type': 'text/xml'}
     url=sys.argv[1]
+    opener=openerFactory(ErrorHandler(), cookieProcessor)
 
     for fn in sys.argv[2:]:
         sys.stderr.write("*** Sending " + fn + "\n")
