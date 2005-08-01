a9db7d06083098f73011afec29407d093b8b5d2f
Dustin Sallings <dustin@spy.net>
2005-08-01 14:58:09 +0000

    GPG interface.
    imported from: dustin@spy.net--snippets/python--main--1.0--patch-22

---
+++ python/security/gpg.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+"""
+My GPG module.
+
+Copyright (c) 2005  Dustin Sallings <dustin@spy.net>
+"""
+# arch-tag: 52E1C7DC-0810-4EB6-B254-6368D46068C0
+
+import os
+import exceptions
+import subprocess
+import cStringIO
+
+class Operation(object):
+    """An operation for GPG to do."""
+
+    def getCmd(self):
+        """Get the command this operation specifies."""
+        raise exceptions.NotImplementedError(self.__class__.__module__ + "."
+            + self.__class__.__name__ + " does not implement getCmd")
+
+class Encrypt(Operation):
+    """An encrypt operation."""
+
+    def __init__(self, recipients):
+        """Encrypt with the given list of recipients."""
+        self.recipients=recipients
+
+    def getCmd(self):
+        rv=[]
+        rv.append("-e")
+        for i in self.recipients:
+            rv.append("-r")
+            rv.append(i)
+        return(rv)
+
+class AsciiArmor(Operation):
+    """Use Ascii armor."""
+    def getCmd(self):
+        return ["-a"]
+
+class CompositeCommand(Operation):
+    """Command made up of multiple other commands."""
+
+    def __init__(self, cmds):
+        self.cmds=cmds
+
+    def getCmd(self):
+        rv=[]
+        for x in self.cmds:
+            rv.extend(x.getCmd())
+        return rv
+
+class GPG(object):
+    """GPG Interface."""
+
+    def __init__(self, gpg_location):
+        """Get a GPG instance using the given location of the GPG binary."""
+
+        self.gpg=gpg_location
+
+    def __execute(self, op, instream, outstream, errstream):
+        """Execute the given set of operations reading data from the given
+        input stream and writing to the given output stream."""
+
+        args=[self.gpg, '--batch']
+        args.extend(op.getCmd())
+
+        sub=subprocess.Popen(args,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            close_fds=True)
+        sub.stdin.write(instream.read())
+        sub.stdin.close()
+        outstream.write(sub.stdout.read())
+        errstream.write(sub.stderr.read())
+
+        os.waitpid(sub.pid, 0)
+
+    def encryptString(self, recips, msg):
+        """Convenience method for encrypting a message.
+           Returns (encrypted_msg, warnings) as strings"""
+
+        ops=CompositeCommand([AsciiArmor(), Encrypt(recips)])
+
+        sbuf=cStringIO.StringIO()
+        ebuf=cStringIO.StringIO()
+        self.__execute(ops, cStringIO.StringIO(msg), sbuf, ebuf)
+        return sbuf.getvalue(), ebuf.getvalue()
---
+++ python/security/gpgtest.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+"""
+
+Copyright (c) 2005  Dustin Sallings <dustin@spy.net>
+"""
+# arch-tag: 4F5BAC9B-85BE-46C1-AA9C-59E8E9F5AFD1
+
+import unittest
+import exceptions
+
+import gpg
+
+class OpTest(unittest.TestCase):
+
+    def testBase(self):
+        """Test the operation base."""
+        op=gpg.Operation()
+        try:
+            rv=op.getCmd()
+            self.fail("Shouldn't be able to invoke op.getCmd, got " + rv)
+        except exceptions.NotImplementedError, e:
+            self.assertEquals(str(e), "gpg.Operation does not implement getCmd")
+
+        class Tmp(gpg.Operation):
+            pass
+        t=Tmp()
+        try:
+            rv=t.getCmd()
+            self.fail("Shouldn't be able to invoke t.getCmd, got " + rv)
+        except exceptions.NotImplementedError, e:
+            self.assertEquals(str(e), "__main__.Tmp does not implement getCmd")
+
+    def testEncrypt(self):
+        """Test the encrypt operation"""
+        op=gpg.Encrypt(['home', 'work', 'other'])
+
+        rv=op.getCmd()
+        self.assertEquals(rv, ['-e', '-r', 'home', '-r', 'work', '-r', 'other'])
+
+    def testAsciiArmor(self):
+        """Test the ascii armor operation."""
+        op=gpg.AsciiArmor()
+        self.assertEquals(op.getCmd(), ['-a'])
+
+    def testCompositeCommand(self):
+        """Test the command compositing."""
+        op=gpg.CompositeCommand([gpg.AsciiArmor(),
+            gpg.Encrypt(["test", "test2"])])
+        self.assertEquals(op.getCmd(),
+            ['-a', '-e', '-r', 'test', '-r', 'test2'])
+
+class GPGTest(unittest.TestCase):
+    """Test the GPG object."""
+
+    def testEncryptString(self):
+        """Test encrypting a string."""
+        g=gpg.GPG('/usr/local/bin/gpg')
+        encrypted, warnings=g.encryptString(['primary'], 'hello')
+        lines=encrypted.split("\n")
+        self.failUnless("-----BEGIN PGP MESSAGE-----" in lines)
+        self.failUnless("-----END PGP MESSAGE-----" in lines)
+        # begin, end, version, empty line, data, checksum
+        self.failUnless(len(lines) > 6)
+
+if __name__ == '__main__':
+    unittest.main()
