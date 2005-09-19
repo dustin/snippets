e7409dfe63eea4720b1d252ced2500dde51adc8d
Dustin Sallings <dustin@spy.net>
2005-09-19 03:53:41 +0000

    Moved the zipcode data into the geo schema.
    imported from: dustin@spy.net--snippets/sql--main--1.0--patch-1

--- sql/zip.sql
+++ sql/zip.sql
@@ -4,48 +4,48 @@
 
 -- My zipcode stuff
 
-create table zips.states (
+create table geo.states (
 	state_id serial,
 	code char(2) not null,
 	name varchar(32) not null,
 	primary key(state_id)
 );
-create unique index states_bycode on zips.states(code);
-create unique index states_byname on zips.states(name);
+create unique index states_bycode on geo.states(code);
+create unique index states_byname on geo.states(name);
 
-create table zips.counties (
+create table geo.counties (
 	county_id serial,
 	state_id integer not null,
 	name varchar(40) not null,
 	primary key(county_id),
-	foreign key(state_id) references zips.states(state_id)
+	foreign key(state_id) references geo.states(state_id)
 );
-create index counties_bystate on zips.counties(state_id);
-create index counties_byname on zips.counties(name);
+create index counties_bystate on geo.counties(state_id);
+create index counties_byname on geo.counties(name);
 
-create table zips.cities (
+create table geo.cities (
 	city_id serial,
 	county_id integer not null,
 	name varchar(40) not null,
 	primary key(city_id),
-	foreign key(county_id) references zips.counties(county_id)
+	foreign key(county_id) references geo.counties(county_id)
 );
-create index cities_bycounty on zips.cities(county_id);
-create index cities_byname on zips.cities(name);
+create index cities_bycounty on geo.cities(county_id);
+create index cities_byname on geo.cities(name);
 
-create table zips.zipcodes (
+create table geo.zipcodes (
 	zipcode integer not null,
 	city_id integer not null,
 	longitude float not null,
 	latitude float not null,
 	primary key(zipcode),
-	foreign key(city_id) references zips.cities(city_id)
+	foreign key(city_id) references geo.cities(city_id)
 );
-create index zipcodes_byzip on zips.zipcodes(zipcode);
-create index zipcodes_bycity on zips.zipcodes(city_id);
+create index zipcodes_byzip on geo.zipcodes(zipcode);
+create index zipcodes_bycity on geo.zipcodes(city_id);
 
 -- Here's a view for the way things used to look.
-create view zips.zipcode_view as
+create view geo.zipcode_view as
 	select
 		z.zipcode,
 		s.code as state_code,
@@ -54,8 +54,8 @@ create view zips.zipcode_view as
 		co.name as county,
 		z.longitude,
 		z.latitude
-	from zips.zipcodes z join zips.cities ci using (city_id)
-		join zips.counties co using (county_id)
-		join zips.states s using (state_id)
+	from geo.zipcodes z join geo.cities ci using (city_id)
+		join geo.counties co using (county_id)
+		join geo.states s using (state_id)
 	order by z.zipcode
 ;
