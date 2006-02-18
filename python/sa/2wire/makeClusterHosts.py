0f6880c6aef41a4fc432b5dcb2cbe52e98c5ca67
Dustin Sallings <dustin@spy.net>
2006-02-18 09:17:33 +0000

    Resin cluster host maker.
    imported from: dustin@spy.net--snippets/python--main--1.0--patch-70

---
+++ python/sa/2wire/makeClusterHosts.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+"""
+Make a cluster hosts file from a resin URL.
+
+Copyright (c) 2005  Dustin Sallings <dustin@spy.net>
+"""
+# arch-tag: 55C357EF-9308-4D5D-8F9B-38CE97C2BA85
+
+import re
+import sys
+import urllib2
+
+def fetchHosts(url):
+    """Get the hosts discovered via the given resin status URL."""
+    pattern=re.compile(".*>\d+\..(.*):\d+")
+    rv=[]
+    f=urllib2.urlopen(url)
+    for line in f.readlines():
+        g=pattern.match(line)
+        if g is not None:
+            a=g.groups()[0].split('.')
+            # Remove the last two parts of the host name (i.e. 2wire.com)
+            host='.'.join(a[:-2])
+            if host not in rv:
+                rv.append(host)
+    f.close()
+    return rv
+
+if __name__ == '__main__':
+    print "Cluster " + " ".join(sys.argv[2:])
+    for h in fetchHosts(sys.argv[1]):
+        print "\t" + h
