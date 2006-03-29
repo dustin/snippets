814c3b54456ccd924be66d6b9b9e7efcf0042cfe
Dustin Sallings <dustin@spy.net>
2006-03-29 15:02:58 +0000

    Removed sems from math utilities.
    imported from: dustin@spy.net--snippets/ocaml--main--1.0--patch-23

--- ocaml/lib/math.ml
+++ ocaml/lib/math.ml
@@ -6,9 +6,9 @@
 
 (** Some math utilities. *)
 
-open Num;;
+open Num
 
-let one = num_of_int 1;;
+let one = num_of_int 1
 
 (** Calculate the factorial of the given parameter. *)
 let rec fact x =
@@ -17,14 +17,11 @@ let rec fact x =
 	) else (
 		x */ (fact (pred_num x))
 	)
-;;
 
 (** Calculation the number of combinations of n taken k at a time. *)
 let combo n k =
 	(fact n) // ((fact k) */ (fact (n -/ k)))
-;;
 
 (** Int wrapper for small combinations. *)
 let combo_int n k =
-	int_of_num (combo (num_of_int n) (num_of_int k));
-;;
+	int_of_num (combo (num_of_int n) (num_of_int k))
