157d9e9177c0993fbd886667a031763fbba935d9
Dustin Sallings <dustin@spy.net>
2006-06-10 02:56:58 +0000

    Simplified and more robust blockDigest
    imported from: dustin@spy.net--snippets/ocaml--main--1.1--patch-5

--- ocaml/misc/blockDigest.ml
+++ ocaml/misc/blockDigest.ml
@@ -14,36 +14,39 @@
 let digest_of_block s len =
 	Digest.to_hex (Digest.substring s 0 len)
 
-(**
- Get the previously stored digest.
- *)
-let get_old_digest ch =
-	try
-		input_line ch
-	with End_of_file -> ""
+(** Take a string and int and make a filename.
+ e.g. ``x aabbcc 213'' -> x/aa/aabbcc.213 *)
+let srcFnForBlock d s =
+	Printf.sprintf "%s/%s/%s.%d" d (String.sub s 0 2) s
+
+(** true if the given file exists *)
+let exists f =
+	try (Unix.access f [Unix.F_OK]; true) with _ -> false
+
+let makefn = Printf.sprintf "%s/%05d"
 
 (**
  Deal with the blocks.
 
- @param bs function to call when the blocks are the same
- @param bd function to call when the blocks differ
- @param digestin the input digest file
- @param digestout the output (new) digest file
- @param ch the input stream we're comparing
+ For every block, check to see if we have a file with the given md5 and size.
+ If not, create one.  Then hard link the current filename to that file.
+
+ @param blocksize the size of the blocks to read
+ @param md5dir the directory containing the md5 blocks
+ @param newdir the directory that will receive the new files
+ @param bd function to call when a block is unknown
+ @param ch the file being digested
  *)
-let do_blocks blocksize bs bd digestin digestout ch =
-	(* Eat the first line of the oldmd5 file (meta info) *)
-	ignore(get_old_digest digestin);
+let do_blocks blocksize md5dir newdir bd ch =
 	let buf = String.create blocksize in
 	let rec loop n =
 		let bytesread = Fileutils.input_block ch buf 0 blocksize in
-		let old_digest = get_old_digest digestin in
 		let new_digest = digest_of_block buf bytesread in
+		let srcFile = srcFnForBlock md5dir new_digest bytesread in
 		(* Let the caller know whether it's the same or different *)
-		(if new_digest = old_digest then bs else bd) n
-			(String.sub buf 0 bytesread) new_digest old_digest;
-		(* Record the new digest *)
-		output_string digestout (new_digest ^ "\n");
+		if not (exists srcFile) then bd srcFile newdir n
+			(String.sub buf 0 bytesread) new_digest;
+		Unix.link srcFile (makefn newdir n);
 		(* If we didn't read EOF, do it again *)
 		if bytesread = blocksize then (
 			loop (succ n)
@@ -51,26 +54,17 @@ let do_blocks blocksize bs bd digestin digestout ch =
 		in
 	loop 0
 
-let makefn = Printf.sprintf "%s/%05d"
-
-let block_same prevdir newdir n data digest old_digest =
-	let oldpath = makefn prevdir n in
-	let newpath = makefn newdir n in
-	(* print_endline ("Linking " ^ oldpath ^ " -> " ^ newpath); *)
-	Unix.link oldpath newpath
-
 exception Subprocess_error of int;;
 
-let block_diff prevdir newdir prog n data digest old_digest =
+let block_diff prog srcFile newdir n data digest =
 	(*
 	Printf.eprintf "Difference at block %d - %s != %s\n%!" n digest old_digest;
 	*)
 	let args = (Array.of_list
 		["BLOCK=" ^ (string_of_int n);
 		 "DIGEST=" ^ digest;
-		 "PREVDIR=" ^ prevdir;
 		 "NEWDIR=" ^ newdir;
-		 "PREVPATH=" ^ (makefn prevdir n);
+		 "PREVPATH=" ^ srcFile;
 		 "NEWPATH=" ^ (makefn newdir n)]) in
 	(* Open the subprocess *)
 	let pout,pin,perr = Unix.open_process_full prog args in
@@ -88,50 +82,36 @@ let block_diff prevdir newdir prog n data digest old_digest =
  Process the given files.
 
  @param blocksize digest block size
- @param bs function to call when the blocks are the same
  @param bd function to call when the blocks are different
  @param datafn name of the file containing the data (may be - for stdin)
- @param oldmd5 location of the file containing existing md5s
- @param tmpmd5 location of the tmp file that will contain the new md5s
  *)
-let process blocksize bs bd datafn oldmd5 tmpmd5 =
-	let newdigestch = (open_out tmpmd5) in
-	(* Write out the block size on the first line *)
-	output_string newdigestch ("# block size:  "
-		^ (string_of_int blocksize) ^ "\n");
+let process blocksize md5dir newdir bd datafn =
 	Fileutils.operate_on_file_in
-		(do_blocks blocksize bs bd (open_in oldmd5) newdigestch) Sys.argv.(1);
-	close_out newdigestch;
-	Unix.rename tmpmd5 oldmd5
+		(do_blocks blocksize md5dir newdir bd) Sys.argv.(1)
 
 (**
- Args:  input_file digest_file diff_cmd
+ Args:  streamfn diffscript md5dir newdir [blocksize]
  *)
 let main() =
 	(* Arg checking *)
-	if (Array.length Sys.argv) < 6 then (
+	if (Array.length Sys.argv) < 5 then (
 		prerr_endline("Usage:  " ^ Sys.argv.(0)
-			^ " streamfn digestfile diffscript prevdir newdir [blocksize]");
+			^ " streamfn diffscript md5dir newdir [blocksize]");
 		exit(1)
 	);
 	let blocksize = ref (8 * 1024 * 1024) in
-	if (Array.length Sys.argv) > 6 then (
-		blocksize := (int_of_string Sys.argv.(6))
+	if (Array.length Sys.argv) > 5 then (
+		blocksize := (int_of_string Sys.argv.(5))
 	);
 	if !blocksize > Sys.max_string_length then (
 		blocksize := Sys.max_string_length
 	);
 	(* Pop off the args *)
 	let streamfn = Sys.argv.(1) in
-	let oldfn = Sys.argv.(2) in
-	let newfn = (oldfn ^ ".new") in
-	let scriptname = Sys.argv.(3) in
-	let prevdir = Sys.argv.(4) in
-	let newdir = Sys.argv.(5) in
-	process !blocksize
-		(block_same prevdir newdir)
-		(block_diff prevdir newdir scriptname)
-		streamfn oldfn newfn
+	let scriptname = Sys.argv.(2) in
+	let md5dir = Sys.argv.(3) in
+	let newdir = Sys.argv.(4) in
+	process !blocksize md5dir newdir (block_diff scriptname) streamfn
 ;;
 
 (* Start main unless we're interactive. *)
