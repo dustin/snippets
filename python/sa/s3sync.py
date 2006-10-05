d3a3b825389fd40547ad918af3d4c589240e119b
Dustin Sallings <dustin@spy.net>
2006-10-05 14:06:32 +0000

    Major refactoring allowing retries on s3 uploads
    imported from: dustin@spy.net--snippets/python--main--1.0--patch-96

--- python/sa/s3sync.py
+++ python/sa/s3sync.py
@@ -12,12 +12,71 @@ import sets
 import time
 import signal
 import httplib
+import traceback
 import exceptions
 
 import s3
 
+# How many times to attempt a given upload
+NUM_TRIES=5
+
 def sighandler(x, st):
     raise exceptions.Exception("Got signal " + `x`)
+
+def addOne(bucket, relpath):
+    signal.alarm(900)
+    print "Adding", relpath
+    try:
+        f=open(relpath)
+        ob=s3.S3Object(relpath, f, {})
+        bucket.save(ob)
+        print "...done"
+    finally:
+        f.close()
+    signal.alarm(0)
+
+def addWithRetry(bucket, relpath):
+    tries=0
+    success=False
+
+    while not success:
+        tries += 1
+        try:
+            addOne(bucket, relpath)
+            success=True
+        except:
+            if tries >= NUM_TRIES:
+                raise
+            else:
+                traceback.print_exc()
+                print "Trying again in %d second(s)" % (tries,)
+                time.sleep(tries)
+
+def doAdditions(bucket, remote):
+    local=sets.Set()
+    added=0
+
+    for d, dirs, files in os.walk("."):
+        for f in files:
+            relpath=os.path.join(d, f)
+            assert relpath[0:2] == './'
+            relpath=relpath[2:]
+            local.add(relpath)
+
+            if relpath not in remote:
+                addWithRetry(bucket, relpath)
+                added += 1
+
+    return added, local
+
+def doDeletions(bucket, todelete):
+    deleted = 0
+    for f in todelete:
+        print "Deleting", f
+        bucket.delete(f)
+        print "...done"
+        deleted += 1
+    return deleted
 
 if __name__ == '__main__':
     # This doesn't exist in my older httplib, but s3 needs it
@@ -32,39 +91,15 @@ if __name__ == '__main__':
     bucket=c[bucket]
     assert bucket is not None
     remote=sets.Set(bucket.keys())
-    local=sets.Set()
+    os.chdir(top)
 
-    added=0
     deleted=0
 
     signal.signal(signal.SIGALRM, sighandler)
 
-    try:
-        os.chdir(top)
-        for d, dirs, files in os.walk("."):
-            for f in files:
-                relpath=os.path.join(d, f)
-                assert relpath[0:2] == './'
-                relpath=relpath[2:]
-                local.add(relpath)
+    added, local=doAdditions(bucket, remote)
+    deleted = doDeletions(bucket, remote.difference(local))
 
-                if relpath not in remote:
-                    signal.alarm(900)
-                    print "Adding", relpath
-                    f=open(relpath)
-                    ob=s3.S3Object(relpath, f, {})
-                    bucket.save(ob)
-                    f.close()
-                    print "...done"
-                    signal.alarm(0)
-                    added += 1
-
-        for f in remote.difference(local):
-            print "Deleting", f
-            bucket.delete(f)
-            print "...done"
-            deleted += 1
-    finally:
-        print "Added:", added
-        print "Deleted:", deleted
+    print "Added:", added
+    print "Deleted:", deleted
 
