1830a51af85f8aa486dee32ff32309d975c8d102
Dustin Sallings <dustin@spy.net>
2006-10-10 04:00:34 +0000

    Added stuff to deal with apparently truncted results.
    imported from: dustin@spy.net--snippets/python--main--1.0--patch-97

--- python/sa/s3sync.py
+++ python/sa/s3sync.py
@@ -64,6 +64,7 @@ def doAdditions(bucket, remote):
             local.add(relpath)
 
             if relpath not in remote:
+                assert not bucket.has_key(relpath)
                 addWithRetry(bucket, relpath)
                 added += 1
 
@@ -91,6 +92,14 @@ if __name__ == '__main__':
     bucket=c[bucket]
     assert bucket is not None
     remote=sets.Set(bucket.keys())
+    # Stupid special case of 1000 items because my list seems to get truncated
+    # there.  There should be a flag indicating whether this list was truncated
+    # or not, but apparently I don't get to see that from here.
+    if len(remote) == 1000:
+        print "Looking for buckets by hex prefix"
+        for p in "0123456789abcdef":
+            remote.union_update(sets.Set(bucket.keys(prefix=p)))
+    print "Found %d items" % (len(remote),)
     os.chdir(top)
 
     deleted=0
