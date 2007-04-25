5e6bfc3eb4acf51f4f6fd9fed3d95a6680f892dc
Dustin Sallings <dustin@spy.net>
2007-04-25 16:14:17 -0700

    Allow overriding of the document handler.

--- python/xml/saxkit.py
+++ python/xml/saxkit.py
@@ -37,12 +37,12 @@ class RootHandler(ElementHandler):
 class StackedHandler(xml.sax.handler.ContentHandler):
     """SAX event handler that will delegate results to sub handlers."""
 
-    def __init__(self, rootType, rootHandler):
+    def __init__(self, rootType, rootHandler, documentHandler=RootHandler):
         """root type and handler expected."""
 
         xml.sax.handler.ContentHandler.__init__(self)
         self.stack=[]
-        self.root=RootHandler(rootType, rootHandler)
+        self.root=documentHandler(rootType, rootHandler)
         self.stack.append(self.root)
 
     def startElementNS(self, name, qname, attrs):
