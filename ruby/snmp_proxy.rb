6792962b2ea7eea3b80e7c5496d17eada5e76514
Dustin Sallings <dustin@spy.net>
2007-11-01 17:09:37 -0700

    Need to bind to multiple ports.

--- ruby/snmp_proxy.rb
+++ ruby/snmp_proxy.rb
@@ -4,11 +4,17 @@ require 'socket'
 
 $SAFE=2
 
-l=UDPSocket.new
-l.bind('', 161)
+# Requested ports
+ports=[161, 162]
+# Create the bound sockets.
+sockets = ports.map{|p| l = UDPSocket.new; l.bind('', p); l }
+puts "Bound sockets:  #{sockets.join(', ')}"
 
 while true:
-  d, stuff=l.recvfrom(1024)
-  puts "proxying #{d.length} bytes of trapness"
-  l.send(d, 0, '127.0.0.1', 1162)
+  ls=IO.select(sockets)
+  ls[0].each do |l|
+    d, stuff=l.recvfrom(1024)
+    puts "proxying #{d.length} bytes of trapness (from #{l})"
+    l.send(d, 0, '127.0.0.1', 1162)
+  end
 end
