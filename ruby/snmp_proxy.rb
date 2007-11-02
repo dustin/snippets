0103c918273b899937563874dd197f6a241ba510
Dustin Sallings <dustin@spy.net>
2007-11-01 17:34:04 -0700

    Build a Hash of the sockets so I can print the port

--- ruby/snmp_proxy.rb
+++ ruby/snmp_proxy.rb
@@ -8,13 +8,14 @@ $SAFE=2
 ports=[161, 162]
 # Create the bound sockets.
 sockets = ports.map{|p| l = UDPSocket.new; l.bind('', p); l }
-puts "Bound sockets:  #{sockets.join(', ')}"
+pmap=Hash[*sockets.zip(ports).flatten]
+puts "Bound to #{ports.join(', ')}"
 
 while true:
   ls=IO.select(sockets)
   ls[0].each do |l|
     d, stuff=l.recvfrom(1024)
-    puts "proxying #{d.length} bytes of trapness (from #{l})"
+    puts "proxying #{d.length} bytes of trapness (from #{pmap[l]})"
     l.send(d, 0, '127.0.0.1', 1162)
   end
 end
