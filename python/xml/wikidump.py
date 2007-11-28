ac6009105e4196f3686cc746c4aaeefe1ffd66ce
Dustin Sallings <dustin@spy.net>
2007-11-27 21:27:28 -0800

    Stage one wikipedia geo extraction.
    Find potential matches and record them in sqlite.

--- python/xml/wikidump.py
+++ python/xml/wikidump.py
@@ -5,18 +5,27 @@ Extract articles containing longitude and latitude points from wikipedia.
 Copyright (c) 2007  Dustin Sallings <dustin@spy.net>
 """
 
-import re
 import sys
+from sqlite3 import dbapi2 as sqlite
 import xml.sax
 import saxkit
 
 ROOT_NS="http://www.mediawiki.org/xml/export-0.3/"
 ROOT_EL="mediawiki"
 
-def rc(s):
-    return re.compile(s, re.M)
+DB=sqlite.connect("wiki.db")
+CUR=DB.cursor()
 
-POINT_REGEX=rc(r"^\|Latitude.*Longitude.*$^|(\d+\S+\s+[NS]).*(\d+\S+\s+[EW])$")
+HAS_SEEN_QUERY="""
+    select 1 from seen_articles where title = ?
+"""
+SEEN_QUERY="""
+    insert into seen_articles values(?)
+"""
+
+INTERESTING_QUERY="""
+    insert into interesting_pages(title, article_text) values(?, ?)
+"""
 
 class OptInHandler(saxkit.ElementHandler):
 
@@ -32,28 +41,39 @@ class RevisionHandler(OptInHandler):
     def addChild(self, name, val):
         if isinstance(val, saxkit.SimpleValueParser):
             s=val.getValue()
-            v = POINT_REGEX.search(s)
-            if v:
-                g=v.groups()
-                self.sup.latitude=g[0]
-                self.sup.longitude=g[1]
+            self.sup.text=s
+            if s.find("{{coord|") > 0 or s.find("{{Geolinks") > 0:
+                self.sup.interesting=True
 
 class PageHandler(OptInHandler):
     def __init__(self):
         OptInHandler.__init__(self)
         self.parsers[(ROOT_NS, 'title')]=saxkit.SimpleValueParser()
-        self.parsers[(ROOT_NS, 'revision')]=RevisionHandler(self)
 
-        self.latitude=None
-        self.longitude=None
+        self.was_seen=False
+        self.interesting=False
 
     def addChild(self, name, val):
         if name[1] == 'title':
             self.title=val.getValue()
 
+    def has_seen(self, title):
+        assert title
+        CUR.execute(HAS_SEEN_QUERY, (title, ))
+        if CUR.fetchall():
+            self.was_seen=True
+        return self.was_seen
+
+    def getParser(self, name):
+        rv=None
+        if name == (ROOT_NS, 'revision') and not self.has_seen(self.title):
+            rv=RevisionHandler(self)
+        else:
+            rv=OptInHandler.getParser(self, name)
+        return rv
+
     def __unicode__(self):
-        return "<Page name='%s', latitude=%s, longitude=%s>" % (self.title,
-            self.latitude, self.longitude)
+        return "<Page name='%s', %d bytes>" % (self.title, len(self.text))
 
 class RootHandler(OptInHandler):
     def __init__(self):
@@ -61,8 +81,13 @@ class RootHandler(OptInHandler):
         self.parsers[(ROOT_NS, 'page')]=PageHandler
 
     def addChild(self, name, val):
-        if isinstance(val, PageHandler) and val.latitude:
-            print unicode(val)
+        if isinstance(val, PageHandler):
+            if not val.was_seen:
+                CUR.execute(SEEN_QUERY, (val.title,))
+            if val.interesting:
+                print unicode(val)
+                CUR.execute(INTERESTING_QUERY, (val.title, val.text))
+                DB.commit()
 
 if __name__ == '__main__':
     handler=saxkit.StackedHandler(
@@ -72,3 +97,7 @@ if __name__ == '__main__':
     parser.setContentHandler(handler)
     parser.parse(sys.stdin)
     sm=handler.getRootElement()
+
+    DB.commit()
+    CUR.close()
+    DB.close()
