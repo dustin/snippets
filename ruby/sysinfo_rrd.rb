6ecedf110ae26199417d5b542c364b2667dfcb86
Dustin Sallings <dustin@spy.net>
2007-12-03 17:20:50 -0800

    System info module for ruby

---
+++ ruby/sysinfo_rrd.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby -w
+
+class LinuxHostInfo
+
+  def loadavg
+    open("/proc/loadavg") { |f| f.read.split[0..2].map {|x| x.to_f} }
+  end
+
+  def cpustat
+    process_lines("/proc/stat") { |s| s.map {|x| x.to_i} }
+  end
+
+  def vmstat
+    process_lines("/proc/stat") { |s| s[0].to_i }
+  end
+
+  def meminfo
+    process_lines("/proc/meminfo") do |s|
+      raise "Unhandled multiplier:  #{s[1]}" if s[1] != "kB"
+      s[0].to_i * 1024
+    end
+  end
+
+  private
+
+  def process_lines(fn)
+    rv={}
+    IO.foreach(fn) do |l|
+      stuff=l.split(/:?\s+/)
+      rv[stuff[0]]=yield stuff[1..-1]
+    end
+    rv
+  end
+
+end
