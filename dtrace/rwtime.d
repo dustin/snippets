823219c0bf394d3816ef9923b1d3ec9592bb45c0
Dustin Sallings <dustin@spy.net>
2008-06-05 10:29:31 -0700

    Include writev in rwtime dtrace probe (also show beginning).

--- dtrace/rwtime.d
+++ dtrace/rwtime.d
@@ -1,12 +1,15 @@
 syscall::read:entry,
-syscall::write:entry
+syscall::write:entry,
+syscall::writev:entry
 /pid == $1/
 {
 	ts[probefunc] = timestamp;
+	printf(".");
 }
 
 syscall::read:return,
-syscall::write:return
+syscall::write:return,
+syscall::writev:return
 /pid == $1 && ts[probefunc] != 0/
 {
 	printf("%d nsecs", timestamp - ts[probefunc]);
