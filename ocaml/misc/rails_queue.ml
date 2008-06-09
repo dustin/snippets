2dab1878ded677abf4e5ad8fb859e53a2715e5e1
Dustin Sallings <dustin@spy.net>
2008-06-09 13:33:56 -0700

    Added an ocaml tool to enqueue an async_observer job to a rails app.

---
+++ ocaml/misc/rails_queue.ml
@@ -0,0 +1,15 @@
+(*
+ * Copyright (c) 2007  Dustin Sallings <dustin@spy.net>
+ *)
+
+let main () =
+	let bs = Beanstalk.connect Sys.argv.(1) 11300 in
+	ignore(Beanstalk.put bs 65535 0 900 (
+		Printf.sprintf "---\r\n:type: :rails\r\n:code: %s\r\n" Sys.argv.(2)
+		));
+	Beanstalk.shutdown bs
+;;
+
+(* Start main unless we're interactive. *)
+if !Sys.interactive then () else begin main() end
+
