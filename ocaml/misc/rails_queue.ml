62cd1e6e6193a0390deebdbfc0d22e03a767d77d
Dustin Sallings <dustin@spy.net>
2008-06-24 15:10:25 -0700

    Default job ttr should be an hour.

--- ocaml/misc/rails_queue.ml
+++ ocaml/misc/rails_queue.ml
@@ -4,7 +4,7 @@
 
 let main () =
 	let bs = Beanstalk.connect Sys.argv.(1) 11300 in
-	ignore(Beanstalk.put bs 65535 0 900 (
+	ignore(Beanstalk.put bs 65535 0 3600 (
 		Printf.sprintf "---\r\n:type: :rails\r\n:code: %s\r\n" Sys.argv.(2)
 		));
 	Beanstalk.shutdown bs
