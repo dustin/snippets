bbaa6a0c4494f5418cc4e2d1e8c0071ea17ba38b
Dustin Sallings <dustin@spy.net>
2014-10-25 20:32:26 -0700

    Formatting fixes

--- python/misc/merge-csv.py
+++ python/misc/merge-csv.py
@@ -7,21 +7,21 @@ import sys
 keepers = ['some', 'field', 'names']
 
 def load(fn):
-  name = os.path.basename(os.path.splitext(fn)[0])
-  rv = []
-  with open(fn) as f:
-    r = csv.reader(f)
-    hdr = r.next()
-    hx = dict((y,x) for x,y in enumerate(hdr))
-    for l in r:
-      if not l:
-        continue
-      rv.append( [l[hx[x]] for x in keepers])
+    name = os.path.basename(os.path.splitext(fn)[0])
+    rv = []
+    with open(fn) as f:
+        r = csv.reader(f)
+        hdr = r.next()
+        hx = dict((y,x) for x,y in enumerate(hdr))
+        for l in r:
+            if not l:
+                continue
+            rv.append([l[hx[x]] for x in keepers])
 
-  return rv
+    return rv
 
 w = csv.writer(sys.stdout)
 w.writerow(keepers)
 
 for c in [load(fn) for fn in sys.argv[1:]]:
-  w.writerows(c)
+    w.writerows(c)
