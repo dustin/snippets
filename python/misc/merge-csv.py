383b27f5105c011cd053c6b82be10e0c1fc2ea2e
Dustin Sallings <dustin@spy.net>
2014-10-26 18:12:17 -0700

    Add support for numeric fields in merge-csv

--- python/misc/merge-csv.py
+++ python/misc/merge-csv.py
@@ -4,7 +4,7 @@ import os
 import csv
 import sys
 
-keepers = ['some', 'field', 'names']
+keepers = ['some', 'field', 'names', -1]
 
 def load(fn):
     name = os.path.basename(os.path.splitext(fn)[0])
@@ -13,10 +13,11 @@ def load(fn):
         r = csv.reader(f)
         hdr = r.next()
         hx = dict((y,x) for x,y in enumerate(hdr))
+        cols = [x if isinstance(x, int) else hx[x] for x in keepers]
         for l in r:
             if not l:
                 continue
-            rv.append([l[hx[x]] for x in keepers])
+            rv.append([l[cols] for x in cols])
 
     return rv
 
