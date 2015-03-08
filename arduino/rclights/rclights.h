7de26428a9545642bd93a441dfdc5fe81e813e3e
Dustin Sallings <dustin@spy.net>
2015-03-07 23:42:42 -0800

    PWM-controlled LEDs.

---
+++ arduino/rclights/rclights.h
@@ -0,0 +1,9 @@
+typedef struct {
+    int in;
+    int out;
+    int minValue;
+    int maxValue;
+    int lastValue;
+    bool dirty;
+} sensor;
+
---
+++ arduino/rclights/rclights.ino
@@ -0,0 +1,82 @@
+#include <EEPROM.h>
+
+#include "rclights.h"
+
+const int deadBand = 5;
+const int closeEnough = 20;
+const int eepromTooLow = 500;
+const int eepromTooHigh = 3000;
+
+unsigned long nextWrite = 0;
+
+sensor led1 = { A1, 0, 6000, 0, 0, false};
+sensor led2 = { A2, 1, 6000, 0, 0, false};
+
+void setup() {
+  setupLed(&led1);
+  setupLed(&led2);
+  loadEEProm(&led1, 0);
+  loadEEProm(&led2, 4);
+}
+
+void setupLed(sensor *led) {
+  pinMode(led->in, INPUT);
+  pinMode(led->out, OUTPUT);
+}
+
+void loadEEProm(sensor *led, int pos) {
+  led->minValue = (EEPROM.read(pos) << 8) | EEPROM.read(pos + 1);
+  led->maxValue = (EEPROM.read(pos + 2) << 8) | EEPROM.read(pos + 3);
+  if (led->minValue < eepromTooLow) {
+    led->minValue = 6000;
+  }
+  if (led->maxValue > eepromTooHigh) {
+    led->maxValue = 0;
+  }
+}
+
+void writeEEProm(sensor *led, int pos) {
+  if (led->dirty) {
+    return;
+  }
+
+  EEPROM.write(pos++, led->minValue >> 8);
+  EEPROM.write(pos++, led->minValue & 0xff);
+  EEPROM.write(pos++, led->maxValue >> 8);
+  EEPROM.write(pos++, led->maxValue & 0xff);
+  
+  led->dirty = false;
+}
+
+void rangeCheck(sensor *led, unsigned long val) {
+  if (val < led->minValue || val > led->maxValue) {
+    led->minValue = min(val, led->minValue);
+    led->maxValue = max(val, led->maxValue);
+    led->dirty = true;
+  }
+}
+
+void updateLed(sensor* led) {
+  unsigned long val = pulseIn(led->in, HIGH);
+  rangeCheck(led, val);
+  if (abs(val - led->lastValue) > deadBand) {
+    if (val < led->minValue + closeEnough) {
+      analogWrite(led->out, 0);
+    } else {
+      analogWrite(led->out, map(val, led->minValue, led->maxValue, 0, 255));
+    }
+    led->lastValue = val;
+  }
+}
+
+void loop() {
+  updateLed(&led1);
+  updateLed(&led2);
+  delay(100);
+  
+  if (millis() > nextWrite) {
+    writeEEProm(&led1, 0);
+    writeEEProm(&led2, 4);
+    nextWrite = millis() + 10000;
+  }
+}
