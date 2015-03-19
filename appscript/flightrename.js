a0166eae19bf4772fe3e1995578172523bd208d8
Dustin Sallings <dustin@spy.net>
2015-03-19 11:01:02 -0700

    My Google Drive flight renaming thing

---
+++ appscript/flightrename.js
@@ -0,0 +1,59 @@
+var flightFolder = '0B-xxxxxxxxxxxxxxxx';
+
+function fixupLocationDir(sub) {
+  var renameDirs = sub.getFolders();
+  while (renameDirs.hasNext()) {
+    var rdir = renameDirs.next();
+    Logger.log("Found rename dir in %s: %s", sub.getName(), rdir.getName());
+    
+    var rfiles = rdir.getFiles();
+    while (rfiles.hasNext()) {
+      var rfile = rfiles.next();
+      var newname = rdir.getName() + "-" + rfile.getName();
+      // newname = newname.replace("20141012-2014012-", "");
+      Logger.log("Renaming %s to %s", rfile.getName(), newname);
+      rfile.setName(newname);
+      Logger.log(" -- Adding %s to %s", newname, sub.getName());
+      sub.addFile(rfile);
+      Logger.log(" -- Removing %s from %s", newname, rdir.getName());
+      rdir.removeFile(rfile);
+    }
+  }
+}
+
+function removeEmptyDirsIn(sub) {
+  var renameDirs = sub.getFolders();
+  while (renameDirs.hasNext()) {
+    var rdir = renameDirs.next();
+    Logger.log("Found rename dir in %s: %s", sub.getName(), rdir.getName());
+    
+    var rfiles = rdir.getFiles();
+    var nFiles = 0;
+    while (rfiles.hasNext()) {
+      nFiles++;
+      var rfile = rfiles.next();
+    }
+    if (nFiles > 0) {
+      Logger.log("Keeping %s", rdir.getName());
+    } else {
+      Logger.log("Removing %s", rdir.getName());
+      sub.removeFolder(rdir);
+    }
+  }
+}
+
+function renameFlightMedia() {
+  var folder = DriveApp.getFolderById(flightFolder);
+  var subs = folder.getFolders();
+  while (subs.hasNext()) {
+    fixupLocationDir(subs.next())
+  }
+}
+
+function removeEmptyDirs() {
+  var folder = DriveApp.getFolderById(flightFolder);
+  var subs = folder.getFolders();
+  while (subs.hasNext()) {
+    removeEmptyDirsIn(subs.next())
+  }
+}
