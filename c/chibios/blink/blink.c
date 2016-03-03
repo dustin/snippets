589c652fd36f896ea451680824f1fdfa94df4ce1
Dustin Sallings <dustin@spy.net>
2016-03-02 23:35:56 -0800

    chibios based blink thing (might work)

---
+++ c/chibios/blink/blink.c
@@ -0,0 +1,38 @@
+/*
+ * My simple application.
+ */
+
+#include <ch.h>
+#include <hal.h>
+
+/*
+ * Working area for the LED flashing thread.
+ */
+static THD_WORKING_AREA(myThreadWorkingArea, 128);
+
+#define LED PAL_LINE(GPIOB, 1)
+
+/*
+ * LED flashing thread.
+ */
+static THD_FUNCTION(myThread, arg) {
+    palSetLineMode(LED, PAL_MODE_OUTPUT_PUSHPULL);
+    while (true) {
+        palClearLine(LED);
+        chThdSleepMilliseconds(500);
+        palSetLine(LED);
+        chThdSleepMilliseconds(500);
+    }
+}
+
+int main(void) {
+
+    halInit();
+    chSysInit();
+
+    /* Starting the flashing LEDs thread.*/
+    (void)chThdCreateStatic(myThreadWorkingArea, sizeof(myThreadWorkingArea),
+                            NORMALPRIO, myThread, NULL);
+
+    for (;;) {}
+}
