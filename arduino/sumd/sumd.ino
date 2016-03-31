d1bb6a253a86c3690bc175822f5367ba0deb3234
Dustin Sallings <dustin@spy.net>
2016-03-31 00:06:09 -0700

    Various SUMD bug fixes.
    Almost works a little.

--- arduino/sumd/sumd.ino
+++ arduino/sumd/sumd.ino
@@ -6,19 +6,13 @@
 
 volatile uint16_t channels[NCHAN];
 
-union chandata {
-    uint16_t val;
-    byte data[2];
-};
-
 struct {
     byte magic;
     byte hdr1;
-    byte hdr2;
     byte nchan;
-    union chandata channels[NCHAN*2];
+    byte channels[NCHAN*2];
     byte crc[2];
-} packet;
+} packet __attribute__((packed));
 
 void setup() {
     Serial.begin(115200);
@@ -46,24 +40,38 @@ uint16_t CRC16(uint16_t crc, uint8_t value) {
 void populateChans() {
     packet.hdr1 = SUMD_VALID;
 
+    uint16_t crc = CRC16(0, packet.magic);
+    crc = CRC16(crc, packet.hdr1);
+    crc = CRC16(crc, packet.nchan);
+
+    int j = 0;
     for (int i = 0; i < NCHAN; i++) {
-        packet.channels[i].val = 8*channels[i];
-    }
-    uint16_t crc = 0;
-    byte *b = (byte*)&packet;
-    for (int i = 0; i < sizeof(packet)-2; i++) {
-        crc = CRC16(crc, b[i]);
+        uint16_t val = 8*channels[i];
+
+        packet.channels[j] = val >> 8;
+        crc = CRC16(crc, packet.channels[j]);
+        j++;
+
+        packet.channels[j] = val&0xff;
+        crc = CRC16(crc, packet.channels[j]);
+        j++;
     }
     packet.crc[0] = crc >> 8;
     packet.crc[1] = crc & 0xff;
 }
 
+void transmit() {
+    Serial.write((byte*)&packet, sizeof(packet));
+}
+
 void loop() {
     for (int i = 0; i < NCHAN; i++) {
-        channels[i] += (1 << i);
+        channels[i] += (i*2);
         if (channels[i] > 2000) {
             channels[i] = 1000;
         }
     }
+    populateChans();
+    transmit();
     delay(1000);
 }
