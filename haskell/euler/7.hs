77b0117e0f16e7d3fee050eca22f925072ed81fe
Dustin Sallings <dustin@spy.net>
2017-09-11 13:49:41 -0700

    lib for 7

--- haskell/euler/7.hs
+++ haskell/euler/7.hs
@@ -3,25 +3,6 @@
 
 -- What is the 10001^(st) prime number?
 
--- I stole this prime list from one of my first attempts to do
--- something in haskel.
-isPrime :: Integer -> Bool
+import Euler
 
-isPrime 0 = False
-isPrime 1 = False
-isPrime 2 = True
-isPrime x
-        | even(x)   = False
-	| otherwise = rprime 3
-	where rprime a
-                | (a ^ 2) > x    = True
-                | mod x a == 0   = False
-                | otherwise      = rprime (a + 2)
-
--- All prime numbers
-primes = [ x | x <- [2..], isPrime x ]
-
-euler_7 :: Int -> Integer
-
--- Almost curried this, were it not for the stupid 0-base
-euler_7 n = primes !! (n - 1)
+euler_7 = primes !! (10000)
