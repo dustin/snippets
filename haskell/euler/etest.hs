aea92bbb47eb470588990111b09c153fcc1f94cc
Dustin Sallings <dustin@spy.net>
2017-09-12 13:18:13 -0700

    Factor tests and use Test.QuickCheck.Modifiers

--- haskell/euler/etest.hs
+++ haskell/euler/etest.hs
@@ -1,9 +1,19 @@
 import Euler
 import Test.QuickCheck
+import Test.QuickCheck.Modifiers
 
-digits_symmetry_prop x = x < 0 || (x == (undigits.digits) x)
+digits_symmetry_prop (Positive x) = x == (undigits.digits) x
 
+divisors_divise_prop (Positive x) = all (\d -> x `mod` d == 0) $ divisors x
+
+factor_divise_prop (Positive x) = all (\d -> x `mod` d == 0) $ factor x
+
+factor'_divise_prop (Positive x) = all (\d -> x `mod` d == 0) $ factor' x
 
 main :: IO ()
-main =
+main = do
   quickCheck digits_symmetry_prop
+  quickCheck divisors_divise_prop
+  quickCheck factor_divise_prop
+  quickCheck factor'_divise_prop
+
