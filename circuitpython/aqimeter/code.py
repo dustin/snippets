190aca2aee5b00fa73799d418f233687d9307f7c
Dustin Sallings <dustin@spy.net>
2021-08-18 20:50:02 -0700

    Use MQTT for aqi meter

--- circuitpython/aqimeter/code.py
+++ circuitpython/aqimeter/code.py
@@ -12,24 +12,10 @@ except ImportError:
     print("WiFi secrets are kept in secrets.py, please add them there!")
     raise
 
-magtag = MagTag(url='http://192.168.2.170/json',
-                json_path=[["pm2.5_aqi"], ["pm2.5_aqi_b"], ["current_temp_f"]],
-                # json_transform=lambda d: d['aqi'] = (val[0] + val[1]) / 2.0
-                )
-
-magtag.peripherals.neopixel_disable = False
-magtag.peripherals.neopixels.fill((8, 0, 0))
-wifi.radio.connect(secrets["ssid"], secrets["password"])
-magtag.peripherals.neopixels.fill((6, 3, 16))
-
-val = magtag.fetch(auto_refresh=False)
-magtag.peripherals.neopixels.fill((0, 0, 0))
-magtag.peripherals.neopixel_disable = True
+magtag = MagTag()
 
 magtag.add_text(
     text_font="/fonts/Helvetica-Bold-100.bdf",
-    # text_font=terminalio.FONT,
-    # text_position=(70, 40),
     text_position=(
         (magtag.graphics.display.width // 2) - 1,
         (magtag.graphics.display.height // 2) - 22,
@@ -39,13 +25,46 @@ magtag.add_text(
 
 magtag.add_text(
     text_font="/fonts/Arial-Bold-12.pcf",
-    # text_font=terminalio.FONT,
     text_position=(10, magtag.graphics.display.height - 14),
 )
 
-magtag.set_text('{aqi:.0f}'.format(aqi=(val[0] + val[1]) / 2.0), 0, False)
-magtag.set_text('Battery: {bat:.2f}V'.format(bat=magtag.peripherals.battery), 1)
+magtag.peripherals.neopixel_disable = False
+magtag.peripherals.neopixels.fill((8, 0, 0))
+wifi.radio.connect(secrets["ssid"], secrets["password"])
+magtag.peripherals.neopixels.fill((6, 3, 16))
 
-time.sleep(2)
+def message(client, topic, message):
+    magtag.peripherals.neopixels.fill((0, 0, 0))
+    magtag.peripherals.neopixel_disable = True
 
-magtag.exit_and_deep_sleep(900)
+    magtag.set_text('{aqi:.0f}'.format(aqi=float(message)), 0, False)
+    magtag.set_text('Battery: {bat:.2f}V'.format(bat=magtag.peripherals.battery), 1)
+
+    time.sleep(2)
+    magtag.exit_and_deep_sleep(900)
+
+pool = socketpool.SocketPool(wifi.radio)
+
+mqtt_client = MQTT.MQTT(
+    broker=secrets["broker"],
+    port=secrets["port"],
+    username=secrets["mqtt_username"],
+    password=secrets["mqtt_pw"],
+    socket_pool=pool,
+)
+
+mqtt_client.on_message = message
+mqtt_client.connect()
+mqtt_client.subscribe("oro/purpleair/aqi")
+mqtt_client.loop()
+
+
+while not done:
+    try:
+        mqtt_client.loop()
+    except (ValueError, RuntimeError) as e:
+        print("Failed to get data, retrying\n", e)
+        wifi.reset()
+        mqtt_client.reconnect()
+        continue
+    time.sleep(1)
