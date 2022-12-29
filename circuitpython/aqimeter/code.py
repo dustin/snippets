e0c6cf70539c774322d3ae1454820d78b109558f
Dustin Sallings <dustin@spy.net>
2022-12-28 21:19:08 -1000

    Looping magtag telling me how the network is and stuff.

--- circuitpython/aqimeter/code.py
+++ circuitpython/aqimeter/code.py
@@ -6,6 +6,7 @@ import wifi
 from microcontroller import watchdog as w
 from watchdog import WatchDogMode
 import time
+import circuitpython_schedule as schedule
 
 import busio
 import board
@@ -40,6 +41,7 @@ except ImportError:
 
 AQI_TOPIC="home/purpleair/aqi"
 TIME_TOPIC="home/local/time"
+NETSTATE_TOPIC="home/ping/8.8.8.8/label"
 PERIOD_TOPIC="home/magtag/period"
 VOLT_TOPIC="home/magtag/{mqtt_username}/voltage".format(**secrets)
 BAT_TOPIC="home/magtag/{mqtt_username}/battery".format(**secrets)
@@ -52,7 +54,7 @@ MIN_LIGHT=500
 
 sleepTime=900
 
-w.timeout=10.0
+w.timeout=60.0
 w.mode = WatchDogMode.RAISE
 w.feed()
 
@@ -70,6 +72,7 @@ def cylon(color):
     magtag.peripherals.neopixel_disable = True
     w.feed()
 
+schedule.every(1).seconds.do(w.feed)
 
 # Before we do anything of interest, check to see if the light's on.
 # If it's dark, we shouldn't do anything.
@@ -80,11 +83,72 @@ if magtag.peripherals.light < MIN_LIGHT:
     cylon((4,0,0))
     magtag.exit_and_deep_sleep(60)
 
+class State:
+    def __init__(self):
+        self.dirty = False
+        self.time = None
+        self.purpleAQI = None
+        self.netState = None
+        self.volts = None
 
-def main():
-    w.feed()
-    startTime = time.monotonic()
+        pool = socketpool.SocketPool(wifi.radio)
 
+        self.mqtt_client = MQTT.MQTT(
+            broker=secrets["broker"],
+            port=secrets["port"],
+            username=secrets["mqtt_username"],
+            password=secrets["mqtt_pw"],
+            socket_pool=pool,
+        )
+
+    def gotTime(self, client, topic, t):
+        self.time = t
+
+    def gotAQI(self, client, topic, a):
+        self.purpleAQI = float(a)
+        self.dirty = True
+
+    def updatePM25(self):
+        if pm25:
+            aqdata = pm25.read()
+            # See also "pm10 standard", "pm100 standard", "pm10 env", "pm25 env", "pm100 env"
+            self.mqtt_client.publish(PM25_TOPIC, aqdata["pm25 standard"], retain=True)
+            magtag.set_text('Inside: {aqi}'.format(aqi=aqdata["pm25 standard"]), 2, False)
+            self.dirty = True
+
+    def updateCO2(self):
+        if scd and scd.data_available and scd.CO2 > 0:
+            self.mqtt_client.publish(CO2_TOPIC, scd.CO2, retain=True)
+            self.mqtt_client.publish(TEMP_TOPIC, scd.temperature, retain=True)
+            self.mqtt_client.publish(HUMIDITY_TOPIC, scd.relative_humidity, retain=True)
+            magtag.set_text('CO2: {co2:.0f} ppm'.format(co2=scd.CO2), 3, False)
+            self.dirty = True
+
+    def updateBattery(self):
+        self.volts = magtag.peripherals.battery
+        self.mqtt_client.publish(VOLT_TOPIC, self.volts, retain=True)
+        self.mqtt_client.publish(BAT_TOPIC, min(100, self.volts*100 / 4.2), retain=True)
+
+    def gotNetState(self, client, topic, t):
+        self.netState = t
+        colors = {'ok': (0, 8, 0),
+                  'slow': (255, 191, 0),
+                  'loss': (255, 0, 0)}
+        magtag.peripherals.neopixels.fill(colors[t])
+
+    def draw(self):
+        if self.dirty and self.time is not None and self.purpleAQI is not None and self.volts is not None:
+            magtag.set_text('{aqi:.0f}'.format(aqi=self.purpleAQI), 0, False)
+            magtag.set_text('{time}                 {bat:.2f}V'.format(time=self.time, bat=self.volts), 1, False)
+            magtag.refresh()
+            self.dirty = False
+
+state = State()
+
+def init():
+    schedule.every(60).seconds.do(state.updatePM25)
+    schedule.every(60).seconds.do(state.updateCO2)
+    schedule.every(60).seconds.do(state.updateBattery)
     # The outside AQI
     magtag.add_text(
         text_font="/fonts/Helvetica-Bold-100.bdf",
@@ -130,101 +194,43 @@ def main():
 
     w.feed()
 
-    timeAndAQI = ["", -1, 0]
+    state.mqtt_client.add_topic_callback(AQI_TOPIC, state.gotAQI)
+    state.mqtt_client.add_topic_callback(TIME_TOPIC, state.gotTime)
+    state.mqtt_client.add_topic_callback(NETSTATE_TOPIC, state.gotNetState)
+    state.mqtt_client.connect()
+    state.mqtt_client.subscribe(AQI_TOPIC)
+    state.mqtt_client.subscribe(TIME_TOPIC)
+    state.mqtt_client.subscribe(PERIOD_TOPIC)
+    schedule.every(60).seconds.do(state.mqtt_client.loop)
 
-    def isReady():
-        return timeAndAQI[0] != "" and timeAndAQI[1] != -1 and timeAndAQI[2] != 0
 
-    def gotAQI(client, topic, message):
-        timeAndAQI[1] = float(message)
-
-    def gotTime(client, topic, message):
-        timeAndAQI[0] = message
-
-    def gotPeriod(client, topic, message):
-        global sleepTime
-        sleepTime = int(message)
-        timeAndAQI[2] = sleepTime
-
-    pool = socketpool.SocketPool(wifi.radio)
-
-    mqtt_client = MQTT.MQTT(
-        broker=secrets["broker"],
-        port=secrets["port"],
-        username=secrets["mqtt_username"],
-        password=secrets["mqtt_pw"],
-        socket_pool=pool,
-    )
-
-    volts = magtag.peripherals.battery
-
-    mqtt_client.add_topic_callback(AQI_TOPIC, gotAQI)
-    mqtt_client.add_topic_callback(TIME_TOPIC, gotTime)
-    mqtt_client.add_topic_callback(PERIOD_TOPIC, gotPeriod)
-    mqtt_client.connect()
-    mqtt_client.subscribe(AQI_TOPIC)
-    mqtt_client.subscribe(TIME_TOPIC)
-    mqtt_client.subscribe(PERIOD_TOPIC)
-    mqtt_client.publish(VOLT_TOPIC, volts, retain=True)
-    mqtt_client.publish(BAT_TOPIC, min(100, volts*100 / 4.2), retain=True)
-
+def main():
     w.feed()
-
-    if pm25:
-        aqdata = pm25.read()
-        # See also "pm10 standard", "pm100 standard", "pm10 env", "pm25 env", "pm100 env"
-        mqtt_client.publish(PM25_TOPIC, aqdata["pm25 standard"], retain=True)
-        magtag.set_text('Inside: {aqi}'.format(aqi=aqdata["pm25 standard"]), 2, False)
-
-    w.feed()
-
-    if scd and scd.data_available and scd.CO2 > 0:
-        mqtt_client.publish(CO2_TOPIC, scd.CO2, retain=True)
-        mqtt_client.publish(TEMP_TOPIC, scd.temperature, retain=True)
-        mqtt_client.publish(HUMIDITY_TOPIC, scd.relative_humidity, retain=True)
-        magtag.set_text('CO2: {co2:.0f} ppm'.format(co2=scd.CO2), 3, False)
-
-
-    w.feed()
-    for i in range (0, 30):
-        mqtt_client.loop()
-
-        if isReady():
-            break
-        time.sleep(1)
-
-    w.feed()
-    finished = time.monotonic()
-    print("time spent: ", finished)
-    mqtt_client.publish(DUR_TOPIC, finished)
-
-    if not isReady():
-        cylon((6,3,16))
-        return
-
+    init()
+    schedule.run_all()
     magtag.peripherals.neopixels.fill((0, 0, 0))
-    magtag.peripherals.neopixel_disable = True
+    state.mqtt_client.subscribe(NETSTATE_TOPIC)
 
-    magtag.set_text('{aqi:.0f}'.format(aqi=timeAndAQI[1]), 0, False)
-    magtag.set_text('{time}                 {bat:.2f}V'.format(time=timeAndAQI[0], bat=volts), 1, False)
+    while True:
+        schedule.run_pending()
+        state.draw()
+        time.sleep(1) # schedule.idle_seconds())
 
-    magtag.refresh()
-
-    w.feed()
-    time.sleep(2)
-    if volts > 4.1:
-        global sleepTime
-        sleepTime = sleepTime / 10
-
+        if state.volts < 3.5:
+            print("doing a deep sleep")
+            magtag.peripherals.neopixels.fill((0, 0, 0))
+            magtag.peripherals.neopixel_disable = True
+            w.deinit()
+            magtag.exit_and_deep_sleep(900)
 
 main()
 
-try:
-    main()
-except:
-    print("oh no: exception")
-    cylon((16,0,0))
-
-w.deinit()
-print("Sleeping for", sleepTime)
-magtag.exit_and_deep_sleep(sleepTime)
+while True:
+    try:
+        main()
+    except:
+        print("oh no: exception")
+        cylon((16,0,0))
+    for i in range(60):
+        w.feed()
+        time.sleep(1)
